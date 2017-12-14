2f4c9d18af7a607d211a55436fcc26ded30c2577
Simone Zanin <zabi94@gmail.com>
2017-12-14 22:30:19 +0100

    Garlic seeds now drop from allium flower
    1 in 5 flowers should drop garlic seeds instead of allium. Allium can be replanted until a seed is dropped, so I only added a way to guarantee it to drop allium (any silk touch)

--- src/main/java/com/bewitchment/common/item/food/seed/SeedDropRegistry.java
+++ src/main/java/com/bewitchment/common/item/food/seed/SeedDropRegistry.java
@@ -1,14 +1,20 @@
 package com.bewitchment.common.item.food.seed;
 
 import com.bewitchment.common.item.ModItems;
+
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * This class was created by Joseph on 5/29/2017.
  * It's distributed as part of Bewitchment under
  * the MIT license.
  */
+@Mod.EventBusSubscriber
 public class SeedDropRegistry {
 
 	public static void init() {
@@ -25,7 +31,14 @@ public class SeedDropRegistry {
 		MinecraftForge.addGrassSeed(new ItemStack(ModItems.seed_asphodel), 6);
 		MinecraftForge.addGrassSeed(new ItemStack(ModItems.seed_tulsi), 3);
 		MinecraftForge.addGrassSeed(new ItemStack(ModItems.seed_kenaf), 3);
-		MinecraftForge.addGrassSeed(new ItemStack(ModItems.seed_garlic), 4);
 		MinecraftForge.addGrassSeed(new ItemStack(ModItems.seed_silphium), 1);
+	}
+	
+	@SubscribeEvent
+	public static void onHarvestAllium(BlockEvent.HarvestDropsEvent harvest) {
+		if (harvest.getState().getBlock() == Blocks.RED_FLOWER && harvest.getState().getBlock().getMetaFromState(harvest.getState()) == 2 && harvest.getWorld().rand.nextInt(5) == 0 && !harvest.isSilkTouching()) {
+			harvest.getDrops().clear();
+			harvest.getDrops().add(new ItemStack(ModItems.seed_garlic, 1));
+		}
 	}
 }
