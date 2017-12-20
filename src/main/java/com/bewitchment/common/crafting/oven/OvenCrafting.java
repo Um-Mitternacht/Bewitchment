baff446cbf5a563ff993e20b982a82dea64da7bb
Simone Zanin <zabi94@gmail.com>
2017-12-20 11:56:48 +0100

    Re-added oven recipes
    Since my recipes don't have a jungle fume counterpart, the output is cloudy oil (~foul fume). Also reduced instantiation overhead of ash itemstack and cloudy oil itemstack (not much but meh)

--- src/main/java/com/bewitchment/common/crafting/oven/OvenCrafting.java
+++ src/main/java/com/bewitchment/common/crafting/oven/OvenCrafting.java
@@ -22,12 +22,15 @@ public class OvenCrafting {
 	private final Map<ItemStack, ItemStack> fumeList = Maps.<ItemStack, ItemStack>newHashMap();
 
 	public OvenCrafting() {
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume, 1, 2));
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 1), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume, 1, 3));
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 2), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume, 1, 1));
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 3), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume));
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 4), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume, 1, 8));
-		// addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 5), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume, 1, 7));
+		final ItemStack ash = new ItemStack(ModItems.wood_ash);
+		final ItemStack cloudy_oil = new ItemStack(ModItems.fume, 1, 6);
+		
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING), ash, new ItemStack(ModItems.fume, 1, 2));
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 1), ash, new ItemStack(ModItems.fume, 1, 5));
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 2), ash, new ItemStack(ModItems.fume, 1, 3));
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 3), ash, cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 4), ash, new ItemStack(ModItems.fume, 1, 4));
+		addSmeltingRecipe(new ItemStack(Blocks.SAPLING, 1, 5), ash, new ItemStack(ModItems.fume, 1, 2));
 		addSmeltingRecipe(new ItemStack(Items.ROTTEN_FLESH), new ItemStack(Items.LEATHER), new ItemStack(ModItems.ectoplasm, 3));
 		addSmeltingRecipe(new ItemStack(Blocks.IRON_ORE), new ItemStack(Items.IRON_INGOT), new ItemStack(Items.IRON_NUGGET, 4));
 		addSmeltingRecipe(new ItemStack(Blocks.GOLD_ORE), new ItemStack(Items.GOLD_INGOT), new ItemStack(Items.GOLD_NUGGET, 2));
@@ -39,17 +42,17 @@ public class OvenCrafting {
 		addSmeltingRecipe(new ItemStack(Blocks.LOG2), new ItemStack(Items.COAL, 1, 1), new ItemStack(ModItems.wood_ash, 3));
 		addSmeltingRecipe(new ItemStack(Blocks.LOG2, 1, 1), new ItemStack(Items.COAL, 1, 1), new ItemStack(ModItems.wood_ash, 3));
 		addSmeltingRecipe(new ItemStack(Items.BONE), new ItemStack(Items.DYE, 1, 15), new ItemStack(ModItems.ectoplasm, 1));
-		addSmeltingRecipe(new ItemStack(Items.WHEAT), new ItemStack(Items.BREAD), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.RABBIT), new ItemStack(Items.COOKED_RABBIT), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.PORKCHOP), new ItemStack(Items.COOKED_PORKCHOP), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.BEEF), new ItemStack(Items.COOKED_BEEF), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.CHICKEN), new ItemStack(Items.COOKED_CHICKEN), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.FISH), new ItemStack(Items.COOKED_FISH), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.FISH, 1, 1), new ItemStack(Items.COOKED_FISH, 1, 1), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Items.MELON), new ItemStack(ModItems.grilled_watermelon), new ItemStack(ModItems.fume));
-		addSmeltingRecipe(new ItemStack(Blocks.CACTUS), new ItemStack(Items.DYE, 1, 2), new ItemStack(ModItems.fume));
+		addSmeltingRecipe(new ItemStack(Items.WHEAT), new ItemStack(Items.BREAD), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.RABBIT), new ItemStack(Items.COOKED_RABBIT), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.PORKCHOP), new ItemStack(Items.COOKED_PORKCHOP), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.BEEF), new ItemStack(Items.COOKED_BEEF), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.CHICKEN), new ItemStack(Items.COOKED_CHICKEN), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.FISH), new ItemStack(Items.COOKED_FISH), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.FISH, 1, 1), new ItemStack(Items.COOKED_FISH, 1, 1), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Items.MELON), new ItemStack(ModItems.grilled_watermelon), cloudy_oil);
+		addSmeltingRecipe(new ItemStack(Blocks.CACTUS), new ItemStack(Items.DYE, 1, 2), cloudy_oil);
 		addSmeltingRecipe(new ItemStack(Items.CHORUS_FRUIT), new ItemStack(Items.CHORUS_FRUIT_POPPED), new ItemStack(ModItems.dimensional_sand, 2));
-		addSmeltingRecipe(new ItemStack(ModItems.mandrake_root), new ItemStack(ModItems.wood_ash), new ItemStack(ModItems.fume));
+		addSmeltingRecipe(new ItemStack(ModItems.mandrake_root), ash, cloudy_oil);
 	}
 
 	public static OvenCrafting instance() {
