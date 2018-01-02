a7616f767bbc610802f2fa373cb9c337a1c01093
Sunconure11 <eyespot7b@aol.com>
2018-01-01 20:54:21 -0500

    More nazar progress

--- src/main/java/com/bewitchment/common/item/equipment/ItemNazar.java
+++ src/main/java/com/bewitchment/common/item/equipment/ItemNazar.java
@@ -1,13 +1,24 @@
 package com.bewitchment.common.item.equipment;
 
 import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
 import baubles.api.IBauble;
+import baubles.api.cap.IBaublesItemHandler;
 import com.bewitchment.common.core.BewitchmentCreativeTabs;
 import com.bewitchment.common.item.ItemMod;
 import com.bewitchment.common.lib.LibItemName;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -22,8 +33,38 @@ public class ItemNazar extends ItemMod implements IBauble {
 	}
 
 	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if (!world.isRemote) {
+			IBaublesItemHandler baubles = BaublesApi.getBaublesHandler(player);
+			for (int i = 0; i < baubles.getSlots(); i++)
+				if ((baubles.getStackInSlot(i) == null || baubles.getStackInSlot(i).isEmpty()) && baubles.isItemValidForSlot(i, player.getHeldItem(hand), player)) {
+					baubles.setStackInSlot(i, player.getHeldItem(hand).copy());
+					if (!player.capabilities.isCreativeMode) {
+						player.inventory.setInventorySlotContents(player.inventory.currentItem, ItemStack.EMPTY);
+					}
+					onEquipped(player.getHeldItem(hand), player);
+					break;
+				}
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+	}
+
+	@Override
 	public BaubleType getBaubleType(ItemStack itemStack) {
 		return BaubleType.AMULET;
+	}
+
+	//Apply good luck for now, figure out how to clear bad luck later. I need to sleep.
+	@Override
+	public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
+		if (itemstack.getItemDamage() == 0 && player.ticksExisted % 39 == 0) {
+			player.addPotionEffect(new PotionEffect(MobEffects.LUCK, 60, 0, true, true));
+		}
+	}
+
+	@Override
+	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
+		player.playSound(SoundEvents.ITEM_ARMOR_EQUIP_CHAIN, .75F, 1.9f);
 	}
 
 	@SubscribeEvent
