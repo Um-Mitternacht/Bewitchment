72d30a2acd15b032971c081ec10e2d21dc5a4438
Anonymous <Sunconure11@users.noreply.github.com>
2018-01-05 00:05:15 -0500

    Update package-info.java

--- src/main/java/com/bewitchment/api/package-info.java
+++ src/main/java/com/bewitchment/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = "bewitchment", apiVersion = "0.1", provides = "BewitchmentAPI")
+@API(owner = "bewitchment", apiVersion = "0.0.2", provides = "BewitchmentAPI")
 package com.bewitchment.api;
 
 import net.minecraftforge.fml.common.API;
