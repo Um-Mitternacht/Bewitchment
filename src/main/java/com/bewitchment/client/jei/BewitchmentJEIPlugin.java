2a9326870a8b31b5a200a4ef315cd31c91308795
zabi94 <zabi94@gmail.com>
2018-01-10 22:07:52 +0100

    JEI plugin for Rituals
    Also, this clearly shows that there's a bug with the wither conjuration recipe, will need to be fixed. Witch conjuration recipe is also missing for a bug, to investigate

---
+++ src/main/java/com/bewitchment/client/jei/BewitchmentJEIPlugin.java
@@ -0,0 +1,42 @@
+package com.bewitchment.client.jei;
+
+import com.bewitchment.api.ritual.Ritual;
+import com.bewitchment.client.jei.components.RitualCategory;
+import com.bewitchment.client.jei.components.RitualWrapper;
+import com.bewitchment.common.block.tools.BlockCircleGlyph;
+import com.bewitchment.common.item.ModItems;
+
+import mezz.jei.api.*;
+import mezz.jei.api.recipe.IRecipeCategoryRegistration;
+import mezz.jei.api.recipe.IRecipeWrapper;
+import mezz.jei.api.recipe.IRecipeWrapperFactory;
+import net.minecraft.item.ItemStack;
+
+@JEIPlugin
+public class BewitchmentJEIPlugin implements IModPlugin {
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registry) {
+		registry.addRecipeCategories(new RitualCategory(registry.getJeiHelpers().getGuiHelper()));
+	}
+	
+	@Override
+	public void register(IModRegistry registry) {
+		registry.handleRecipes(Ritual.class, new RitualWrapperFactory(registry.getJeiHelpers().getGuiHelper()), RitualCategory.UID);
+		registry.addRecipes(Ritual.REGISTRY.getValues(), RitualCategory.UID);
+		registry.addRecipeCatalyst(new ItemStack(ModItems.ritual_chalk, 1, BlockCircleGlyph.GlyphType.GOLDEN.ordinal()), RitualCategory.UID);
+	}
+	
+	protected static class RitualWrapperFactory implements IRecipeWrapperFactory<Ritual> {
+		
+		private IGuiHelper igh;
+		
+		public RitualWrapperFactory(IGuiHelper igh) {
+			this.igh = igh;
+		}
+		
+		@Override
+		public IRecipeWrapper getRecipeWrapper(Ritual recipe) {
+			return new RitualWrapper(recipe, igh);
+		}
+	}
+}
