79d4fbd958c31242d681be5a194163fdd943daba
zabi94 <zabi94@gmail.com>
2018-01-11 14:51:58 +0100

    Minor tweak to show in jei that circles can be different

--- src/main/java/com/bewitchment/client/jei/components/RitualWrapper.java
+++ src/main/java/com/bewitchment/client/jei/components/RitualWrapper.java
@@ -1,9 +1,12 @@
 package com.bewitchment.client.jei.components;
 
+import java.util.List;
+
 import com.bewitchment.api.ritual.Ritual;
 import com.bewitchment.common.block.tools.BlockCircleGlyph;
 import com.bewitchment.common.block.tools.BlockCircleGlyph.GlyphType;
 import com.bewitchment.common.lib.LibMod;
+
 import mezz.jei.api.IGuiHelper;
 import mezz.jei.api.gui.IDrawable;
 import mezz.jei.api.ingredients.IIngredients;
@@ -14,8 +17,6 @@ import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-
-import java.util.List;
 
 public class RitualWrapper implements IRecipeWrapper {
 	private static IDrawable centerGlyph, circle1, circle2, circle3;
@@ -62,24 +63,24 @@ public class RitualWrapper implements IRecipeWrapper {
 		GlyphType typeFirst = BlockCircleGlyph.GlyphType.fromMeta(circles >> 2 & 3);
 		GlyphType typeSecond = BlockCircleGlyph.GlyphType.fromMeta(circles >> 4 & 3);
 		GlyphType typeThird = BlockCircleGlyph.GlyphType.fromMeta(circles >> 6 & 3);
-		color(GlyphType.GOLDEN, null);
+		color(GlyphType.GOLDEN, null, 0);
 
 		int dx = 53, dy = 35;
 
 		centerGlyph.draw(minecraft, dx, dy);
-		color(typeFirst, minecraft);
+		color(typeFirst, minecraft, 0);
 		circle1.draw(minecraft, dx, dy);
 		if (requiredCircles > 0) {
-			color(typeSecond, minecraft);
+			color(typeSecond, minecraft, 1);
 			circle2.draw(minecraft, dx, dy);
 		}
 		if (requiredCircles > 1) {
-			color(typeThird, minecraft);
+			color(typeThird, minecraft, 2);
 			circle3.draw(minecraft, dx, dy);
 		}
 	}
 
-	private void color(GlyphType gt, Minecraft minecraft) {
+	private void color(GlyphType gt, Minecraft minecraft, int circle) {
 		switch (gt) {
 			case ENDER:
 				GlStateManager.color(0.5f, 0f, 0.5f);
@@ -94,22 +95,22 @@ public class RitualWrapper implements IRecipeWrapper {
 				GlStateManager.color(0.9f, 0.9f, 0.9f);
 				break;
 			case ANY:
-				colorRandom(minecraft.world.getTotalWorldTime());
+				colorRandom(minecraft.world.getTotalWorldTime(), circle);
 				break;
 		}
 	}
 
-	private void colorRandom(long v) {
+	private void colorRandom(long v, int circle) {
 		int r = (int) (v % 60 / 20);
-		switch (r) {
+		switch ((r + circle) % 3) {
 			case 1:
-				color(GlyphType.NORMAL, null);
+				color(GlyphType.NORMAL, null, 0);
 				break;
 			case 2:
-				color(GlyphType.ENDER, null);
+				color(GlyphType.ENDER, null, 0);
 				break;
 			case 0:
-				color(GlyphType.NETHER, null);
+				color(GlyphType.NETHER, null, 0);
 				break;
 			default:
 				break;
