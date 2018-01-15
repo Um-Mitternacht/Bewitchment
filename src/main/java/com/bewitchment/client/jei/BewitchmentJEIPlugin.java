cf7ce2042556e8bc3a2fc9d5db299a0679fd8340
zabi94 <zabi94@gmail.com>
2018-01-15 20:46:49 +0100

    Temp ritual sorter

--- src/main/java/com/bewitchment/client/jei/BewitchmentJEIPlugin.java
+++ src/main/java/com/bewitchment/client/jei/BewitchmentJEIPlugin.java
@@ -1,18 +1,15 @@
 package com.bewitchment.client.jei;
 
+import java.util.stream.Collectors;
+
 import com.bewitchment.api.ritual.Ritual;
-import com.bewitchment.client.jei.components.RitualCategory;
-import com.bewitchment.client.jei.components.RitualWrapper;
-import com.bewitchment.client.jei.components.SpinnerCategory;
-import com.bewitchment.client.jei.components.SpinnerWrapper;
+import com.bewitchment.client.jei.components.*;
 import com.bewitchment.common.block.ModBlocks;
 import com.bewitchment.common.block.tools.BlockCircleGlyph;
 import com.bewitchment.common.item.ModItems;
 import com.bewitchment.common.spinning.SpinningThreadRecipe;
-import mezz.jei.api.IGuiHelper;
-import mezz.jei.api.IModPlugin;
-import mezz.jei.api.IModRegistry;
-import mezz.jei.api.JEIPlugin;
+
+import mezz.jei.api.*;
 import mezz.jei.api.recipe.IRecipeCategoryRegistration;
 import mezz.jei.api.recipe.IRecipeWrapper;
 import mezz.jei.api.recipe.IRecipeWrapperFactory;
@@ -29,7 +26,7 @@ public class BewitchmentJEIPlugin implements IModPlugin {
 	@Override
 	public void register(IModRegistry registry) {
 		registry.handleRecipes(Ritual.class, new RitualWrapperFactory(registry.getJeiHelpers().getGuiHelper()), RitualCategory.UID);
-		registry.addRecipes(Ritual.REGISTRY.getValues(), RitualCategory.UID);
+		registry.addRecipes(Ritual.REGISTRY.getValues().stream().sorted(BewitchmentJEIPlugin::compareRituals).collect(Collectors.toList()), RitualCategory.UID);
 		registry.addRecipeCatalyst(new ItemStack(ModItems.ritual_chalk, 1, BlockCircleGlyph.GlyphType.GOLDEN.ordinal()), RitualCategory.UID);
 
 		registry.handleRecipes(SpinningThreadRecipe.class, i -> new SpinnerWrapper(i), SpinnerCategory.UID);
@@ -49,5 +46,15 @@ public class BewitchmentJEIPlugin implements IModPlugin {
 		public IRecipeWrapper getRecipeWrapper(Ritual recipe) {
 			return new RitualWrapper(recipe, igh);
 		}
+	}
+	
+	protected static int compareRituals(Ritual a, Ritual b) {
+		if (a == b)
+			return 0;
+		int av = a.getInput().size() / 3;
+		int bv = b.getInput().size() / 3;
+		av += a.getCircles() & 3;
+		bv += b.getCircles() & 3;
+		return av > bv ? 1 : -1;
 	}
 }
