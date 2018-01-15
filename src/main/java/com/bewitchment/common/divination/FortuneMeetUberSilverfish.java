734276bb38d08a71a3b2a723a6307bbd417861c1
Sunconure11 <eyespot7b@aol.com>
2018-01-15 15:13:59 -0500

    Created a trollish fortune for now
    Better ones to come later

---
+++ src/main/java/com/bewitchment/common/divination/FortuneMeetUberSilverfish.java
@@ -0,0 +1,47 @@
+package com.bewitchment.common.divination;
+
+import com.bewitchment.api.divination.Fortune;
+import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.BlockPos;
+
+public class FortuneMeetUberSilverfish extends Fortune {
+
+	public FortuneMeetUberSilverfish(int weight, String name, String modid) {
+		super(weight, name, modid);
+	}
+
+	@Override
+	public boolean canBeUsedFor(EntityPlayer player) {
+		return true;
+	}
+
+	@Override
+	public boolean canShouldBeAppliedNow(EntityPlayer player) {
+		return player.dimension != 1 && player.dimension != -1 && player.getRNG().nextDouble() < 0.0001;
+	}
+
+	@Override
+	public boolean apply(EntityPlayer player) {
+		for (int i = 0; i < 10; i++) {
+			BlockPos pos = new BlockPos(player.posX + player.getRNG().nextGaussian() * 4, player.posY, player.posZ + player.getRNG().nextGaussian() * 4);
+			EntityZombie zombie = new EntityZombie(player.world);
+			if (player.world.isAirBlock(pos) && player.world.isAirBlock(pos.up()) && player.world.getBlockState(pos.down()).canEntitySpawn(zombie)) {
+				zombie.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+				zombie.onInitialSpawn(player.world.getDifficultyForLocation(pos), null);
+				player.world.spawnEntity(zombie);
+				if (player.getRNG().nextBoolean() && player.getRNG().nextBoolean())
+					zombie.addPotionEffect(new PotionEffect(MobEffects.RESISTANCE, 900, 2));
+				if (player.getRNG().nextBoolean() && player.getRNG().nextBoolean())
+					zombie.addPotionEffect(new PotionEffect(MobEffects.STRENGTH, 900, 2));
+				if (player.getRNG().nextBoolean() && player.getRNG().nextBoolean())
+					zombie.addPotionEffect(new PotionEffect(MobEffects.SPEED, 900, 2));
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
--- src/main/java/com/bewitchment/common/divination/ModFortunes.java
+++ src/main/java/com/bewitchment/common/divination/ModFortunes.java
@@ -7,15 +7,18 @@ import com.bewitchment.common.lib.LibMod;
 public class ModFortunes {
 
 	private static Fortune zombie;
+	private static Fortune silverfish;
 
 	public static void init() {
 		zombie = new FortuneMeetZombie(10, "meet_zombie", LibMod.MOD_ID);
+		silverfish = new FortuneMeetZombie(10, "meet_silverfish", LibMod.MOD_ID);
 		registerAll();
 	}
 
 	public static void registerAll() {
 		Fortune.REGISTRY.registerAll(
-				zombie
+				zombie,
+				silverfish
 		);
 	}
 }
