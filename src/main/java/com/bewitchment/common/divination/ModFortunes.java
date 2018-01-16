0ceefc354e195069eec142adbff126bbe8d9e6ce
zabi94 <zabi94@gmail.com>
2018-01-16 14:09:48 +0100

    Drop bow fortune

--- src/main/java/com/bewitchment/common/divination/ModFortunes.java
+++ src/main/java/com/bewitchment/common/divination/ModFortunes.java
@@ -8,17 +8,20 @@ public class ModFortunes {
 
 	private static Fortune zombie;
 	private static Fortune silverfish;
+	private static Fortune dropBow;
 
 	public static void init() {
 		zombie = new FortuneMeetZombie(10, "meet_zombie", LibMod.MOD_ID);
 		silverfish = new FortuneMeetUberSilverfish(10, "meet_silverfish", LibMod.MOD_ID);
+		dropBow = new FortuneDropBow(7, "drop_bow", LibMod.MOD_ID);
 		registerAll();
 	}
 
 	public static void registerAll() {
 		Fortune.REGISTRY.registerAll(
 				zombie,
-				silverfish
+				silverfish,
+				dropBow
 		);
 	}
 }
