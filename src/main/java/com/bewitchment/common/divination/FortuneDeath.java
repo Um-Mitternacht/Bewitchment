a0e1fca8f1901616e1bd7466296c36fd44162676
Sunconure11 <eyespot7b@aol.com>
2018-01-16 09:14:39 -0500

    Committing due to a power outage

--- src/main/java/com/bewitchment/common/divination/FortuneDeath.java
+++ src/main/java/com/bewitchment/common/divination/FortuneDeath.java
@@ -2,38 +2,42 @@ package com.bewitchment.common.divination;
 
 import com.bewitchment.api.divination.Fortune;
 import com.bewitchment.common.core.capability.divination.CapabilityDivination;
-
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.ArrowNockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class FortuneDropBow extends Fortune {
-	
-	public FortuneDropBow(int weight, String name, String modid) {
+import javax.annotation.Nonnull;
+
+/**
+ * Created by Joseph on 1/16/2018.
+ */
+public class FortuneDeath extends Fortune {
+
+	public FortuneDeath(int weight, String name, String modid) {
 		super(weight, name, modid);
 		MinecraftForge.EVENT_BUS.register(this);
 	}
-	
+
 	@Override
-	public boolean canBeUsedFor(EntityPlayer player) {
+	public boolean canBeUsedFor(@Nonnull EntityPlayer player) {
 		return true;
 	}
-	
+
 	@Override
-	public boolean canShouldBeAppliedNow(EntityPlayer player) {
+	public boolean canShouldBeAppliedNow(@Nonnull EntityPlayer player) {
 		return player.getRNG().nextDouble() < 0.0005d;
 	}
-	
+
 	@Override
-	public boolean apply(EntityPlayer player) {
+	public boolean apply(@Nonnull EntityPlayer player) {
 		player.getCapability(CapabilityDivination.CAPABILITY, null).setActive();
 		return false;
 	}
-	
+
 	@SubscribeEvent
-	public void onArrowKnock(ArrowNockEvent evt) {
+	public void onArrowNock(ArrowNockEvent evt) {
 		if (evt.getEntityPlayer() != null && !evt.getEntityPlayer().isCreative() && evt.getHand() == EnumHand.MAIN_HAND) { // Needs to check for mainhand due to how the event works
 			CapabilityDivination cap = evt.getEntityPlayer().getCapability(CapabilityDivination.CAPABILITY, null);
 			if (cap.getFortune() == this && cap.isActive()) {
