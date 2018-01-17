4c170be2b03919582e6ae8d393b1f53ebed981fc
Sunconure11 <eyespot7b@aol.com>
2018-01-17 10:52:02 -0500

    Reformat

--- src/main/java/com/bewitchment/common/block/decorations/BlockScornedBrickStairs.java
+++ src/main/java/com/bewitchment/common/block/decorations/BlockScornedBrickStairs.java
@@ -9,7 +9,6 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockStairs;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
