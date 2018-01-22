17800ae76915a27665b8e5c32433849e10e94a0a
Sunconure11 <eyespot7b@aol.com>
2018-01-22 14:07:59 -0500

    Witch fortune

--- src/main/java/com/bewitchment/common/divination/ModFortunes.java
+++ src/main/java/com/bewitchment/common/divination/ModFortunes.java
@@ -2,10 +2,7 @@ package com.bewitchment.common.divination;
 
 
 import com.bewitchment.api.divination.Fortune;
-import com.bewitchment.common.divination.fortunes.FortuneDeath;
-import com.bewitchment.common.divination.fortunes.FortuneDropBow;
-import com.bewitchment.common.divination.fortunes.FortuneMeetUberSilverfish;
-import com.bewitchment.common.divination.fortunes.FortuneMeetZombie;
+import com.bewitchment.common.divination.fortunes.*;
 import com.bewitchment.common.lib.LibMod;
 
 public class ModFortunes {
@@ -14,10 +11,12 @@ public class ModFortunes {
 	private static Fortune silverfish;
 	private static Fortune dropBow;
 	private static Fortune death;
+	private static Fortune witch;
 
 	public static void init() {
 		zombie = new FortuneMeetZombie(10, "meet_zombie", LibMod.MOD_ID);
 		silverfish = new FortuneMeetUberSilverfish(8, "meet_silverfish", LibMod.MOD_ID);
+		witch = new FortuneMeetWitch(7, "meet_witch", LibMod.MOD_ID);
 		dropBow = new FortuneDropBow(7, "drop_bow", LibMod.MOD_ID);
 		death = new FortuneDeath(2, "death", LibMod.MOD_ID);
 		registerAll();
@@ -28,7 +27,8 @@ public class ModFortunes {
 				zombie,
 				silverfish,
 				dropBow,
-				death
+				death,
+				witch
 		);
 	}
 }
