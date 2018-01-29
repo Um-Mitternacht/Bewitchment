5566fe3aeb2690ef605e0d03bcd89503fb198f64
Sunconure11 <eyespot7b@aol.com>
2018-01-29 15:51:01 -0500

    Dark green

--- src/main/java/com/bewitchment/common/item/baubles/ItemBarkBelt.java
+++ src/main/java/com/bewitchment/common/item/baubles/ItemBarkBelt.java
@@ -261,7 +261,7 @@ public class ItemBarkBelt extends ItemMod implements IBauble, IRenderBauble {
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
-		tooltip.add(TextFormatting.AQUA + I18n.format("witch.tooltip." + getUnlocalizedNameInefficiently(stack).substring(5) + "_description.name"));
+		tooltip.add(TextFormatting.DARK_GREEN + I18n.format("witch.tooltip." + getUnlocalizedNameInefficiently(stack).substring(5) + "_description.name"));
 	}
 
 	@Override
