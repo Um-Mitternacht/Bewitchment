8461a8f6872e2473e70a9522ce4b32f4b4051d97
zabi94 <zabi94@gmail.com>
2018-01-30 11:07:08 +0100

    Fix Planks crashing

--- src/main/java/com/bewitchment/common/block/natural/tree/BlockPlanks.java
+++ src/main/java/com/bewitchment/common/block/natural/tree/BlockPlanks.java
@@ -1,8 +1,7 @@
 package com.bewitchment.common.block.natural.tree;
 
-import com.bewitchment.common.core.BewitchmentCreativeTabs;
-import com.bewitchment.common.lib.LibMod;
-import net.minecraft.block.Block;
+import com.bewitchment.common.block.BlockMod;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 
@@ -10,14 +9,11 @@ import net.minecraft.block.material.Material;
  * Original class created by Zabi94.
  */
 
-public class BlockPlanks extends Block {
+public class BlockPlanks extends BlockMod {
 
 	public BlockPlanks(String name) {
-		super(Material.WOOD);
-		this.setUnlocalizedName("planks_" + name);
-		this.setRegistryName(LibMod.MOD_ID, "planks_" + name);
+		super(name, Material.WOOD);
 		this.setHarvestLevel("axe", 0);
-		this.setCreativeTab(BewitchmentCreativeTabs.BLOCKS_CREATIVE_TAB);
 		this.setSoundType(SoundType.WOOD);
 	}
 }
