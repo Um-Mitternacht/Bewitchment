e30e629a90947d79feb3f8f9061489e7468b06f6
zabi94 <zabi94@gmail.com>
2018-03-05 21:12:54 +0100

    Default keybindings to NONE
    Let the users decide what they wanna do

--- src/main/java/com/bewitchment/client/handler/Keybinds.java
+++ src/main/java/com/bewitchment/client/handler/Keybinds.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @SideOnly(Side.CLIENT)
 public class Keybinds {
 	
-	public static KeyBinding gotoExtraBar = new KeyBinding("key.description.jump_to_bar", Keyboard.KEY_Z, "key.categories.bewitchment");
+	public static KeyBinding gotoExtraBar = new KeyBinding("key.description.jump_to_bar", Keyboard.KEY_NONE, "key.categories.bewitchment");
 	public static KeyBinding alwaysEnableBar = new KeyBinding("key.description.enable_bar", Keyboard.KEY_NONE, "key.categories.bewitchment");
 	
 	public static void registerKeys() {
