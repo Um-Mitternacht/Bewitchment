103ffd533aba32a1d8255da8785d8897b5046821
Sunconure11 <eyespot7b@aol.com>
2018-03-09 04:53:13 -0500

    Minor refactor

--- src/main/java/com/bewitchment/common/divination/fortunes/FortuneMeetDog.java
+++ src/main/java/com/bewitchment/common/divination/fortunes/FortuneMeetDog.java
@@ -25,12 +25,12 @@ public class FortuneMeetDog extends Fortune {
 	public boolean apply(EntityPlayer player) {
 		for (int i = 0; i < 10; i++) {
 			BlockPos pos = new BlockPos(player.posX + player.getRNG().nextGaussian() * 4, player.posY, player.posZ + player.getRNG().nextGaussian() * 4);
-			EntityWolf wolf = new EntityWolf(player.world);
-			if (player.world.isAirBlock(pos) && player.world.isAirBlock(pos.up()) && player.world.getBlockState(pos.down()).canEntitySpawn(wolf)) {
-				wolf.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
-				wolf.onInitialSpawn(player.world.getDifficultyForLocation(pos), null);
-				wolf.setTamedBy(player);
-				player.world.spawnEntity(wolf);
+			EntityWolf dog = new EntityWolf(player.world);
+			if (player.world.isAirBlock(pos) && player.world.isAirBlock(pos.up()) && player.world.getBlockState(pos.down()).canEntitySpawn(dog)) {
+				dog.setPosition(pos.getX() + 0.5, pos.getY(), pos.getZ() + 0.5);
+				dog.onInitialSpawn(player.world.getDifficultyForLocation(pos), null);
+				dog.setTamedBy(player);
+				player.world.spawnEntity(dog);
 				return true;
 			}
 		}
