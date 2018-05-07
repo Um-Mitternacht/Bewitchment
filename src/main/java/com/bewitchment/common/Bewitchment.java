bde0be0780aa1238bd555139b383e65bae7ac5db
Anonymous <Sunconure11@users.noreply.github.com>
2018-05-07 00:15:57 -0400

    Fixed a lyric line in the main branch too

--- src/main/java/com/bewitchment/common/Bewitchment.java
+++ src/main/java/com/bewitchment/common/Bewitchment.java
@@ -120,7 +120,7 @@ public class Bewitchment {
 		BewitchmentAspectRegistry.register();
 
 		logger.info("It's a fact, she is exactly that!");
-		logger.info("A harbinger of death from the world of bewitchment,");
+		logger.info("A harbinger of death from the world of witchcraft,");
 		logger.info("And she's feeding them cakes and her ale to this innocent boy,");
 		logger.info("And her magic brings dismay!");
 
