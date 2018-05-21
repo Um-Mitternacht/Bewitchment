b1712b168e5ad16c404a9e6203e5d83383fc8be3
jdawg3636 <jdawg3636minecraft@gmail.com>
2018-05-20 20:59:17 -0500

    Added a method of obtaining cold iron ingots
    Added the "Stone Mortar and Pestle," which may be used in a shapeless
    recipie with iron ore to a create cold iron ingot.

--- src/main/java/com/bewitchment/common/lib/LibItemName.java
+++ src/main/java/com/bewitchment/common/lib/LibItemName.java
@@ -77,6 +77,7 @@ public final class LibItemName {
 	public static final String BOLINE = "boline";
 	public static final String TAGLOCK = "taglock";
 	public static final String MORTAR_AND_PESTLE = "mortar_and_pestle";
+	public static final String MORTAR_AND_PESTLE_STONE = "mortar_and_pestle_stone";
 	public static final String WAX = "wax";
 	public static final String SALT = "salt";
 	public static final String BEE = "bee";
