9c41b597b28f4300005b1450969250fbfffb839b
Sunconure11 <eyespot7b@aol.com>
2018-06-14 12:37:50 -0400

    Ores now smelt if obtained via silk touch

--- src/main/java/com/bewitchment/common/crafting/VanillaCrafting.java
+++ src/main/java/com/bewitchment/common/crafting/VanillaCrafting.java
@@ -26,9 +26,21 @@ public final class VanillaCrafting {
 		GameRegistry.addSmelting(ModItems.silver_scales, new ItemStack(ModItems.silver_nugget, 1), 0.20F);
 		GameRegistry.addSmelting(Items.MELON, new ItemStack(ModItems.grilled_watermelon, 1), 0.45F);
 		GameRegistry.addSmelting((new ItemStack(ModItems.fume, 1, ItemFumes.Type.unfired_jar.ordinal())), new ItemStack(ModItems.fume, 1, ItemFumes.Type.empty_jar.ordinal()), 0.45F);
+		GameRegistry.addSmelting(ModBlocks.gem_ore, new ItemStack(ModItems.gem, 4), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 1), new ItemStack(ModItems.gem, 4, 1), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 2), new ItemStack(ModItems.gem, 4, 2), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 3), new ItemStack(ModItems.gem, 4, 3), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 4), new ItemStack(ModItems.gem, 4, 4), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 5), new ItemStack(ModItems.gem, 4, 5), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 6), new ItemStack(ModItems.gem, 4, 6), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 7), new ItemStack(ModItems.gem, 4, 7), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 8), new ItemStack(ModItems.gem, 4, 8), 0.35F);
+		GameRegistry.addSmelting(new ItemStack((ModBlocks.gem_ore), 1, 9), new ItemStack(ModItems.gem, 4, 9), 0.35F);
 
 		ModMaterials.TOOL_RITUAL.setRepairItem(new ItemStack(ModItems.silver_ingot));
 		ModMaterials.ARMOR_SILVER.setRepairItem(new ItemStack(ModItems.silver_ingot));
 		ModMaterials.TOOL_SILVER.setRepairItem(new ItemStack(ModItems.silver_ingot));
+		ModMaterials.TOOL_COLD_IRON.setRepairItem(new ItemStack(ModItems.cold_iron_ingot));
+		ModMaterials.ARMOR_COLD_IRON.setRepairItem(new ItemStack(ModItems.cold_iron_ingot));
 	}
 }
