309519077d1f4adf7b93271910ad4bb489f69b48
zabi94 <zabi94@gmail.com>
2018-06-16 18:42:54 +0200

    Fix crash when no thaumcraft is installed
    We can't reference classes outside hard dependencies and this mod itself. Thaumcraft is not a hard dependency

--- src/main/java/com/bewitchment/common/item/tool/ItemColdIronSword.java
+++ src/main/java/com/bewitchment/common/item/tool/ItemColdIronSword.java
@@ -1,11 +1,14 @@
 package com.bewitchment.common.item.tool;
 
+import javax.annotation.Nonnull;
+
 import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.client.core.IModelRegister;
 import com.bewitchment.client.handler.ModelHandler;
 import com.bewitchment.common.core.ModCreativeTabs;
 import com.bewitchment.common.item.ModMaterials;
 import com.bewitchment.common.lib.LibItemName;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityWither;
 import net.minecraft.entity.monster.*;
@@ -15,9 +18,6 @@ import net.minecraft.item.ItemSword;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import thaumcraft.common.entities.monster.*;
-
-import javax.annotation.Nonnull;
 
 /**
  * Created by Joseph on 6/14/2018.
@@ -32,11 +32,10 @@ public class ItemColdIronSword extends ItemSword implements IModelRegister {
 		setCreativeTab(ModCreativeTabs.ITEMS_CREATIVE_TAB);
 	}
 
-	//FIXME: Better way of handling thaumcraft mobs
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, @Nonnull EntityLivingBase attacker) {
 		if (!target.world.isRemote) {
-			if (target.getCreatureAttribute() == BewitchmentAPI.getAPI().DEMON || target.getCreatureAttribute() == BewitchmentAPI.getAPI().SPIRIT || target instanceof EntityEnderman || target instanceof EntityBlaze || target instanceof EntityGhast || target instanceof EntityWither || target instanceof EntityGuardian || target instanceof EntityEldritchGuardian || target instanceof EntityVex || target instanceof EntityEndermite || target instanceof EntityEldritchCrab || target instanceof EntityFireBat || target instanceof EntityWisp || target instanceof EntityPech || target instanceof EntityMindSpider && attacker instanceof EntityPlayer) {
+			if (target.getCreatureAttribute() == BewitchmentAPI.getAPI().DEMON || target.getCreatureAttribute() == BewitchmentAPI.getAPI().SPIRIT || target instanceof EntityEnderman || target instanceof EntityBlaze || target instanceof EntityGhast || target instanceof EntityWither || target instanceof EntityGuardian || /* target instanceof EntityEldritchGuardian || target instanceof EntityVex || target instanceof EntityEndermite || target instanceof EntityEldritchCrab || target instanceof EntityFireBat || target instanceof EntityWisp || target instanceof EntityPech || target instanceof EntityMindSpider && */ attacker instanceof EntityPlayer) {
 				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), 12);
 				stack.damageItem(5, attacker);
 			} else {
