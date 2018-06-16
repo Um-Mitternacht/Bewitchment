a67173211267806531a3ac7c94a9908c81b6d2ad
zabi94 <zabi94@gmail.com>
2018-06-16 19:48:05 +0200

    these are not hard dependencies

--- src/main/java/com/bewitchment/common/lib/LibMod.java
+++ src/main/java/com/bewitchment/common/lib/LibMod.java
@@ -17,7 +17,7 @@ public final class LibMod {
 	public static final String MOD_VER = "@VERSION@";
 
 	//Dependency
-	public static final String DEPENDENCIES = "required-after:forge@[14.23.3.2655,];required-after:JEI@[4.9.1.168,];required-after:Waila@[1.8.24-B39_1.12,];required-after:baubles@[1.5.2,];required-after:craftstudioapi@[0.3.0,]";
+	public static final String DEPENDENCIES = "required-after:forge@[14.23.3.2655,];after:JEI@[4.9.1.168,];after:Waila@[1.8.24-B39_1.12,];required-after:baubles@[1.5.2,];required-after:craftstudioapi@[0.3.0,]";
 
 	//Client proxy location
 	public static final String PROXY_CLIENT = "com.bewitchment.client.core.ClientProxy";
