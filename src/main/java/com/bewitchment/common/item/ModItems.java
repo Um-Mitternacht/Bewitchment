a1326e3b57ff8c02e9d0f7b5b57f72514e08396f
Sunconure11 <eyespot7b@aol.com>
2018-06-18 10:17:38 -0400

    Cold iron axe
    Crashes though, someone please fix

--- src/main/java/com/bewitchment/common/item/ModItems.java
+++ src/main/java/com/bewitchment/common/item/ModItems.java
@@ -272,7 +272,8 @@ public final class ModItems {
 				new ItemSilverArmor(LibItemName.SILVER_CHESTPLATE, ModMaterials.ARMOR_SILVER, 1, EntityEquipmentSlot.CHEST),
 				new ItemSilverArmor(LibItemName.SILVER_LEGGINGS, ModMaterials.ARMOR_SILVER, 2, EntityEquipmentSlot.LEGS),
 				new ItemSilverArmor(LibItemName.SILVER_BOOTS, ModMaterials.ARMOR_SILVER, 1, EntityEquipmentSlot.FEET),
-				new ItemColdIronSword()
+				new ItemColdIronSword(),
+				new ItemColdIronAxe()
 		);
 		// Item Blocks
 		registry.registerAll(
