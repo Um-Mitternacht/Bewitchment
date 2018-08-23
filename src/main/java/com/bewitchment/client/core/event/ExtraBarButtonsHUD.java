998b01fc7f2c2bedd79016558a501594642bfabf
zabi94 <zabi94@gmail.com>
2018-08-23 16:13:30 +0200

    Reduce cooldown

--- src/main/java/com/bewitchment/client/core/event/ExtraBarButtonsHUD.java
+++ src/main/java/com/bewitchment/client/core/event/ExtraBarButtonsHUD.java
@@ -1,11 +1,18 @@
 package com.bewitchment.client.core.event;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.GL11;
+
 import com.bewitchment.api.hotbar.IHotbarAction;
 import com.bewitchment.client.handler.Keybinds;
 import com.bewitchment.common.core.handler.ConfigHandler;
 import com.bewitchment.common.core.hotbar.HotbarAction;
 import com.bewitchment.common.core.net.NetworkHandler;
 import com.bewitchment.common.core.net.messages.PlayerUsedAbilityMessage;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.BufferBuilder;
@@ -19,11 +26,6 @@ import net.minecraftforge.fml.common.gameevent.InputEvent.KeyInputEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.input.Keyboard;
-import org.lwjgl.opengl.GL11;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SideOnly(Side.CLIENT)
 public class ExtraBarButtonsHUD {
@@ -67,7 +69,7 @@ public class ExtraBarButtonsHUD {
 					e_id = Minecraft.getMinecraft().pointedEntity.getEntityId();
 				}
 				NetworkHandler.HANDLER.sendToServer(new PlayerUsedAbilityMessage(actions.get(slotSelected).getName().toString(), e_id));
-				cooldown = 15;
+				cooldown = 5;
 			}
 		}
 	}
