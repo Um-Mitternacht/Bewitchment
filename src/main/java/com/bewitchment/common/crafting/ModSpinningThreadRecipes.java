63135f0dd95132d08fb478e3810025c48a2f5e80
Sunconure11 <eyespot7b@aol.com>
2018-08-24 21:43:11 -0400

    Whoops

--- src/main/java/com/bewitchment/common/crafting/ModSpinningThreadRecipes.java
+++ src/main/java/com/bewitchment/common/crafting/ModSpinningThreadRecipes.java
@@ -28,6 +28,7 @@ public class ModSpinningThreadRecipes {
 				new SpinningThreadRecipe(rl("witches_stitching"), new ItemStack(ModItems.witches_stitching, 4), string, string, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 2)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 2))),
 				new SpinningThreadRecipe(rl("diabolic_vein"), new ItemStack(ModItems.diabolic_vein, 4), soul_string, soul_string, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 8)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 13))),
 				new SpinningThreadRecipe(rl("pure_filament"), new ItemStack(ModItems.pure_filament, 4), stitching, stitching, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 7)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 7))),
+				new SpinningThreadRecipe(rl("soulstring"), new ItemStack(ModItems.soul_string, 2), stitching, stitching, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 10)), ectoplasm),
 				new SpinningThreadRecipe(rl("spanish_moss_stuffing"), new ItemStack(ModItems.spanish_moss, 4))
 		);
 	}
