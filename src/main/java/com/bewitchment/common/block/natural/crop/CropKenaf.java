96fa1411f15616ba9f109d21944afade7a496140
Sunconure11 <eyespot7b@aol.com>
2018-09-05 12:46:12 -0400

    Made most crops flammable
    Fire can now take out some of the weedier crops, but it will not be the only way.

--- src/main/java/com/bewitchment/common/block/natural/crop/CropKenaf.java
+++ src/main/java/com/bewitchment/common/block/natural/crop/CropKenaf.java
@@ -4,6 +4,7 @@ import com.bewitchment.common.lib.LibBlockName;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
@@ -58,6 +59,11 @@ public class CropKenaf extends BlockCrop {
 	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
 		IBlockState state = worldIn.getBlockState(pos.down());
 		return canSustainBush(state);
+	}
+
+	@Override
+	public boolean isFlammable(IBlockAccess world, BlockPos pos, EnumFacing face) {
+		return true;
 	}
 
 	@Override
