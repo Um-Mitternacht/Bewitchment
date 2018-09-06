e6b8d7586cde1edcb49f05006a5a792afddf8f8f
Sunconure11 <eyespot7b@aol.com>
2018-09-05 23:42:26 -0400

    Whoops

--- src/main/java/com/bewitchment/common/integration/thaumcraft/ThaumcraftCompat.java
+++ src/main/java/com/bewitchment/common/integration/thaumcraft/ThaumcraftCompat.java
@@ -231,7 +231,7 @@ public class ThaumcraftCompat {
 		//Saplings
 		evt.register.registerObjectTag(new ItemStack(ModBlocks.sapling, 1, 0), new AspectList().add(Aspect.PLANT, 15).add(Aspect.LIFE, 5).add(Aspect.MIND, 3));
 		evt.register.registerObjectTag(new ItemStack(ModBlocks.sapling, 1, 1), new AspectList().add(Aspect.PLANT, 15).add(Aspect.LIFE, 5).add(Aspect.MAGIC, 3));
-		evt.register.registerObjectTag(new ItemStack(ModBlocks.sapling, 1, 2), new AspectList().add(Aspect.PLANT, 15).add(Aspect.LIFE, 5).add(Aspect.EXCHANGE, 5));
+		evt.register.registerObjectTag(new ItemStack(ModBlocks.sapling, 1, 2), new AspectList().add(Aspect.PLANT, 15).add(Aspect.LIFE, 5).add(Aspect.EXCHANGE, 3));
 		evt.register.registerObjectTag(new ItemStack(ModBlocks.sapling, 1, 3), new AspectList().add(Aspect.PLANT, 15).add(Aspect.LIFE, 5).add(Aspect.DEATH, 3));
 
 		//Add some of our aspects to existing items in vanilla
