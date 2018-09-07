4c86586b3d5b8267e34e3a5659320b7f988a5ee0
zabi94 <zabi94@gmail.com>
2018-09-07 12:19:54 +0200

    Bat swarm particles

--- src/main/java/com/bewitchment/client/render/entity/renderer/RenderBatSwarm.java
+++ src/main/java/com/bewitchment/client/render/entity/renderer/RenderBatSwarm.java
@@ -1,10 +1,16 @@
 package com.bewitchment.client.render.entity.renderer;
 
+import java.util.Random;
+
+import com.bewitchment.client.fx.ParticleF;
+import com.bewitchment.common.Bewitchment;
 import com.bewitchment.common.entity.EntityBatSwarm;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
@@ -12,6 +18,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class RenderBatSwarm extends Render<EntityBatSwarm> {
 
+	private static Random rng = new Random();
+	
 	public RenderBatSwarm(RenderManager renderManager) {
 		super(renderManager);
 	}
@@ -20,16 +28,28 @@ public class RenderBatSwarm extends Render<EntityBatSwarm> {
 	protected ResourceLocation getEntityTexture(EntityBatSwarm entity) {
 		return null;
 	}
-
+	
 	@Override
 	public void doRender(EntityBatSwarm entity, double x, double y, double z, float entityYaw, float partialTicks) {
-		// TODO
+		for (int i = 0; i < 4; i++) {
+			double ipx = entity.prevPosX + (entity.posX - entity.prevPosX) * partialTicks;
+			double ipy = entity.prevPosY + (entity.posY - entity.prevPosY) * partialTicks;
+			double ipz = entity.prevPosZ + (entity.posZ - entity.prevPosZ) * partialTicks;
+			ipx += rng.nextGaussian() * 0.3;
+			ipy += rng.nextGaussian() * 0.3;
+			ipz += rng.nextGaussian() * 0.3;
+			if (rng.nextBoolean()) {
+				entity.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, ipx, ipy, ipz, 0, 0, 0);
+			} else {
+				Bewitchment.proxy.spawnParticle(ParticleF.BAT, ipx, ipy, ipz, 0, 0, 0);
+			}
+		}
 	}
-
+	
 	@Override
 	public void doRenderShadowAndFire(Entity entityIn, double x, double y, double z, float yaw, float partialTicks) {
 	}
-
+	
 	public static class PlayerHider {
 
 		@SubscribeEvent
