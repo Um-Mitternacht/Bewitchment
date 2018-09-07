17dc31aa762142bf2be6eea9a3ec58f8d2d39207
Ingoleth <aiglesia97@gmail.com>
2018-09-07 22:25:21 +0200

    Broom broom!
    Broom animations!
    Broom will float when unmounted, will rotate slightly when so

--- src/main/java/com/bewitchment/client/render/entity/model/ModelBroom.java
+++ src/main/java/com/bewitchment/client/render/entity/model/ModelBroom.java
@@ -1,9 +1,13 @@
 package com.bewitchment.client.render.entity.model;
 
 import net.minecraft.client.model.ModelBase;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.util.math.MathHelper;
 
 /**
  * ModelBroom - zabi94
@@ -21,32 +25,42 @@ public class ModelBroom extends ModelBase {
 		this.handle.setRotationPoint(-13.0F, 16.0F, 0.0F);
 		this.handle.addBox(0.0F, 0.0F, 0.0F, 16, 1, 1, 0.4F);
 		this.twigs_bound = new ModelRenderer(this, 0, 2);
-		this.twigs_bound.setRotationPoint(8.0F, 15.0F, -1.0F);
+		this.twigs_bound.setRotationPoint(17.0F, -1.0F, -1.0F);
 		this.twigs_bound.addBox(0.0F, 0.0F, 0.0F, 1, 3, 3, 0.5F);
 		this.twigs = new ModelRenderer(this, 39, 0);
-		this.twigs.setRotationPoint(9.7F, 14.0F, -2.0F);
+		this.twigs.setRotationPoint(18F, -2.0F, -2.0F);
 		this.twigs.addBox(0.0F, 0.0F, 0.0F, 6, 5, 5, 0.5F);
+		
+		this.handle.addChild(twigs_bound);
+		this.handle.addChild(twigs);
 	}
+	
+	float time1;
+	float time2;
 
 	@Override
-	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+	public void render(Entity entity, float limbSwingAmount, float f1, float f2, float f3, float f4, float f5) {
 		GlStateManager.pushMatrix();
-		GlStateManager.translate(this.handle.offsetX, this.handle.offsetY, this.handle.offsetZ);
-		GlStateManager.translate(this.handle.rotationPointX * f5, this.handle.rotationPointY * f5, this.handle.rotationPointZ * f5);
-		GlStateManager.scale(1.3D, 1.0D, 1.0D);
-		GlStateManager.translate(-this.handle.offsetX, -this.handle.offsetY, -this.handle.offsetZ);
-		GlStateManager.translate(-this.handle.rotationPointX * f5, -this.handle.rotationPointY * f5, -this.handle.rotationPointZ * f5);
-		this.handle.render(f5);
-		GlStateManager.popMatrix();
-		this.twigs_bound.render(f5);
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(this.twigs.offsetX, this.twigs.offsetY, this.twigs.offsetZ);
-		GlStateManager.translate(this.twigs.rotationPointX * f5, this.twigs.rotationPointY * f5, this.twigs.rotationPointZ * f5);
-		GlStateManager.scale(1.2D, 1.0D, 1.0D);
-		GlStateManager.translate(-this.twigs.offsetX, -this.twigs.offsetY, -this.twigs.offsetZ);
-		GlStateManager.translate(-this.twigs.rotationPointX * f5, -this.twigs.rotationPointY * f5, -this.twigs.rotationPointZ * f5);
-		this.twigs.render(f5);
-		GlStateManager.popMatrix();
+        GlStateManager.translate(this.handle.offsetX, this.handle.offsetY, this.handle.offsetZ);
+        GlStateManager.translate(this.handle.rotationPointX * f5, this.handle.rotationPointY * f5, this.handle.rotationPointZ * f5);
+        GlStateManager.scale(1.2D, 1.0D, 1.0D);
+        GlStateManager.translate(-this.handle.offsetX, -this.handle.offsetY, -this.handle.offsetZ);
+        GlStateManager.translate(-this.handle.rotationPointX * f5, -this.handle.rotationPointY * f5, -this.handle.rotationPointZ * f5);
+        this.handle.render(f5);
+        GlStateManager.popMatrix();
+        
+        time1 = MathHelper.sin(entity.ticksExisted*0.10471975512F);
+        time2 = MathHelper.cos(entity.ticksExisted*0.10471975512F);
+
+        if (entity.getPassengers().isEmpty()) {
+        	handle.setRotationPoint(-13.0F + 0.2F*time1, 16F + 0.3F*time1, 0.2F*time2);
+        	handle.rotateAngleX = (time1*0.05235987755F - limbSwingAmount*time1*0.17453292516F);
+        	handle.rotateAngleY = time2*0.05235987755F - limbSwingAmount*time1*0.17453292516F;
+        }else{
+        	handle.setRotationPoint(-13.0F + 0.02F*time1, 16F + 0.03F*time1, 0.02F*time2);
+        	handle.rotateAngleX = time1*0.01745329251F;
+        	handle.rotateAngleY = time2*0.01745329251F;
+        }
 	}
 
 }
