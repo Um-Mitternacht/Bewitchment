a9c110cad0293a4915019d55ed6f45104e7ebbf9
ZaBi94 <zabi94@gmail.com>
2018-09-17 12:56:43 +0200

    Only render mantle when worn

--- src/main/java/com/bewitchment/client/render/entity/layer/MantleLayer.java
+++ src/main/java/com/bewitchment/client/render/entity/layer/MantleLayer.java
@@ -1,12 +1,17 @@
 package com.bewitchment.client.render.entity.layer;
 
-import baubles.api.render.IRenderBauble;
+import org.lwjgl.opengl.GL11;
+
 import com.bewitchment.client.render.entity.model.ModelMantle;
+import com.bewitchment.common.item.ModItems;
+
+import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
+import baubles.api.render.IRenderBauble;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class MantleLayer implements LayerRenderer<AbstractClientPlayer> {
@@ -24,11 +29,13 @@ public class MantleLayer implements LayerRenderer<AbstractClientPlayer> {
 
 	@Override
 	public void doRenderLayer(AbstractClientPlayer player, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-		GL11.glPushMatrix();
-		IRenderBauble.Helper.rotateIfSneaking(player); //Not being a bauble renderer, I'm not sure this works anymore
-		GL11.glScaled(0.07, 0.07, 0.07);
-		model.render(player, player.limbSwing, player.limbSwingAmount, ageInTicks + partialTicks, player.rotationYaw, player.rotationPitch, scale);
-		GL11.glPopMatrix();
+		if (BaublesApi.getBaublesHandler(player).getStackInSlot(BaubleType.HEAD.getValidSlots()[0]).getItem()==ModItems.mantle) {
+			GL11.glPushMatrix();
+			IRenderBauble.Helper.rotateIfSneaking(player); //Not being a bauble renderer, I'm not sure this works anymore
+			GL11.glScaled(0.07, 0.07, 0.07);
+			model.render(player, player.limbSwing, player.limbSwingAmount, ageInTicks + partialTicks, player.rotationYaw, player.rotationPitch, scale);
+			GL11.glPopMatrix();
+		}
 	}
 
 	@Override
