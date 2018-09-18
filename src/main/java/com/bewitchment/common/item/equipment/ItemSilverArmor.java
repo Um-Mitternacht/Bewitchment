3e38824650b41d292c80859036a7f925d941ff5e
ZaBi94 <zabi94@gmail.com>
2018-09-18 19:36:26 +0200

    Silver armor hurts the player when worn

--- src/main/java/com/bewitchment/common/item/equipment/ItemSilverArmor.java
+++ src/main/java/com/bewitchment/common/item/equipment/ItemSilverArmor.java
@@ -12,8 +12,10 @@ import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -50,7 +52,14 @@ public class ItemSilverArmor extends ItemArmor implements IModelRegister {
 			a.attackEntityFrom(DamageSource.causeThornsDamage(event.getEntityLiving()), MathHelper.clamp(event.getAmount()/2, 1f, 4f));
 		}
 	}
-	
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer player, ItemStack itemStack) {
+		if (player.getCapability(CapabilityTransformationData.CAPABILITY, null).getType()== DefaultTransformations.WEREWOLF) {
+			player.attackEntityFrom(DamageSource.MAGIC, 1);
+		}
+	}
+
 	@Override
 	@SideOnly(Side.CLIENT)
 	public void registerModel() {
