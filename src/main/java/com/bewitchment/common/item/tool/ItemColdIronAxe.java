1cd891b2ea4401c4ef0ad7cbbbd828df9101cb56
ZaBi94 <zabi94@gmail.com>
2018-09-21 11:57:39 +0200

    Remove Astral Sorcery Integration base (Not implemented)
    Allows me to code more easily by not having a really heavy mod forced in my dev env, slowing down hings immensly

--- src/main/java/com/bewitchment/common/item/tool/ItemColdIronAxe.java
+++ src/main/java/com/bewitchment/common/item/tool/ItemColdIronAxe.java
@@ -1,24 +1,29 @@
 package com.bewitchment.common.item.tool;
 
+import javax.annotation.Nonnull;
+
 import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.client.core.IModelRegister;
 import com.bewitchment.client.handler.ModelHandler;
 import com.bewitchment.common.core.ModCreativeTabs;
-import com.bewitchment.common.integration.astralsorcery.AstralSorceryCompatBridge;
 import com.bewitchment.common.integration.thaumcraft.ThaumcraftCompatBridge;
 import com.bewitchment.common.item.ModMaterials;
 import com.bewitchment.common.lib.LibItemName;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.boss.EntityWither;
-import net.minecraft.entity.monster.*;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.monster.EntityEnderman;
+import net.minecraft.entity.monster.EntityEndermite;
+import net.minecraft.entity.monster.EntityGhast;
+import net.minecraft.entity.monster.EntityGuardian;
+import net.minecraft.entity.monster.EntityVex;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemAxe;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-
-import javax.annotation.Nonnull;
 
 /**
  * Created by Joseph on 6/14/2018.
@@ -36,7 +41,7 @@ public class ItemColdIronAxe extends ItemAxe implements IModelRegister {
 	@Override
 	public boolean hitEntity(ItemStack stack, EntityLivingBase target, @Nonnull EntityLivingBase attacker) {
 		if (!target.world.isRemote) {
-			if ((target.getCreatureAttribute() == BewitchmentAPI.getAPI().DEMON || target.getCreatureAttribute() == BewitchmentAPI.getAPI().SPIRIT || target instanceof EntityEnderman || target instanceof EntityBlaze || target instanceof EntityVex || target instanceof EntityEndermite || target instanceof EntityGhast || target instanceof EntityWither || target instanceof EntityGuardian || ThaumcraftCompatBridge.isTCSpiritMob(target)) || AstralSorceryCompatBridge.isStellarMob(target) && attacker instanceof EntityPlayer) {
+			if ((target.getCreatureAttribute() == BewitchmentAPI.getAPI().DEMON || target.getCreatureAttribute() == BewitchmentAPI.getAPI().SPIRIT || target instanceof EntityEnderman || target instanceof EntityBlaze || target instanceof EntityVex || target instanceof EntityEndermite || target instanceof EntityGhast || target instanceof EntityWither || target instanceof EntityGuardian || ThaumcraftCompatBridge.isTCSpiritMob(target)) && attacker instanceof EntityPlayer) {
 				target.attackEntityFrom(DamageSource.causePlayerDamage((EntityPlayer) attacker), 12);
 				stack.damageItem(5, attacker);
 			} else {
