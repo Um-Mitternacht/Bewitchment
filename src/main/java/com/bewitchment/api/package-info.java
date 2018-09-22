882d34fd5ccf8421eef0f85f7eb035da3fae86c6
Sunconure11 <eyespot7b@aol.com>
2018-09-22 17:30:53 -0400

    Prepping for next test build of main branch

--- src/main/java/com/bewitchment/api/package-info.java
+++ src/main/java/com/bewitchment/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = com.bewitchment.common.lib.LibMod.MOD_ID, apiVersion = "0.0.11", provides = "BewitchmentAPI")
+@API(owner = com.bewitchment.common.lib.LibMod.MOD_ID, apiVersion = "0.0.12", provides = "BewitchmentAPI")
 package com.bewitchment.api;
 
 import net.minecraftforge.fml.common.API;
