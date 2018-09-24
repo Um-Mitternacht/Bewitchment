aa49629e7f8284adc4c7979a257e8f0bdbbdc561
Ingoleth <aiglesia97@gmail.com>
2018-09-24 15:55:09 +0200

    Witches Pouch

--- src/main/java/com/bewitchment/common/item/ModItems.java
+++ src/main/java/com/bewitchment/common/item/ModItems.java
@@ -169,6 +169,7 @@ public final class ModItems {
 	public static final Item girdle_of_the_wooded = null;
 	public static final Item wrathful_eye = null;
 	public static final Item mantle = null;
+	public static final Item pouch = null;
 
 	public static final Item magic_salve = null;
 
@@ -274,7 +275,8 @@ public final class ModItems {
 				new ItemTalisman(BaubleType.AMULET, 18, LibItemName.TALISMAN_EMERALD_PENDANT),
 				new ItemTalisman(BaubleType.BELT, 30, LibItemName.TALISMAN_RUBY_ORB),
 				new ItemTalisman(BaubleType.CHARM, 18, LibItemName.TALISMAN_WATCHING_EYE),
-				new ItemGirdleOfTheWooded(LibItemName.GIRDLE_OF_THE_WOODED), new ItemMantle(LibItemName.MANTLE)
+				new ItemGirdleOfTheWooded(LibItemName.GIRDLE_OF_THE_WOODED), 
+				new ItemMantle(LibItemName.MANTLE), new ItemPouch(LibItemName.POUCH)
 		);
 
 		//Equipment
--- src/main/java/com/bewitchment/common/lib/LibItemName.java
+++ src/main/java/com/bewitchment/common/lib/LibItemName.java
@@ -134,6 +134,7 @@ public final class LibItemName {
 	public static final String TAROTS = "tarots";
 	public static final String GIRDLE_OF_THE_WOODED = "girdle_of_the_wooded";
 	public static final String MANTLE = "mantle";
+	public static final String POUCH = "pouch";
 	public static final String BROOM = "broom";
 	public static final String EMPTY_BREW_DRINK = "empty_brew_drink";
 	public static final String EMPTY_BREW_SPLASH = "empty_brew_splash";
