3364de710a4f077abe0ab4dcd8a58badfc56c661
Sunconure11 <eyespot7b@aol.com>
2018-10-09 12:37:47 -0400

    Only doing this because of a hurricane

--- src/main/java/com/bewitchment/api/package-info.java
+++ src/main/java/com/bewitchment/api/package-info.java
@@ -1,4 +1,4 @@
-@API(owner = com.bewitchment.common.lib.LibMod.MOD_ID, apiVersion = "0.0.12", provides = "BewitchmentAPI")
+@API(owner = com.bewitchment.common.lib.LibMod.MOD_ID, apiVersion = "0.0.13", provides = "BewitchmentAPI")
 package com.bewitchment.api;
 
 import net.minecraftforge.fml.common.API;
