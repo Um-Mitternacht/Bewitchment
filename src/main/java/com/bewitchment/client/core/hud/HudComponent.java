bba7f4e8c06eb21dd07bf659dc732b461205da2e
ZaBi94 <zabi94@gmail.com>
2018-10-14 13:22:58 +0200

    Javadoc for hud components

--- src/main/java/com/bewitchment/client/core/hud/HudComponent.java
+++ src/main/java/com/bewitchment/client/core/hud/HudComponent.java
@@ -10,10 +10,20 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SideOnly(Side.CLIENT)
+/**
+ * @author zabi94
+ */
 public abstract class HudComponent {
 	
 	protected int w, h;
 	
+	/**
+	 * This class represents hud components for the mod.
+	 * A single instance should be created per hud component
+	 * 
+	 * All components should be registered once during startup on the client side via HudController.registerNewComponent(HudComponent)
+	 * @see HudController#registerNewComponent(HudComponent)
+	 */
 	protected HudComponent(int width, int height) {
 		this.w = width;
 		this.h = height;
@@ -27,17 +37,72 @@ public abstract class HudComponent {
 		return h;
 	}
 
+	/**
+	 *  This method should read from a permanent client data storage, eg: configs
+	 */
 	public abstract boolean isActive();
+	
+	/**
+	 *  This method should write to a permanent client data storage, eg: configs
+	 */
 	public abstract void setHidden(boolean hidden);
+	
+	/**
+	 *  This method should read from a permanent client data storage, eg: configs
+	 */
 	public abstract double getX();
+	
+	/**
+	 *  This method should read from a permanent client data storage, eg: configs
+	 */
 	public abstract double getY();
+	
+	/**
+	 *  This method should write to a permanent client data storage, eg: configs
+	 */
 	public abstract void setRelativePosition(double x, double y, EnumHudAnchor horizontal, EnumHudAnchor vertical);
+	
+	/**
+	 *  This method should write to a permanent client data storage, eg: configs
+	 */
 	public abstract void resetConfig();	
-	public abstract String getTooltip(int mouseX, int mouseY);
-	public abstract void onClick(int mouseX, int mouseY);
-	public abstract void render(ScaledResolution resolution, float partialTicks, boolean renderDummy);
+	
+	/**
+	 *  This method should read from a permanent client data storage, eg: configs
+	 */
 	public abstract EnumHudAnchor getAnchorHorizontal();
+	
+	/**
+	 *  This method should read from a permanent client data storage, eg: configs
+	 */
 	public abstract EnumHudAnchor getAnchorVertical();
+	
+	/**
+	 *  This only gets called when the cursor is hovering on the hud element
+	 *  @param mouseX is given only to provide a way to give different tooltips depending on the cursor position inside the hud
+	 *  @param mouseY is given only to provide a way to give different tooltips depending on the cursor position inside the hud
+	 *  
+	 *  @return A string to be displayed as tooltip, or a null or empty string to not show anything
+	 */
+	public abstract String getTooltip(int mouseX, int mouseY);
+	
+	/**
+	 *  This only gets called when the cursor is hovering on the hud element and a mouse button is pressed
+	 *  @param mouseX is given only to provide a way to act differently depending on the cursor position inside the hud
+	 *  @param mouseY is given only to provide a way to act differently depending on the cursor position inside the hud
+	 */
+	public abstract void onClick(int mouseX, int mouseY);
+	
+	/**
+	 * Actually do the rendering on the screen. Anything drawn should always be inside the rectangle described by
+	 * {@link #getX()}, {@link #getY()}, {@link #getX()}+{@link #getWidth()}, {@link #getY()}+{@link #getHeight()}.
+	 * <br><br>If the element is not active when using {@link #isActive()}, this method won't be called at all, except
+	 * for dummy renders
+	 * @param resolution The current resolution of the minecraft window
+	 * @param partialTicks A float representing half ticks progress
+	 * @param renderDummy if true, the hud should display sample data that causes it to be visible, and not real data. This is used by the bwguicfg command to show all huds even when they are not active
+	 */
+	public abstract void render(ScaledResolution resolution, float partialTicks, boolean renderDummy);
 	
 	protected static void renderTextureAt(double x, double y, int w, int h) {
 		Tessellator tessellator = Tessellator.getInstance();
