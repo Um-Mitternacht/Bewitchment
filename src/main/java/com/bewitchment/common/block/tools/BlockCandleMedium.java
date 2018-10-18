0287dfb5cd292fe45fea2ce77a829b1ac4de2e8c
ZaBi94 <zabi94@gmail.com>
2018-10-18 10:49:08 +0200

    Moved TC method to superclass

--- src/main/java/com/bewitchment/common/block/tools/BlockCandleMedium.java
+++ src/main/java/com/bewitchment/common/block/tools/BlockCandleMedium.java
@@ -9,17 +9,13 @@ import net.minecraft.item.Item;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraft.world.World;
-import net.minecraftforge.fml.common.Optional;
-import thaumcraft.api.crafting.IInfusionStabiliserExt;
 
 /**
  * This class was created by Joseph on 3/4/2017.
  * It's distributed as part of Bewitchment under
  * the MIT license.
  */
-@Optional.Interface(iface = "thaumcraft.api.crafting.IInfusionStabiliserExt", modid = "thaumcraft")
-public class BlockCandleMedium extends BlockCandle implements IInfusionStabiliserExt {
+public class BlockCandleMedium extends BlockCandle {
 
 	private static final AxisAlignedBB MEDIUM_BOX = new AxisAlignedBB(0.31, 0, 0.31, 0.69, 0.75, 0.69);
 
@@ -41,17 +37,5 @@ public class BlockCandleMedium extends BlockCandle implements IInfusionStabilise
 	@Override
 	public int getType() {
 		return 1;
-	}
-
-	@Override
-	@Optional.Method(modid = "thaumcraft")
-	public boolean canStabaliseInfusion(World world, BlockPos pos) {
-		return true;
-	}
-	
-	@Override
-	@Optional.Method(modid = "thaumcraft")
-	public float getStabilizationAmount(World world, BlockPos pos) {
-		return 0;
 	}
 }
