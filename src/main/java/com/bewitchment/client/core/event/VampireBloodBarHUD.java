63f64cd8999f93d268619b8ccae7eda1e249706c
ZaBi94 <zabi94@gmail.com>
2018-10-24 14:47:02 +0200

    Discard current blood meter, the texture is bad

--- src/main/java/com/bewitchment/client/core/event/VampireBloodBarHUD.java
+++ src/main/java/com/bewitchment/client/core/event/VampireBloodBarHUD.java
@@ -4,18 +4,17 @@ import com.bewitchment.api.transformation.DefaultTransformations;
 import com.bewitchment.common.content.transformation.capability.CapabilityTransformationData;
 import com.bewitchment.common.content.transformation.capability.ITransformationData;
 import com.bewitchment.common.lib.LibMod;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
-import org.lwjgl.opengl.GL11;
 
 @SideOnly(Side.CLIENT)
 public class VampireBloodBarHUD {
@@ -25,34 +24,8 @@ public class VampireBloodBarHUD {
 	protected static void renderTextureAt(double x, double y, int w, int h, ITransformationData td) {
 		Tessellator tessellator = Tessellator.getInstance();
 		BufferBuilder buf = tessellator.getBuffer();
-
-		buf.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-
-		double maxBlood = 0.1d * td.getLevel();
-		int dx = (int) (maxBlood * w);
-
-		buf.pos(x + dx, y + h, 0).tex(maxBlood, 0.75).endVertex();
-		buf.pos(x + w, y + h, 0).tex(1, 0.75).endVertex();
-		buf.pos(x + w, y, 0).tex(1, 0.5).endVertex();
-		buf.pos(x + dx, y, 0).tex(maxBlood, 0.5).endVertex();
-
-		double bloodpc = maxBlood * td.getBlood() / td.getMaxBlood();
-		double currentBlood = w * bloodpc;
-
-		buf.pos(x, y + h, 0).tex(0, 0.5).endVertex();
-		buf.pos(x + currentBlood, y + h, 0).tex(bloodpc, 0.5).endVertex();
-		buf.pos(x + currentBlood, y, 0).tex(bloodpc, 0.25).endVertex();
-		buf.pos(x, y, 0).tex(0, 0.25).endVertex();
-
-		buf.pos(x, y + h, 0).tex(0, 0.25).endVertex();
-		buf.pos(x + w, y + h, 0).tex(1, 0.25).endVertex();
-		buf.pos(x + w, y, 0).tex(1, 0).endVertex();
-		buf.pos(x, y, 0).tex(0, 0).endVertex();
-
-		buf.pos(x - 10, y + 9, 0).tex(0, 1).endVertex();
-		buf.pos(x - 3, y + 9, 0).tex(7f / 61f, 1).endVertex();
-		buf.pos(x - 3, y, 0).tex(7f / 61f, 0.75).endVertex();
-		buf.pos(x - 10, y, 0).tex(0, 0.75).endVertex();
+		
+		buf.getClass();//TODO RENDER HERE
 
 		tessellator.draw();
 	}
