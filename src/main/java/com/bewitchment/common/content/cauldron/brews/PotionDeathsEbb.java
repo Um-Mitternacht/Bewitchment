45ed8556098a5cb6fe7fb16f3919c0fd66c3f324
ZaBi94 <zabi94@gmail.com>
2018-10-27 08:43:49 +0200

    Fix death's ebb brew

--- src/main/java/com/bewitchment/common/content/cauldron/brews/PotionDeathsEbb.java
+++ src/main/java/com/bewitchment/common/content/cauldron/brews/PotionDeathsEbb.java
@@ -1,8 +1,13 @@
 package com.bewitchment.common.content.cauldron.brews;
 
+import com.bewitchment.api.cauldron.DefaultModifiers;
+import com.bewitchment.api.cauldron.IBrewModifierList;
 import com.bewitchment.common.content.cauldron.BrewMod;
+
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
@@ -13,15 +18,37 @@ public class PotionDeathsEbb extends BrewMod {
 		this.setIconIndex(5, 1);
 	}
 
-	//Fixme: Why isn't this working?
 	@Override
-	public void performEffect(EntityLivingBase entity, int amplifier) {
-		World world = entity.world;
+	public void applyInWorld(World world, BlockPos pos, EnumFacing side, IBrewModifierList modifiers, EntityLivingBase thrower) {
 		if (!world.isRemote) {
-			BlockPos pos = entity.getPosition();
-			EntityZombie zombie = new EntityZombie(world);
-			zombie.setPosition(pos.getX(), pos.getY(), pos.getZ());
-			world.spawnEntity(zombie);
+			int zombies = 1 + modifiers.getLevel(DefaultModifiers.POWER).orElse(0);
+			for (int i = 0; i < zombies; i++) {
+				EntityZombie zombie = new EntityZombie(world);
+				zombie.setPosition(pos.getX(), pos.getY(), pos.getZ());
+				world.spawnEntity(zombie);
+				setZombieAI(zombie);
+			}
 		}
+	}
+	
+	@Override
+	public void affectEntity(Entity source, Entity indirectSource, EntityLivingBase elb, int amplifier, double health) {
+		if (elb instanceof EntityZombie) {
+			setZombieAI(elb);
+		} else {
+			if (!elb.world.isRemote) {
+				int zombies = 1 + amplifier;
+				for (int i = 0; i < zombies; i++) {
+					EntityZombie zombie = new EntityZombie(elb.world);
+					zombie.setPosition(elb.posX, elb.posY, elb.posZ);
+					elb.world.spawnEntity(zombie);
+					setZombieAI(zombie);
+				}
+			}
+		}
+	}
+	
+	private void setZombieAI(EntityLivingBase elb) {
+		//TODO
 	}
 }
