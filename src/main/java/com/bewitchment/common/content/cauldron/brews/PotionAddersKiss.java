540c586fc1ae73892723872238d0e03cac94f318
Sunconure11 <eyespot7b@aol.com>
2018-10-30 13:33:28 -0400

    Whoops
    Adder's Kiss now uses the same list as snake prey

--- src/main/java/com/bewitchment/common/content/cauldron/brews/PotionAddersKiss.java
+++ src/main/java/com/bewitchment/common/content/cauldron/brews/PotionAddersKiss.java
@@ -2,6 +2,9 @@ package com.bewitchment.common.content.cauldron.brews;
 
 import com.bewitchment.common.core.helper.MobHelper;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.monster.EntitySpider;
+import net.minecraft.entity.passive.EntityChicken;
+import net.minecraft.entity.passive.EntityRabbit;
 
 //Todo: Make snake venom return glass bottles after being used in the cauldron
 public class PotionAddersKiss extends GenericBrewDamageVS {
@@ -12,7 +15,8 @@ public class PotionAddersKiss extends GenericBrewDamageVS {
 
 	@Override
 	protected boolean shouldAffect(EntityLivingBase entity) {
-		return MobHelper.isSnakeFodder(entity);
+		String className = entity.getClass().getSimpleName();
+		return entity instanceof EntityRabbit || entity instanceof EntitySpider || entity instanceof EntityChicken || className.contains("Rat") || className.contains("Mouse") || className.contains("Hamster") || className.contains("Mole") || className.contains("Blindworm") || className.contains("Frog") || className.contains("Toad") || className.contains("Newt") || className.contains("Salamander") || className.contains("GuineaPig") || className.contains("Cavy") || className.contains("Chick") || className.contains("Chinchilla");
 	}
 
 	@Override
