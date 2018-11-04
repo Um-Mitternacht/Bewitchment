8f71e351b3ba03661619b63fdd94d82fa384d455
ZaBi94 <zabi94@gmail.com>
2018-11-04 14:34:22 +0100

    The Withe... Ninja'd. Have some fake tarots instead

--- src/main/java/com/bewitchment/common/content/tarot/ModTarots.java
+++ src/main/java/com/bewitchment/common/content/tarot/ModTarots.java
@@ -1,5 +1,7 @@
 package com.bewitchment.common.content.tarot;
 
+import java.util.function.Predicate;
+
 import com.bewitchment.api.infusion.DefaultInfusions;
 import com.bewitchment.api.mp.IMagicPowerContainer;
 import com.bewitchment.api.transformation.DefaultTransformations;
@@ -7,12 +9,15 @@ import com.bewitchment.common.content.crystalBall.capability.CapabilityFortune;
 import com.bewitchment.common.content.infusion.capability.InfusionCapability;
 import com.bewitchment.common.content.transformation.capability.CapabilityTransformationData;
 
+import net.minecraft.entity.player.EntityPlayer;
+
 public class ModTarots {
 
 	private ModTarots() {
 	}
 
 	public static void init() {
+		Predicate<EntityPlayer> random = p -> p.getRNG().nextBoolean();
 		TarotHandler.registerTarot(new QuickTarot("enderman", p -> p.getCapability(InfusionCapability.CAPABILITY, null).getType() == DefaultInfusions.END, null, null));
 		TarotHandler.registerTarot(new QuickTarot("diamonds", p -> p.getCapability(CapabilityFortune.CAPABILITY, null).getFortune() != null, p -> p.getCapability(CapabilityFortune.CAPABILITY, null).getFortune().isNegative(), null));
 		TarotHandler.registerTarot(new QuickTarot("iron_golem", p -> p.getCapability(InfusionCapability.CAPABILITY, null).getType() == DefaultInfusions.OVERWORLD, null, null));
@@ -22,6 +27,18 @@ public class ModTarots {
 		TarotHandler.registerTarot(new QuickTarot("moon", p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getType() == DefaultTransformations.VAMPIRE || p.getCapability(CapabilityTransformationData.CAPABILITY, null).getType() == DefaultTransformations.WEREWOLF, p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getType() == DefaultTransformations.WEREWOLF, p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getLevel()));
 		TarotHandler.registerTarot(new QuickTarot("silver_sword", p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getType() == DefaultTransformations.HUNTER, p -> false, p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getLevel()));
 		TarotHandler.registerTarot(new QuickTarot("hermit", p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getType() == DefaultTransformations.SPECTRE, null, p -> p.getCapability(CapabilityTransformationData.CAPABILITY, null).getLevel()));
+		
+		TarotHandler.registerTarot(new QuickTarot("ender_dragon", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("evoker", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("guardian", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("illusioner", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("stronghold", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("sun", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("mounts", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("world", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("witch", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("wither", random, null, null));
+		TarotHandler.registerTarot(new QuickTarot("zombie", random, null, null));
 	}
 
 }
