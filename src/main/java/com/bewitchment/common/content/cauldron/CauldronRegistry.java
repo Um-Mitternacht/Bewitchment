eb2e75983507d9dd976ef39b30718f36bbbce051
Sunconure11 <eyespot7b@aol.com>
2018-11-19 23:12:49 -0500

    Reformat

--- src/main/java/com/bewitchment/common/content/cauldron/CauldronRegistry.java
+++ src/main/java/com/bewitchment/common/content/cauldron/CauldronRegistry.java
@@ -313,7 +313,7 @@ public class CauldronRegistry {
 			throw new NullPointerException("Null potion cannot be registered");
 		}
 		if (ingredient.getMatchingStacks().length == 0) {
-			throw new IllegalArgumentException("Ingredient for "+potion.getRegistryName()+" is empty");
+			throw new IllegalArgumentException("Ingredient for " + potion.getRegistryName() + " is empty");
 		}
 		if (potion instanceof IBrewEffect) {
 			BewitchmentAPI.getAPI().registerBrewEffect((IBrewEffect) potion, potion, ingredient);
