e589ecc32fa5579a8d110371ee154dd4f3d933dd
Sunconure11 <eyespot7b@aol.com>
2018-11-21 13:12:33 -0500

    Kenaf op pls nerf

--- src/main/java/com/bewitchment/common/crafting/ModSpinningThreadRecipes.java
+++ src/main/java/com/bewitchment/common/crafting/ModSpinningThreadRecipes.java
@@ -19,6 +19,7 @@ public class ModSpinningThreadRecipes {
 		Ingredient stitching = Ingredient.fromItem(ModItems.witches_stitching);
 		Ingredient ectoplasm = Ingredient.fromItem(ModItems.ectoplasm);
 		Ingredient soul_string = Ingredient.fromItem(ModItems.soul_string);
+		Ingredient kenaf = Ingredient.fromItem(ModItems.kenaf);
 
 		SpinningThreadRecipe.REGISTRY.registerAll(
 				new SpinningThreadRecipe(rl("spider_web"), new ItemStack(Blocks.WEB), string, string, string),
@@ -27,7 +28,8 @@ public class ModSpinningThreadRecipes {
 				new SpinningThreadRecipe(rl("witches_stitching"), new ItemStack(ModItems.witches_stitching, 4), string, string, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 2)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 2))),
 				new SpinningThreadRecipe(rl("diabolic_vein"), new ItemStack(ModItems.diabolic_vein, 4), soul_string, soul_string, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 8)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 13))),
 				new SpinningThreadRecipe(rl("pure_filament"), new ItemStack(ModItems.pure_filament, 4), stitching, stitching, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 7)), Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 7))),
-				new SpinningThreadRecipe(rl("soulstring"), new ItemStack(ModItems.soul_string, 2), stitching, stitching, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 10)), ectoplasm)
+				new SpinningThreadRecipe(rl("soulstring"), new ItemStack(ModItems.soul_string, 2), stitching, stitching, Ingredient.fromStacks(new ItemStack(ModItems.fume, 1, 10)), ectoplasm),
+				new SpinningThreadRecipe(rl("string"), new ItemStack(Items.STRING, 12, 0), kenaf, kenaf, kenaf, kenaf)
 		);
 	}
 
