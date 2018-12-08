3c0853d0643f03692799a125d658abd6ec20c732
Sunconure11 <eyespot7b@aol.com>
2018-12-08 01:16:20 -0500

    Attempt at animation

--- src/main/java/com/bewitchment/client/render/entity/model/ModelSnake.java
+++ src/main/java/com/bewitchment/client/render/entity/model/ModelSnake.java
@@ -1,7 +1,6 @@
 package com.bewitchment.client.render.entity.model;
 
 import com.bewitchment.common.entity.living.animals.EntitySnake;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
--- src/main/java/com/bewitchment/client/render/entity/model/ModelToad.java
+++ src/main/java/com/bewitchment/client/render/entity/model/ModelToad.java
@@ -1,7 +1,6 @@
 package com.bewitchment.client.render.entity.model;
 
 import com.bewitchment.common.entity.living.animals.EntityToad;
-
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
