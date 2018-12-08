1b349b2f087585d38b71223ee3785265ca85cf34
Sunconure11 <eyespot7b@aol.com>
2018-12-08 14:47:06 -0500

    Uranids glow properly

--- src/main/java/com/bewitchment/common/entity/spirits/demons/EntityUran.java
+++ src/main/java/com/bewitchment/common/entity/spirits/demons/EntityUran.java
@@ -28,6 +28,8 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Set;
 
@@ -102,6 +104,12 @@ public class EntityUran extends EntityMultiSkin {
 	@Override
 	protected ResourceLocation getLootTable() {
 		return loot;
+	}
+
+	@SideOnly(Side.CLIENT)
+	public int getBrightnessForRender()
+	{
+		return 15728880;
 	}
 
 	@Override
