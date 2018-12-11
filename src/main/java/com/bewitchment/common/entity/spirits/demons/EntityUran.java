ab71a09fb2067ed7df18922c16c16f01bf714205
Sunconure11 <eyespot7b@aol.com>
2018-12-11 16:00:57 -0500

    Still not spawning where I want to spawn them

--- src/main/java/com/bewitchment/common/entity/spirits/demons/EntityUran.java
+++ src/main/java/com/bewitchment/common/entity/spirits/demons/EntityUran.java
@@ -90,7 +90,7 @@ public class EntityUran extends EntityMultiSkin implements IMob {
 		int k = MathHelper.floor(this.posZ);
 		BlockPos blockpos = new BlockPos(i, j, k);
 		Block block = this.world.getBlockState(blockpos.down()).getBlock();
-		return block instanceof BlockNetherBrick && this.world.getLight(blockpos) > 4 && super.getCanSpawnHere() && this.world.getDifficulty() != EnumDifficulty.PEACEFUL;
+		return block instanceof BlockNetherBrick && super.getCanSpawnHere() && this.world.getDifficulty() != EnumDifficulty.PEACEFUL;
 	}
 
 	@Override
