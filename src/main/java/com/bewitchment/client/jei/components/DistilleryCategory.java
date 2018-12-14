41cb706b73078f99a257b0316657a4bc09ed9387
Sunconure11 <eyespot7b@aol.com>
2018-12-14 03:44:45 -0500

    Setting up the bare bones for the distillery plugin

--- src/main/java/com/bewitchment/client/jei/components/DistilleryCategory.java
+++ src/main/java/com/bewitchment/client/jei/components/DistilleryCategory.java
@@ -1,7 +1,36 @@
 package com.bewitchment.client.jei.components;
 
+import mezz.jei.api.gui.IDrawable;
+import mezz.jei.api.gui.IRecipeLayout;
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IRecipeCategory;
+
 /**
  * Created by Joseph on 12/10/2018.
  */
-public class DistilleryCategory {
+public class DistilleryCategory implements IRecipeCategory<DistilleryWrapper> {
+	@Override
+	public String getUid() {
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		return null;
+	}
+
+	@Override
+	public String getModName() {
+		return null;
+	}
+
+	@Override
+	public IDrawable getBackground() {
+		return null;
+	}
+
+	@Override
+	public void setRecipe(IRecipeLayout recipeLayout, DistilleryWrapper recipeWrapper, IIngredients ingredients) {
+
+	}
 }
--- src/main/java/com/bewitchment/client/jei/components/DistilleryWrapper.java
+++ src/main/java/com/bewitchment/client/jei/components/DistilleryWrapper.java
@@ -1,7 +1,14 @@
 package com.bewitchment.client.jei.components;
 
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.IRecipeWrapper;
+
 /**
  * Created by Joseph on 12/10/2018.
  */
-public class DistilleryWrapper {
+public class DistilleryWrapper implements IRecipeWrapper {
+	@Override
+	public void getIngredients(IIngredients ingredients) {
+
+	}
 }
