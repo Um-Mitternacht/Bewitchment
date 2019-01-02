7d6a2820bd26fdb07b81bcdbe0c9ff8d6f43a121
Sunconure11 <eyespot7b@aol.com>
2019-01-02 12:56:57 -0500

    More things ported over from covens reborn

--- src/main/java/com/bewitchment/common/integration/patchouli/processors/RitualProcessor.java
+++ src/main/java/com/bewitchment/common/integration/patchouli/processors/RitualProcessor.java
@@ -12,28 +12,34 @@ import java.util.Objects;
 
 public class RitualProcessor implements IComponentProcessor {
 
-	AdapterIRitual ritual;
+	private AdapterIRitual ritual;
 
 	@Override
 	public void setup(IVariableProvider<String> json) {
-		ritual = AdapterIRitual.REGISTRY.getValue(new ResourceLocation(json.get("ritual")));
-		Objects.requireNonNull(ritual);
+		this.ritual = AdapterIRitual.REGISTRY.getValue(new ResourceLocation(json.get("ritual")));
+		Objects.requireNonNull(this.ritual);
 	}
 
 	@Override
 	public String process(String val) {
 		try {
 			if (val.startsWith("circle")) {
-				return getCircle(val.substring(6));
+				return this.getCircle(val.substring(6));
 			}
 			if (val.startsWith("input")) {
-				return getInput(val.substring(5));
+				return this.getInput(val.substring(5));
 			}
 			if (val.startsWith("output")) {
-				return getOutput(val.substring(6));
+				return this.getOutput(val.substring(6));
 			}
-			if (val.equals("ritualname")) {
-				return I18n.format("ritual." + ritual.getRegistryName().toString().replace(':', '.') + ".name");
+			if ("ritualname".equals(val)) {
+				return I18n.format("ritual." + this.ritual.getRegistryName().toString().replace(':', '.') + ".name");
+			}
+			if ("ritual".equals(val)) {
+				return this.ritual.getRegistryName().toString();
+			}
+			if ("separator".equals(val)) {
+				return this.ritual.getOutputRaw().isEmpty() ? "bewitchment:textures/gui/books/ritual/separator_none.png" : "bewitchment:textures/gui/books/ritual/separator.png";
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -45,7 +51,7 @@ public class RitualProcessor implements IComponentProcessor {
 	private String getOutput(String substring) {
 		try {
 			int index = Integer.parseInt(substring) - 1;
-			return PatchouliAPI.instance.serializeItemStack(ritual.getOutputRaw().get(index));
+			return PatchouliAPI.instance.serializeItemStack(this.ritual.getOutputRaw().get(index));
 		} catch (Exception e) {
 			return "minecraft:air";
 		}
@@ -54,7 +60,7 @@ public class RitualProcessor implements IComponentProcessor {
 	private String getInput(String substring) {
 		try {
 			int index = Integer.parseInt(substring) - 1;
-			return PatchouliAPI.instance.serializeItemStack(ritual.getInput().get(index).getMatchingStacks()[0]);
+			return PatchouliAPI.instance.serializeItemStack(this.ritual.getInput().get(index).getMatchingStacks()[0]);
 		} catch (Exception e) {
 			return "minecraft:air";
 		}
@@ -62,9 +68,10 @@ public class RitualProcessor implements IComponentProcessor {
 
 	private String getCircle(String substring) {
 		int index = Integer.parseInt(substring);
-		if ((ritual.getCircles() & 3) < index - 1) {
+		if ((this.ritual.getCircles() & 3) < (index - 1)) {
 			return "bewitchment:textures/gui/books/ritual/circle_center.png";
 		}
-		return "bewitchment:textures/gui/books/ritual/circle_" + EnumGlyphType.fromMeta((ritual.getCircles() >> (2 * index)) & 3).name().toLowerCase() + "_" + index + ".png";
+		return "bewitchment:textures/gui/books/ritual/circle_" + EnumGlyphType.fromMeta((this.ritual.getCircles() >> (2 * index)) & 3).name().toLowerCase() + "_" + index + ".png";
 	}
 }
+
