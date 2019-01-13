78176734b7531fa1a07b7508fa40c13a16239c30
MoriyaShiine <moriyashiine@gmail.com>
2019-01-13 11:37:48 -0600

    lol i forgot how random works

--- src/main/java/com/bewitchment/common/content/ritual/ModRituals.java
+++ src/main/java/com/bewitchment/common/content/ritual/ModRituals.java
@@ -26,7 +26,7 @@ public class ModRituals {
 			infusion_nether, infusion_end, infusion_dream, flames, sanctuary, spawn_vex,
 			deck, table, crystal_ball, elder_broom, juniper_broom, yew_broom, cypress_broom, gateway,
 			nether_portal, spawn_blaze, spawn_ghast, spawn_magma_cube, shift_biome, vampire_lair,
-			spawn_hellhound, spawn_uranid, day, frenzied_growth;
+			spawn_hellhound, spawn_uranid, day, frenzied_growth, call_of_the_wild;
 
 	public static void init() {
 
@@ -269,6 +269,7 @@ public class ModRituals {
 		cypress_broom = new RitualImpl(rl("cypress_broom"), of(LibIngredients.logCypress, LibIngredients.broomMundane, Ingredient.fromStacks(new ItemStack(ModBlocks.sapling, 1, 3)), LibIngredients.magicSalve, LibIngredients.elytra), ofs(new ItemStack(ModItems.broom, 1, 4)), 130, circles(NORMAL, NORMAL, ENDER), 1000, 4);
 		vampire_lair = new RitualCreateVampireLair(rl("vampire_lair"), of(LibIngredients.bloodyRags, LibIngredients.bloodyRags, LibIngredients.anySapling, LibIngredients.blazePowder, LibIngredients.boline), ofs(new ItemStack(ModItems.boline)), 200, circles(NORMAL, NETHER, NETHER), 5000, 5);
 		frenzied_growth = new RitualFrenziedGrowth(rl("frenzied_growth"), of(LibIngredients.oakAppleGall, LibIngredients.anyLeaf, LibIngredients.anyLog, LibIngredients.anySapling, LibIngredients.dimensionalSand), none, 135, circles(ANY, ANY, ANY), 1000, 3);
+		call_of_the_wild = new RitualCallOfTheWild(rl("call_of_the_wild"), of(LibIngredients.wax), none, 135, circles(ANY, ANY, ANY), 100, 0);
 		registerAll();
 	}
 
@@ -279,7 +280,7 @@ public class ModRituals {
 				flames, sanctuary, spawn_vex, deck, table, crystal_ball, elder_broom,
 				juniper_broom, yew_broom, cypress_broom, gateway, nether_portal, spawn_blaze,
 				spawn_ghast, spawn_magma_cube, shift_biome, vampire_lair, spawn_hellhound,
-				spawn_uranid, day, frenzied_growth
+				spawn_uranid, day, frenzied_growth, call_of_the_wild
 
 
 		).stream().map(r -> new AdapterIRitual(r)).forEach(r -> AdapterIRitual.REGISTRY.register(r));
---
+++ src/main/java/com/bewitchment/common/content/ritual/rituals/RitualCallOfTheWild.java
@@ -0,0 +1,86 @@
+package com.bewitchment.common.content.ritual.rituals;
+
+import java.util.Random;
+
+import javax.annotation.Nullable;
+
+import com.bewitchment.common.content.ritual.RitualImpl;
+import com.bewitchment.common.entity.living.animals.EntityBlindworm;
+import com.bewitchment.common.entity.living.animals.EntityLizard;
+import com.bewitchment.common.entity.living.animals.EntityNewt;
+import com.bewitchment.common.entity.living.animals.EntityOwl;
+import com.bewitchment.common.entity.living.animals.EntityRaven;
+import com.bewitchment.common.entity.living.animals.EntitySnake;
+import com.bewitchment.common.entity.living.animals.EntityToad;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.NonNullList;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class RitualCallOfTheWild extends RitualImpl
+{
+	public RitualCallOfTheWild(ResourceLocation registryName, NonNullList<Ingredient> input, NonNullList<ItemStack> output, int timeInTicks, int circles, int altarStartingPower, int powerPerTick)
+	{
+		super(registryName, input, output, timeInTicks, circles, altarStartingPower, powerPerTick);
+	}
+	
+	@Override
+	public void onFinish(@Nullable EntityPlayer player, TileEntity tile, World world, BlockPos mainGlyphPos, NBTTagCompound data, BlockPos effectivePosition, int covenSize)
+	{
+		if (!world.isRemote)
+		{
+			for (int i = 0; i < world.rand.nextInt(3)+1; i++)
+			{
+				BlockPos pos = effectivePosition.add(world.rand.nextInt(11)-5, 1, world.rand.nextInt(11)-5);
+				Entity e;
+				switch (world.rand.nextInt(7))
+				{
+					case 0:
+						e = new EntityBlindworm(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 1:
+						e = new EntityLizard(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 2:
+						e = new EntityNewt(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 3:
+						e = new EntityOwl(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 4:
+						e = new EntityRaven(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 5:
+						e = new EntitySnake(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					case 6:
+						e = new EntityToad(world);
+						e.setPosition(pos.getX(), pos.getY(), pos.getZ());
+						world.spawnEntity(e);
+						break;
+					default:
+						break;
+				}
+			}
+		}
+	}
+}
