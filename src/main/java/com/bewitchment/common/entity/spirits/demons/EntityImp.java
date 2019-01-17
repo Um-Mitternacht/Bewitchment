cff4387775606222aa9446a7e7457eedbd0007f3
Sunconure11 <eyespot7b@aol.com>
2019-01-17 14:25:22 -0500

    Even more

--- src/main/java/com/bewitchment/common/entity/spirits/demons/EntityImp.java
+++ src/main/java/com/bewitchment/common/entity/spirits/demons/EntityImp.java
@@ -4,10 +4,11 @@ import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.common.entity.living.EntityMultiSkin;
 import net.ilexiconn.llibrary.server.animation.Animation;
 import net.ilexiconn.llibrary.server.animation.IAnimatedEntity;
-import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.EnumCreatureAttribute;
-import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.*;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.init.MobEffects;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -23,6 +24,7 @@ public class EntityImp extends EntityMultiSkin implements IAnimatedEntity, IMob 
 		super(worldIn);
 		setSize(1.0F, 2.0F);
 		this.isImmuneToFire = true;
+		this.experienceValue = 100;
 	}
 
 	@Override
@@ -39,6 +41,20 @@ public class EntityImp extends EntityMultiSkin implements IAnimatedEntity, IMob 
 	@Override
 	public int getSkinTypes() {
 		return 4;
+	}
+
+	@Override
+	public boolean attackEntityAsMob(Entity entity) {
+		super.attackEntityAsMob(entity);
+		boolean flag = entity.attackEntityFrom(DamageSource.causeMobDamage(this), ((int) this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue()));
+		if (flag) {
+			this.applyEnchantments(this, entity);
+			if (entity instanceof EntityLivingBase) {
+				((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.NAUSEA, 500, 0, false, false));
+				setFire(250);
+			}
+		}
+		return flag;
 	}
 
 	public EnumCreatureAttribute getCreatureAttribute() {
