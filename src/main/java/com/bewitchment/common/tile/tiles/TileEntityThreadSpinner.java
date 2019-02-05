f5dec682642c30cc9c0a2a7bfe7210d306d224e1
Sunconure11 <eyespot7b@aol.com>
2019-02-05 14:33:21 -0500

    Porting over a fix pointed out by the issue tracker

--- src/main/java/com/bewitchment/common/tile/tiles/TileEntityThreadSpinner.java
+++ src/main/java/com/bewitchment/common/tile/tiles/TileEntityThreadSpinner.java
@@ -57,9 +57,11 @@ public class TileEntityThreadSpinner extends ModTileEntity implements ITickable,
 
 		ItemStack heldItem = playerIn.getHeldItem(hand);
 		if (!heldItem.isEmpty() && heldItem.getItem() == Items.NAME_TAG) {
-			this.customName = heldItem.getDisplayName();
-			this.markDirty();
-			this.syncToClient();
+			if (!worldIn.isRemote) {
+				this.customName = heldItem.getDisplayName();
+				this.markDirty();
+				this.syncToClient();
+			}
 		} else {
 			playerIn.openGui(Bewitchment.instance, LibGui.THREAD_SPINNER.ordinal(), worldIn, pos.getX(), pos.getY(), pos.getZ());
 		}
