c77ea87c7f6a38b472fb3e332bfd3663a8751e6b
Sunconure11 <eyespot7b@aol.com>
2019-02-07 23:41:53 -0500

    More animation stuff, still a wip

--- src/main/java/com/bewitchment/client/render/entity/model/ModelUran.java
+++ src/main/java/com/bewitchment/client/render/entity/model/ModelUran.java
@@ -350,20 +350,18 @@ public class ModelUran extends AdvancedModelBase {
 		//f = entity.ticksExisted;
 		//f1 = 0.5f;
 
-		float globalSpeed = 0.1f;
+		float globalSpeed = 1;
 		float globalHeight = 1;
 		float globalDegree = 1;
 
 		bob(head, 0.1f, 0.8f * globalHeight, false, f, f1);
-		swing(neck01a, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(neck02, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(neck03, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(body01, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(tail01, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(tail02, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(tail03, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(tail04, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
-		swing(tail05, 0.1f, 0.8f * globalHeight, false, f, f1, 0.3f, 0.1f);
+		swing(neck01a, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(body01, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(tail01, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(tail02, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(tail03, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(tail04, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
+		swing(tail05, 0.1f, 0.8f * globalHeight, false, f, f1, entity.ticksExisted, 1);
 	}
 
 	/**
