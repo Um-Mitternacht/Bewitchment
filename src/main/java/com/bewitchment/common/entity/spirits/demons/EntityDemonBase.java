6dec3076fd780056f3c54f47d22c0e058dfa0536
Sunconure11 <eyespot7b@aol.com>
2019-02-17 11:56:07 -0500

    More progress towards deals with demons

--- src/main/java/com/bewitchment/common/entity/spirits/demons/EntityDemonBase.java
+++ src/main/java/com/bewitchment/common/entity/spirits/demons/EntityDemonBase.java
@@ -8,21 +8,32 @@ import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.monster.IMob;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.village.MerchantRecipe;
 import net.minecraft.village.MerchantRecipeList;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
 import javax.annotation.Nullable;
 
 /**
  * Created by Joseph on 1/13/2019.
+ *
+ * Credit to AlexThe666 for bits and pieces of the code, so that I may create a merchant not based on a villager.
  */
 public class EntityDemonBase extends EntityMultiSkin implements IAnimatedEntity, IMob, IMerchant {
 	private int animationTick;
 	private Animation currentAnimation;
+	@Nullable
+	private EntityPlayer buyingPlayer;
+	@Nullable
+	private MerchantRecipeList buyingList;
+	private java.util.UUID lastBuyingPlayer;
+	private int careerId;
+	private int careerLevel;
 
 	public EntityDemonBase(World worldIn) {
 		super(worldIn);
@@ -70,24 +81,55 @@ public class EntityDemonBase extends EntityMultiSkin implements IAnimatedEntity,
 
 	@Override
 	public void setCustomer(@Nullable EntityPlayer player) {
-
+		this.buyingPlayer = player;
 	}
 
 	@Nullable
 	@Override
 	public EntityPlayer getCustomer() {
-		return null;
+		return this.buyingPlayer;
 	}
 
 	@Nullable
 	@Override
 	public MerchantRecipeList getRecipes(EntityPlayer player) {
-		return null;
+		if (this.buyingList == null) {
+			this.populateBuyingList();
+		}
+
+		return this.buyingList;
 	}
 
 	@Override
 	public void setRecipes(@Nullable MerchantRecipeList recipeList) {
 
+	}
+
+	public VillagerRegistry.VillagerProfession getProfessionForge() {
+		return null;
+	}
+
+	private void populateBuyingList() {
+		if (this.careerId != 0 && this.careerLevel != 0) {
+			++this.careerLevel;
+		} else {
+			this.careerId = this.getProfessionForge().getRandomCareer(this.rand) + 1;
+			this.careerLevel = 1;
+		}
+
+		if (this.buyingList == null) {
+			this.buyingList = new MerchantRecipeList();
+		}
+
+		int i = this.careerId - 1;
+		int j = this.careerLevel - 1;
+		java.util.List<EntityVillager.ITradeList> trades = this.getProfessionForge().getCareer(i).getTrades(j);
+
+		if (trades != null) {
+			for (EntityVillager.ITradeList entityvillager$itradelist : trades) {
+				entityvillager$itradelist.addMerchantRecipe(this, this.buyingList, this.rand);
+			}
+		}
 	}
 
 	@Override
