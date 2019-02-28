d70bc709394f408709a02e57237429697758f7ba
Sunconure11 <eyespot7b@aol.com>
2019-02-27 19:46:54 -0500

    More expansions as to where our loot can be found

--- src/main/java/com/bewitchment/common/core/event/LootTableEventHandler.java
+++ src/main/java/com/bewitchment/common/core/event/LootTableEventHandler.java
@@ -47,6 +47,9 @@ public class LootTableEventHandler {
 				evt.getName().equals(LootTableList.CHESTS_VILLAGE_BLACKSMITH) ||
 				evt.getName().equals(LootTableList.CHESTS_JUNGLE_TEMPLE) ||
 				evt.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID) ||
+				evt.getName().equals(LootTableList.CHESTS_IGLOO_CHEST) ||
+				evt.getName().equals(LootTableList.CHESTS_END_CITY_TREASURE) ||
+				evt.getName().equals(LootTableList.CHESTS_STRONGHOLD_LIBRARY) ||
 				evt.getName().equals(LootTableList.CHESTS_WOODLAND_MANSION)
 				) { //Not sure if this is messy or better looking than the alternative.
 			evt.getTable().addPool(new LootPool(new LootEntry[]{
