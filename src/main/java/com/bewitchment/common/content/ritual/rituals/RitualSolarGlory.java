f9b339af4e57f5dc150a27b8e702bf5e85477f9e
DaveArndt <davemarndt@gmail.com>
2019-03-03 02:51:57 -0800

    cleaned up unnecessary code

--- src/main/java/com/bewitchment/common/content/ritual/rituals/RitualSolarGlory.java
+++ src/main/java/com/bewitchment/common/content/ritual/rituals/RitualSolarGlory.java
@@ -27,9 +27,7 @@ public class RitualSolarGlory extends RitualImpl {
 	public void onFinish(EntityPlayer player, TileEntity tile, World world, BlockPos pos, NBTTagCompound tag, BlockPos effectivePosition, int covenSize) {
 		if (!world.isRemote) {
 			world.playerEntities.stream()
-					.filter(p -> p.getCapability(CapabilityTransformation.CAPABILITY, null)!=null)
 					.filter(p -> p.getCapability(CapabilityTransformation.CAPABILITY, null).getType()== DefaultTransformations.VAMPIRE)
-					.filter(p -> p!=player)
 					.forEach(p -> p.addPotionEffect(new PotionEffect(ModPotions.sun_ward, 30*20)));
 			world.setWorldTime(6000);
 		}
