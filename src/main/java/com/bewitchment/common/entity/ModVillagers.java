ff06306c0416e3244ef69e2199db700d610f72b5
Sunconure11 <eyespot7b@aol.com>
2019-03-07 17:05:11 -0500

    This is so wrong on many levels.

--- src/main/java/com/bewitchment/common/entity/ModVillagers.java
+++ src/main/java/com/bewitchment/common/entity/ModVillagers.java
@@ -1,19 +1,30 @@
 package com.bewitchment.common.entity;
 
-import com.google.common.collect.Maps;
+import com.bewitchment.common.item.ModItems;
+import com.bewitchment.common.lib.LibMod;
+import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.village.MerchantRecipe;
+import net.minecraft.village.MerchantRecipeList;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.VillagerRegistry;
 
-import java.util.Map;
 import java.util.Random;
 
 /**
  * Created by Joseph on 2/9/2019.
  */
+@Mod.EventBusSubscriber(modid = LibMod.MOD_ID)
 public class ModVillagers {
 
 	public static final ModVillagers INSTANCE = new ModVillagers();
-
+	public static final VillagerRegistry.VillagerProfession profAlchemist = new VillagerRegistry.VillagerProfession(LibMod.MOD_ID + "alchemist",
+			LibMod.MOD_ID + "textures/entity/mobs/npcs/villagers/villager_alchemist.png",
+			"minecraft:textures/entity/zombie_villager/zombie_villager.png");
 	//Demonic professions
 	public VillagerRegistry.VillagerProfession warlord;
 	public VillagerRegistry.VillagerProfession slaver;
@@ -21,64 +32,55 @@ public class ModVillagers {
 	public VillagerRegistry.VillagerProfession brute;
 	public VillagerRegistry.VillagerProfession apocryphal_teacher;
 	public VillagerRegistry.VillagerProfession despot;
-
 	//Villager professions
 	public VillagerRegistry.VillagerProfession hedge_witch;
 	public VillagerRegistry.VillagerProfession rockhound;
 	public VillagerRegistry.VillagerProfession herbalist;
-	public VillagerRegistry.VillagerProfession alchemist;
 	public VillagerRegistry.VillagerProfession hunter;
 	public VillagerRegistry.VillagerProfession necromancer;
 
-	public Map<Integer, VillagerRegistry.VillagerProfession> professions = Maps.newHashMap();
+	@SubscribeEvent
+	public static void init(RegistryEvent.Register<VillagerRegistry.VillagerProfession> event) {
+		event.getRegistry().register(profAlchemist);
 
-	public void init() {
-
-		//Demon professions
-		warlord = new VillagerRegistry.VillagerProfession("bewitchment:warlord", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		slaver = new VillagerRegistry.VillagerProfession("bewitchment:slaver", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		noble = new VillagerRegistry.VillagerProfession("bewitchment:noble", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		brute = new VillagerRegistry.VillagerProfession("bewitchment:brute", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		apocryphal_teacher = new VillagerRegistry.VillagerProfession("bewitchment:apocryphal_teacher", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		despot = new VillagerRegistry.VillagerProfession("bewitchment:despot", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		//Human professions
-		hedge_witch = new VillagerRegistry.VillagerProfession("bewitchment:warlord", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		rockhound = new VillagerRegistry.VillagerProfession("bewitchment:slaver", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		herbalist = new VillagerRegistry.VillagerProfession("bewitchment:noble", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		alchemist = new VillagerRegistry.VillagerProfession("bewitchment:brute", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		hunter = new VillagerRegistry.VillagerProfession("bewitchment:apocryphal_teacher", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
-		necromancer = new VillagerRegistry.VillagerProfession("bewitchment:despot", "minecraft:textures/entity/zombie_villager/zombie_farmer.png", "minecraft:textures/entity/zombie_villager/zombie_farmer.png");
-		{
-		}
+		new VillagerRegistry.VillagerCareer(profAlchemist, "alchemist")
+				.addTrade(1, new EmeraldsForItemsWithMeta(new ItemStack(ModItems.tulsi, 16, 0), new EntityVillager.PriceInfo(3, 12)),
+						new EntityVillager.ListItemForEmeralds(ModItems.juniper_berries, new EntityVillager.PriceInfo(1, 6)),
+						makeItemAndEmeraldToItemWithMeta(new ItemStack(ModItems.glass_jar, 1, 0), new EntityVillager.PriceInfo(4, 16), new ItemStack(ModItems.magic_salve, 1, 0), new EntityVillager.PriceInfo(1, 1)));
 	}
 
-	public void setRandomProfession(EntityVillager entity, Random rand) {
-		entity.setProfession(professions.get(rand.nextInt(professions.size())));
+	static EntityVillager.ITradeList makeItemAndEmeraldToItemWithMeta(ItemStack buying, EntityVillager.PriceInfo buyingPriceInfo, ItemStack selling, EntityVillager.PriceInfo sellingPriceInfo) {
+		EntityVillager.ItemAndEmeraldToItem trade = new EntityVillager.ItemAndEmeraldToItem(buying.getItem(), buyingPriceInfo, selling.getItem(), sellingPriceInfo);
+		trade.buyingItemStack = buying;
+		trade.sellingItemstack = selling;
+		return trade;
 	}
 
-	private void register(VillagerRegistry.VillagerProfession prof, int id) {
-		professions.put(id, prof);
+	static class EmeraldsForItemsWithMeta implements EntityVillager.ITradeList {
+		ItemStack stack;
+		EntityVillager.PriceInfo price;
+
+		EmeraldsForItemsWithMeta(ItemStack stack, EntityVillager.PriceInfo price) {
+			this.stack = stack;
+			this.price = price;
+		}
+
+		@Override
+		public void addMerchantRecipe(IMerchant merchant, MerchantRecipeList recipeList, Random random) {
+			int p = 1;
+			if (price != null)
+				p = price.getPrice(random);
+
+			ItemStack buying, selling;
+			if (p > 0) {
+				buying = new ItemStack(stack.getItem(), p, stack.getMetadata());
+				selling = new ItemStack(Items.EMERALD);
+			} else {
+				buying = new ItemStack(stack.getItem(), 1, stack.getMetadata());
+				selling = new ItemStack(Items.EMERALD, -p);
+			}
+
+			recipeList.add(new MerchantRecipe(buying, selling));
+		}
 	}
 }
