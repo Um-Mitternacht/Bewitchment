a4d98b97d5c7da0a5cec1c7bce811793999b32ea
DaveArndt <davemarndt@gmail.com>
2019-03-07 12:51:44 -0800

    fixed reference to client-only method

--- src/main/java/com/bewitchment/common/entity/LargeEntityAIAttackMelee.java
+++ src/main/java/com/bewitchment/common/entity/LargeEntityAIAttackMelee.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraft.util.math.Vec3d;
 
 public class LargeEntityAIAttackMelee extends EntityAIAttackMelee {
 
@@ -18,8 +19,14 @@ public class LargeEntityAIAttackMelee extends EntityAIAttackMelee {
 		double d0 = this.getAttackReachSqr(enemy);
 
 		if (this.attackTick <= 0 && distToEnemySqr <= d0) {
+
 			// call block raytrace from attacker's eyes, confirm hit if not looking at block
-			RayTraceResult trace = super.attacker.rayTrace(Math.sqrt(distToEnemySqr), 1.0f);
+			double distance = Math.sqrt(distToEnemySqr);
+			Vec3d vec3d = this.attacker.getPositionEyes(1.0f);
+			Vec3d vec3d1 = this.attacker.getLook(1.0f);
+			Vec3d vec3d2 = vec3d.add(vec3d1.x * distance, vec3d1.y * distance, vec3d1.z * distance);
+			RayTraceResult trace = this.attacker.world.rayTraceBlocks(vec3d, vec3d2, false, false, true);
+
 			if (trace != null
 					&& trace.typeOfHit == Type.MISS) {
 				this.attackTick = 20;
