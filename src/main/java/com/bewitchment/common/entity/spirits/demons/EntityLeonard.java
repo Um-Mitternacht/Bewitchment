86a1f0605506703801abc97f3eb7da339820bd96
Sunconure11 <eyespot7b@aol.com>
2019-03-12 07:24:33 -0400

    Leonard walk animation plus start of rumination

--- src/main/java/com/bewitchment/common/entity/spirits/demons/EntityLeonard.java
+++ src/main/java/com/bewitchment/common/entity/spirits/demons/EntityLeonard.java
@@ -2,8 +2,11 @@ package com.bewitchment.common.entity.spirits.demons;
 
 import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.common.entity.living.EntityMultiSkin;
+import net.ilexiconn.llibrary.server.animation.Animation;
+import net.ilexiconn.llibrary.server.animation.IAnimatedEntity;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.monster.IMob;
 import net.minecraft.world.World;
 
 import javax.annotation.Nullable;
@@ -11,7 +14,12 @@ import javax.annotation.Nullable;
 /**
  * Created by Joseph on 3/9/2019.
  */
-public class EntityLeonard extends EntityMultiSkin {
+public class EntityLeonard extends EntityMultiSkin implements IAnimatedEntity, IMob {
+
+	public static final Animation RUMINATE_IDLE = Animation.create(20, 10);
+	private int animationTick;
+	private Animation currentAnimation;
+
 	public EntityLeonard(World worldIn) {
 		super(worldIn);
 		setSize(1F, 3.5F);
@@ -30,5 +38,30 @@ public class EntityLeonard extends EntityMultiSkin {
 	@Override
 	public EntityAgeable createChild(EntityAgeable ageable) {
 		return null;
+	}
+
+	@Override
+	public int getAnimationTick() {
+		return animationTick;
+	}
+
+	@Override
+	public void setAnimationTick(int tick) {
+		animationTick = tick;
+	}
+
+	@Override
+	public Animation getAnimation() {
+		return currentAnimation;
+	}
+
+	@Override
+	public void setAnimation(Animation animation) {
+		currentAnimation = animation;
+	}
+
+	@Override
+	public Animation[] getAnimations() {
+		return new Animation[]{IAnimatedEntity.NO_ANIMATION, EntityLeonard.RUMINATE_IDLE};
 	}
 }
