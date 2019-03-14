646a199707e5485f713aa7894e3592634e2cc9c5
MoriyaShiine <moriyashiine@gmail.com>
2019-03-14 12:03:23 -0500

    henlo

--- src/main/java/com/bewitchment/common/entity/living/animals/EntityLizard.java
+++ src/main/java/com/bewitchment/common/entity/living/animals/EntityLizard.java
@@ -1,44 +1,33 @@
 package com.bewitchment.common.entity.living.animals;
 
-import com.bewitchment.common.item.ModItems;
 import com.bewitchment.common.lib.LibMod;
-import com.google.common.collect.Sets;
 import net.ilexiconn.llibrary.server.animation.Animation;
 import net.ilexiconn.llibrary.server.animation.IAnimatedEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockGrass;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityAgeable;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.*;
 import net.minecraft.entity.passive.EntityAnimal;
-import net.minecraft.entity.passive.EntityTameable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.datasync.DataParameter;
 import net.minecraft.network.datasync.DataSerializers;
 import net.minecraft.network.datasync.EntityDataManager;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
-import java.util.Set;
-
 /**
  * Created by Joseph on 10/2/2018.
  */
 
-public class EntityLizard extends EntityTameable implements IAnimatedEntity {
+public class EntityLizard extends EntityAnimal implements IAnimatedEntity {
 
 	private static final ResourceLocation loot = new ResourceLocation(LibMod.MOD_ID, "entities/lizard");
-	private static final Set<Item> TAME_ITEMS = Sets.newHashSet(Items.SPIDER_EYE, Items.FERMENTED_SPIDER_EYE, ModItems.silver_scales, ModItems.envenomed_fang);
 	private static final DataParameter<Integer> SKIN_TYPE = EntityDataManager.createKey(EntityLizard.class, DataSerializers.VARINT);
 	private int animationTick;
 	private Animation currentAnimation;
@@ -52,24 +41,12 @@ public class EntityLizard extends EntityTameable implements IAnimatedEntity {
 	@Override
 	protected void entityInit() {
 		super.entityInit();
-		this.aiSit = new EntityAISit(this);
 		this.dataManager.register(SKIN_TYPE, getRNG().nextInt(4));
 		this.dataManager.setDirty(SKIN_TYPE);
 	}
 
 	public int getSkinIndex() {
 		return dataManager.get(SKIN_TYPE);
-	}
-
-	@Override
-	public boolean attackEntityFrom(DamageSource source, float amount) {
-		if (this.isEntityInvulnerable(source)) {
-			return false;
-		}
-		if (this.aiSit != null) {
-			this.aiSit.setSitting(false);
-		}
-		return super.attackEntityFrom(source, amount);
 	}
 
 	@Override
@@ -90,7 +67,6 @@ public class EntityLizard extends EntityTameable implements IAnimatedEntity {
 		this.tasks.addTask(5, new EntityAILookIdle(this));
 		this.tasks.addTask(4, new EntityAIWatchClosest2(this, EntityPlayer.class, 5f, 1f));
 		this.tasks.addTask(3, new EntityAIMate(this, 0.3d));
-		this.tasks.addTask(4, this.aiSit);
 		this.tasks.addTask(4, new EntityAIFollowParent(this, 0.4D));
 	}
 
@@ -116,65 +92,13 @@ public class EntityLizard extends EntityTameable implements IAnimatedEntity {
 
 	@Override
 	public boolean canMateWith(EntityAnimal otherAnimal) {
-		if (otherAnimal == this) {
-			return false;
-		} else if (!this.isTamed()) {
-			return false;
-		} else if (!(otherAnimal instanceof EntityLizard)) {
-			return false;
-		} else {
-			EntityLizard entitylizard = (EntityLizard) otherAnimal;
-
-			if (!entitylizard.isTamed()) {
-				return false;
-			} else if (entitylizard.isSitting()) {
-				return false;
-			} else {
-				return this.isInLove() && entitylizard.isInLove();
-			}
-		}
+		if (otherAnimal == this || !(otherAnimal instanceof EntityLizard)) return false;
+		return isInLove() && otherAnimal.isInLove();
 	}
 
 	@Override
 	public boolean canBePushed() {
 		return true;
-	}
-
-	@Override
-	public boolean processInteract(EntityPlayer player, EnumHand hand) {
-		{
-			ItemStack itemstack = player.getHeldItem(hand);
-
-			if (!this.isTamed() && TAME_ITEMS.contains(itemstack.getItem())) {
-				if (!player.capabilities.isCreativeMode) {
-					itemstack.shrink(1);
-				}
-
-				if (!this.isSilent()) {
-					this.world.playSound((EntityPlayer) null, this.posX, this.posY, this.posZ, SoundEvents.ENTITY_PARROT_EAT, this.getSoundCategory(), 1.0F, 1.0F + (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F);
-				}
-
-				if (!this.world.isRemote) {
-					if (this.rand.nextInt(10) == 0 && !net.minecraftforge.event.ForgeEventFactory.onAnimalTame(this, player)) {
-						this.setTamedBy(player);
-						this.playTameEffect(true);
-						this.world.setEntityState(this, (byte) 7);
-					} else {
-						this.playTameEffect(false);
-						this.world.setEntityState(this, (byte) 6);
-					}
-				}
-				return true;
-			}
-			return super.processInteract(player, hand);
-		}
-	}
-
-	@Override
-	protected void collideWithEntity(Entity entityIn) {
-		if (!entityIn.equals(getOwner())) {
-			super.collideWithEntity(entityIn);
-		}
 	}
 
 	@Override
