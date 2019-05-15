befc77e32a1ac7f44c4f78bc388464d36365b5b1
Sunconure11 <eyespot7b@aol.com>
2019-05-15 11:48:31 -0400

    Sonata Arctica reference returns
    This song is fitting.

--- src/main/java/com/bewitchment/Bewitchment.java
+++ src/main/java/com/bewitchment/Bewitchment.java
@@ -1,5 +1,28 @@
 package com.bewitchment;
 
+//R DZH SVIV
+//HFMXLMFIV11
+//HKRMLHZFIFH111
+//MLGOVTZOGVMWVI
+//OVHLERPP
+//WZGFIZ
+//GSILFTS GSVHV ORMVH R WL WVXIVV
+//GSZG YB NVIVOB KFGGRMT NB MZNV RM
+//NB RMUOFVMXV TILDH, ZMW GSFH, R YVXLNV RNNLIGZO
+//XBYVIMVGRX DVY
+
+
+//GL HLNV, R ZN YFG Z HSZWV
+//YFG SVIV, R VCVIG KLDVI LEVI GSVN
+//R NZWV Z XZHGOV SVIV LM GSRH SROO
+//ZMW SZEV KFG NB UOZT RM GSV TILFMW
+//GSRH RH NB MVD DLIOW
+
+
+//HSLFOW GSV GIVHKZHHVIH LU GSV LOW DLIOW
+//ZIIREV SVIV, YVZIRMT SLHGRORGRVH
+//GSVB DROO YV NVG DRGS DIZGS
+
 import com.bewitchment.common.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -7,11 +30,15 @@ import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SuppressWarnings({"WeakerAccess", "unused"})
 @Mod(modid = Bewitchment.MODID, name = Bewitchment.NAME, version = Bewitchment.VERSION)
 public class Bewitchment {
 	public static final String MODID = "bewitchment", NAME = "Bewitchment", VERSION = "0.20";
+
+	public static final Logger logger = LogManager.getLogger(NAME);
 
 	@SidedProxy(serverSide = "com.bewitchment.common.CommonProxy", clientSide = "com.bewitchment.client.ClientProxy")
 	public static CommonProxy proxy;
@@ -19,11 +46,20 @@ public class Bewitchment {
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		proxy.preInit(event);
+		logger.info("Remember when I told you how my");
+		logger.info("Kin is different in some ways?");
 	}
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
 		proxy.init(event);
+		logger.info("It's a fact, she is exactly that!");
+		logger.info("A harbinger of death from the world of witchcraft,");
+		logger.info("And she's feeding them cakes and her ale to this innocent boy,");
+		logger.info("And her magic brings dismay!");
+
+		logger.info("I hear her in the wind, the bane of our town");
+		logger.info("Come with me, father, I'm to expose a heathen");
 	}
 
 	@EventHandler
