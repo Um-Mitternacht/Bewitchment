78264fdfe48e32a79100468bdef73b448de539bb
Sunconure11 <eyespot7b@aol.com>
2019-05-23 11:57:19 -0400

    Somewhat functional

--- src/main/java/com/bewitchment/common/item/equipment/baubles/ItemHorseshoe.java
+++ src/main/java/com/bewitchment/common/item/equipment/baubles/ItemHorseshoe.java
@@ -1,12 +1,31 @@
 package com.bewitchment.common.item.equipment.baubles;
 
 import baubles.api.BaubleType;
+import baubles.api.BaublesApi;
+import baubles.api.cap.IBaublesItemHandler;
+import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.common.item.util.ModItemBauble;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Created by Joseph on 5/22/2019.
@@ -16,15 +35,63 @@ public class ItemHorseshoe extends ModItemBauble {
 		super("horseshoe", BaubleType.TRINKET);
 	}
 	
+	private static boolean isSpirit(Entity e) {
+		if (e instanceof EntityLivingBase) {
+			if (((EntityLivingBase) e).getCreatureAttribute() == BewitchmentAPI.getAPI().SPIRIT) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+		if (!world.isRemote) {
+			IBaublesItemHandler baubles = BaublesApi.getBaublesHandler(player);
+			for (int i = 0; i < baubles.getSlots(); i++)
+				if (baubles.getStackInSlot(i).isEmpty() && baubles.isItemValidForSlot(i, player.getHeldItem(hand), player)) {
+					baubles.setStackInSlot(i, player.getHeldItem(hand).copy());
+					if (!player.capabilities.isCreativeMode) {
+						player.setHeldItem(hand, ItemStack.EMPTY);
+					}
+					onEquipped(player.getHeldItem(hand), player);
+					break;
+				}
+		}
+		return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, player.getHeldItem(hand));
+	}
+	
+	@Override
+	public BaubleType getBaubleType(ItemStack itemStack) {
+		return BaubleType.TRINKET;
+	}
+	
+	@Override
+	public void onWornTick(ItemStack itemstack, EntityLivingBase player) {
+		if (itemstack.getItemDamage() == 0 && player.ticksExisted % 40 == 0) {
+			player.addPotionEffect(new PotionEffect(MobEffects.LUCK, 120, 0, true, true));
+		}
+	}
+	
 	@Override
 	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
 		player.playSound(SoundEvents.ITEM_ARMOR_EQUIP_IRON, .75F, 1.9f);
 	}
 	
-	@Override
-	public void onWornTick(ItemStack stack, EntityLivingBase player) {
-		if (player.ticksExisted % 40 == 0) {
-			player.addPotionEffect(new PotionEffect(MobEffects.LUCK, 120, 0, true, true));
+	public String getNameInefficiently(ItemStack stack) {
+		return getTranslationKey().substring(5);
+	}
+	
+	@SubscribeEvent
+	public void onEntityDamage(LivingHurtEvent event) {
+		if (isSpirit(event.getSource().getTrueSource())) {
+			event.setAmount(event.getAmount() * 0.80F);
 		}
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void addInformation(ItemStack stack, @Nullable World player, List<String> tooltip, ITooltipFlag advanced) {
+		tooltip.add(TextFormatting.AQUA + I18n.format("tooltip." + getNameInefficiently(stack) + "_description.name"));
 	}
 }
