af51386e5f386a0861409eedd106809d9ebb5e98
MoriyaShiine <moriyashiine@gmail.com>
2019-05-23 11:27:16 -0500

    healy boi

---
+++ src/main/java/com/bewitchment/common/item/equipment/baubles/ItemTokenOfRemedies.java
@@ -0,0 +1,35 @@
+package com.bewitchment.common.item.equipment.baubles;
+
+import baubles.api.BaubleType;
+import com.bewitchment.common.item.util.ModItemBauble;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.init.MobEffects;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+
+@SuppressWarnings("unused")
+public class ItemTokenOfRemedies extends ModItemBauble {
+	public ItemTokenOfRemedies() {
+		super("token_of_remedies", BaubleType.TRINKET);
+		setMaxDamage(8);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@Override
+	public void onEquipped(ItemStack itemstack, EntityLivingBase player) {
+		player.playSound(SoundEvents.ITEM_ARMOR_EQUIP_IRON, .75F, 1.9f);
+	}
+	
+	@Override
+	public void onWornTick(ItemStack stack, EntityLivingBase player) {
+		if (player.ticksExisted % 40 == 0 && (player.isPotionActive(MobEffects.BLINDNESS) || player.isPotionActive(MobEffects.NAUSEA) || player.isPotionActive(MobEffects.POISON) || player.isPotionActive(MobEffects.WEAKNESS) || player.isPotionActive(MobEffects.WITHER))) {
+			player.removePotionEffect(MobEffects.BLINDNESS);
+			player.removePotionEffect(MobEffects.NAUSEA);
+			player.removePotionEffect(MobEffects.POISON);
+			player.removePotionEffect(MobEffects.WEAKNESS);
+			player.removePotionEffect(MobEffects.WITHER);
+			stack.damageItem(1, player);
+		}
+	}
+}
