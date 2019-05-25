1369f50993c26f1aae8a1c78d75730cb60d68459
MoriyaShiine <moriyashiine@gmail.com>
2019-05-25 15:31:24 -0500

    clean

--- src/main/java/com/bewitchment/common/block/plants/BlockTorchwood.java
+++ src/main/java/com/bewitchment/common/block/plants/BlockTorchwood.java
@@ -1,159 +1,34 @@
 package com.bewitchment.common.block.plants;
 
-import com.bewitchment.registry.ModObjects;
-import net.minecraft.block.Block;
+import com.bewitchment.Util;
 import net.minecraft.block.BlockBush;
-import net.minecraft.block.IGrowable;
 import net.minecraft.block.SoundType;
-import net.minecraft.block.state.BlockFaceShape;
+import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.init.Blocks;
-import net.minecraft.util.BlockRenderLayer;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
-import net.minecraftforge.common.EnumPlantType;
-import net.minecraftforge.common.IPlantable;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
 
-import javax.annotation.Nullable;
 import java.util.Random;
 
 /**
  * Created by Joseph on 5/25/2019.
  */
-public class BlockTorchwood extends BlockBush implements IGrowable, IPlantable {
+@SuppressWarnings("NullableProblems")
+public class BlockTorchwood extends BlockBush {
 	
 	public BlockTorchwood() {
+		super();
+		Util.registerBlock(this, "torchwood", Material.PLANTS, SoundType.WOOD, 0.3f, 0.3f, "", 0);
 		this.setLightLevel(0.7F);
-		this.setTickRandomly(true);
-		setResistance(0.3F);
-		setHardness(0.3F);
-		setSoundType(SoundType.WOOD);
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	public BlockFaceShape getBlockFaceShape(IBlockAccess worldIn, IBlockState state, BlockPos pos, EnumFacing face) {
-		return BlockFaceShape.UNDEFINED;
 	}
 	
 	@Override
-	public boolean isFullCube(IBlockState state) {
-		return false;
-	}
-	
-	@Override
-	public boolean isOpaqueCube(IBlockState state) {
-		return false;
-	}
-	
-	@Override
-	public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
-		return true;
-	}
-	
 	public boolean canSustainBush(IBlockState state) {
-		return state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.STONE || state.getBlock() == Blocks.FARMLAND || state.getBlock() == ModObjects.coquina || state.getBlock() == Blocks.SAND || state.getBlock() == Blocks.GRAVEL || state.getBlock() == Blocks.COBBLESTONE || state.getBlock() == Blocks.NETHERRACK || state.getBlock() == Blocks.END_STONE || state.getBlock() == Blocks.MYCELIUM || state.getBlock() == this;
-	}
-	
-	protected void checkAndDropBlock(World worldIn, BlockPos pos, IBlockState state) {
-		if (!this.canBlockStay(worldIn, pos, state)) {
-			this.dropBlockAsItem(worldIn, pos, state, 0);
-			worldIn.setBlockState(pos, Blocks.AIR.getDefaultState(), 3);
-		}
-	}
-	
-	@SuppressWarnings("deprecation")
-	@Override
-	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
-		super.neighborChanged(state, worldIn, pos, blockIn, fromPos);
-		this.checkAndDropBlock(worldIn, pos, state);
+		return state.getMaterial() == Material.ROCK || state.getMaterial() == Material.GROUND || state.getMaterial() == Material.SAND;
 	}
 	
 	@Override
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		IBlockState soil = worldIn.getBlockState(pos.down());
-		return super.canPlaceBlockAt(worldIn, pos)
-				&& soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
-	}
-	
-	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state) {
-		if (state.getBlock() == this) {
-			IBlockState soil = worldIn.getBlockState(pos.down());
-			return soil.getBlock().canSustainPlant(soil, worldIn, pos.down(), net.minecraft.util.EnumFacing.UP, this);
-		}
-		return this.canSustainBush(worldIn.getBlockState(pos.down()));
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public BlockRenderLayer getRenderLayer() {
-		return BlockRenderLayer.CUTOUT;
-	}
-	
-	@Override
-	public Block.EnumOffsetType getOffsetType() {
-		return Block.EnumOffsetType.XYZ;
-	}
-	
-	@Override
-	@Nullable
-	public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
-		return NULL_AABB;
-	}
-	
-	@Override
-	public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-		return true;
-	}
-	
-	@Override
-	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if (rand.nextInt(25) == 0) {
-			int i = 5;
-			
-			for (BlockPos blockpos : BlockPos.getAllInBoxMutable(pos.add(-4, -1, -4), pos.add(4, 1, 4))) {
-				if (worldIn.getBlockState(blockpos).getBlock() == this) {
-					--i;
-					
-					if (i <= 0) {
-						return;
-					}
-				}
-			}
-			
-			BlockPos blockpos1 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
-			
-			for (int k = 0; k < 4; ++k) {
-				if (worldIn.isAirBlock(blockpos1) && this.canBlockStay(worldIn, blockpos1, this.getDefaultState())) {
-					pos = blockpos1;
-				}
-				
-				blockpos1 = pos.add(rand.nextInt(3) - 1, rand.nextInt(2) - rand.nextInt(2), rand.nextInt(3) - 1);
-			}
-			
-			if (worldIn.isAirBlock(blockpos1) && this.canBlockStay(worldIn, blockpos1, this.getDefaultState())) {
-				worldIn.setBlockState(blockpos1, this.getDefaultState(), 2);
-			}
-		}
-	}
-	
-	@Override
-	public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
-	
-	}
-	
-	@Override
-	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos) {
-		return EnumPlantType.Plains;
-	}
-	
-	@Override
-	public IBlockState getPlant(IBlockAccess world, BlockPos pos) {
-		return this.getDefaultState();
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		super.updateTick(world, pos, state, rand);
 	}
 }
