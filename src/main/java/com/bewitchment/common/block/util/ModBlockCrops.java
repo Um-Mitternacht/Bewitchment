bb5b91da97d4209d5b6039bfad44258c625dc351
MoriyaShiine <moriyashiine@gmail.com>
2019-06-03 11:00:08 -0500

    fix crops not dropping

--- src/main/java/com/bewitchment/common/block/util/ModBlockCrops.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockCrops.java
@@ -1,13 +1,19 @@
 package com.bewitchment.common.block.util;
 
 import com.bewitchment.Util;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 
+@SuppressWarnings("NullableProblems")
 public class ModBlockCrops extends BlockCrops {
 	private Item seed, crop;
 	
@@ -26,6 +32,16 @@ public class ModBlockCrops extends BlockCrops {
 	@Nonnull
 	public Item getCrop() {
 		return crop;
+	}
+	
+	@Override
+	public void neighborChanged(IBlockState state, World world, BlockPos to, Block block, BlockPos from) {
+		super.neighborChanged(state, world, to, block, from);
+		world.notifyBlockUpdate(to, state, state, 3);
+		if (world.getBlockState(to.down()).getBlock() != Blocks.FARMLAND) {
+			dropBlockAsItem(world, to, state, 0);
+			world.setBlockToAir(to);
+		}
 	}
 	
 	public void setItems(Item seed, Item crop) {
