c0691be65de413eed5ff6a000462972fd70b99d6
MoriyaShiine <moriyashiine@gmail.com>
2019-06-04 18:04:42 -0500

    cam how the hell did you find this

--- src/main/java/com/bewitchment/client/render/tile/RenderTileEntityPlacedItem.java
+++ src/main/java/com/bewitchment/client/render/tile/RenderTileEntityPlacedItem.java
@@ -1,6 +1,7 @@
 package com.bewitchment.client.render.tile;
 
 import com.bewitchment.common.block.tile.entity.TileEntityPlacedItem;
+import com.bewitchment.registry.ModObjects;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
@@ -14,15 +15,17 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class RenderTileEntityPlacedItem extends TileEntitySpecialRenderer<TileEntityPlacedItem> {
 	@Override
 	public void render(TileEntityPlacedItem tile, double x, double y, double z, float partialTicks, int destroyStage, float alpha) {
-		ItemStack stack = tile.getInventories()[0].getStackInSlot(0);
-		if (!stack.isEmpty()) {
-			GlStateManager.pushMatrix();
-			GlStateManager.translate(x + 0.5, y + 0.0375, z + 0.5);
-			GlStateManager.rotate(90 * (2 - tile.getWorld().getBlockState(tile.getPos()).getValue(BlockHorizontal.FACING).getOpposite().getHorizontalIndex()), 0, 1, 0);
-			GlStateManager.rotate(90, 1, 0, 0);
-			GlStateManager.scale(0.5, 0.5, 0.5);
-			Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.FIXED);
-			GlStateManager.popMatrix();
+		if (tile.getWorld().getBlockState(tile.getPos()).getBlock() == ModObjects.placed_item) {
+			ItemStack stack = tile.getInventories()[0].getStackInSlot(0);
+			if (!stack.isEmpty()) {
+				GlStateManager.pushMatrix();
+				GlStateManager.translate(x + 0.5, y + 0.0375, z + 0.5);
+				GlStateManager.rotate(90 * (2 - tile.getWorld().getBlockState(tile.getPos()).getValue(BlockHorizontal.FACING).getOpposite().getHorizontalIndex()), 0, 1, 0);
+				GlStateManager.rotate(90, 1, 0, 0);
+				GlStateManager.scale(0.5, 0.5, 0.5);
+				Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.FIXED);
+				GlStateManager.popMatrix();
+			}
 		}
 	}
 }
