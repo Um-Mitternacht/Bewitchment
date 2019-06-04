d5be0450700a95e94a5f5829cf2097d84541d372
MoriyaShiine <moriyashiine@gmail.com>
2019-06-04 13:35:30 -0500

    fix placed item rendering

--- src/main/java/com/bewitchment/client/render/tile/RenderTileEntityPlacedItem.java
+++ src/main/java/com/bewitchment/client/render/tile/RenderTileEntityPlacedItem.java
@@ -4,12 +4,9 @@ import com.bewitchment.common.block.tile.entity.TileEntityPlacedItem;
 import net.minecraft.block.BlockHorizontal;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 import net.minecraft.item.ItemStack;
-import net.minecraftforge.client.ForgeHooksClient;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -20,18 +17,11 @@ public class RenderTileEntityPlacedItem extends TileEntitySpecialRenderer<TileEn
 		ItemStack stack = tile.getInventories()[0].getStackInSlot(0);
 		if (!stack.isEmpty()) {
 			GlStateManager.pushMatrix();
-			GlStateManager.disableLighting();
-			GlStateManager.enableBlend();
-			GlStateManager.translate(x + 0.5, y + 0.1625, z + 0.5);
-			GlStateManager.rotate(90 * (2 - tile.getWorld().getBlockState(tile.getPos()).getValue(BlockHorizontal.FACING).getHorizontalIndex()), 0, 1, 0);
-			GlStateManager.translate(0, -0.125, 0);
-			GlStateManager.rotate(-90, 1, 0, 0);
-			GlStateManager.translate(0, -0.125, 0);
-			IBakedModel model = ForgeHooksClient.handleCameraTransforms(Minecraft.getMinecraft().getRenderItem().getItemModelWithOverrides(stack, tile.getWorld(), null), ItemCameraTransforms.TransformType.GROUND, false);
-			Minecraft.getMinecraft().getTextureManager().bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-			Minecraft.getMinecraft().getRenderItem().renderItem(stack, model);
-			GlStateManager.disableBlend();
-			GlStateManager.enableLighting();
+			GlStateManager.translate(x + 0.5, y + 0.0375, z + 0.5);
+			GlStateManager.rotate(90 * (2 - tile.getWorld().getBlockState(tile.getPos()).getValue(BlockHorizontal.FACING).getOpposite().getHorizontalIndex()), 0, 1, 0);
+			GlStateManager.rotate(90, 1, 0, 0);
+			GlStateManager.scale(0.5, 0.5, 0.5);
+			Minecraft.getMinecraft().getRenderItem().renderItem(stack, ItemCameraTransforms.TransformType.FIXED);
 			GlStateManager.popMatrix();
 		}
 	}
