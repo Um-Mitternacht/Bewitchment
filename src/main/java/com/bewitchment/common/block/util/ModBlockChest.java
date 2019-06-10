53d9a1ded5196b0bbeb3b8c06c4a41bb81d9dc96
Sunconure11 <eyespot7b@aol.com>
2019-06-10 16:17:45 -0400

    Chest progress

--- src/main/java/com/bewitchment/common/block/util/ModBlockChest.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockChest.java
@@ -3,7 +3,11 @@ package com.bewitchment.common.block.util;
 import com.bewitchment.common.block.tile.entity.util.ModTileEntity;
 import net.minecraft.block.BlockChest;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.passive.EntityOcelot;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.items.IItemHandler;
@@ -17,6 +21,31 @@ public class ModBlockChest extends BlockChest {
 	
 	protected ModBlockChest(Type chestTypeIn) {
 		super(chestTypeIn);
+	}
+	
+	@Override
+	public boolean hasTileEntity(IBlockState state) {
+		return true;
+	}
+	
+	private boolean isBlocked(World worldIn, BlockPos pos) {
+		return this.isBelowSolidBlock(worldIn, pos) || this.isOcelotSittingOnChest(worldIn, pos);
+	}
+	
+	private boolean isBelowSolidBlock(World worldIn, BlockPos pos) {
+		return worldIn.getBlockState(pos.up()).doesSideBlockChestOpening(worldIn, pos.up(), EnumFacing.DOWN);
+	}
+	
+	private boolean isOcelotSittingOnChest(World worldIn, BlockPos pos) {
+		for (Entity entity : worldIn.getEntitiesWithinAABB(EntityOcelot.class, new AxisAlignedBB((double) pos.getX(), (double) (pos.getY() + 1), (double) pos.getZ(), (double) (pos.getX() + 1), (double) (pos.getY() + 2), (double) (pos.getZ() + 1)))) {
+			EntityOcelot entityocelot = (EntityOcelot) entity;
+			
+			if (entityocelot.isSitting()) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	@Override
