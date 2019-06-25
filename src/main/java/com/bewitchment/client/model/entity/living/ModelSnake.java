f86493f12c854970476d75230e6f552d4d07341b
Sunconure11 <eyespot7b@aol.com>
2019-06-25 07:53:32 -0400

    Toned down values greatly

--- src/main/java/com/bewitchment/client/model/entity/living/ModelSnake.java
+++ src/main/java/com/bewitchment/client/model/entity/living/ModelSnake.java
@@ -133,13 +133,13 @@ public class ModelSnake extends ModelBase {
 	public void setRotationAngles(float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scaleFactor, Entity entity) {
 		float swingModifier = 0.9f;
 		if (entity instanceof EntityLivingBase) {
-			this.body.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 1.4F * swingModifier * limbSwingAmount;
-			this.tail00.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 1.4F * swingModifier * limbSwingAmount;
-			this.tail01.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * -1.4F * swingModifier * limbSwingAmount;
-			this.tail02.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 1.4F * swingModifier * limbSwingAmount;
-			this.tail03.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * -1.4F * swingModifier * limbSwingAmount;
-			this.tail04.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 1.4F * swingModifier * limbSwingAmount;
-			this.neck00.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 1.4F * swingModifier * limbSwingAmount;
+			this.body.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 0.6F * swingModifier * limbSwingAmount;
+			this.tail00.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 0.6F * swingModifier * limbSwingAmount;
+			this.tail01.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * -0.6F * swingModifier * limbSwingAmount;
+			this.tail02.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 0.6F * swingModifier * limbSwingAmount;
+			this.tail03.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * -0.6F * swingModifier * limbSwingAmount;
+			this.tail04.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 0.6F * swingModifier * limbSwingAmount;
+			this.neck00.rotateAngleY = MathHelper.cos(limbSwing * 0.66F) * 0.6F * swingModifier * limbSwingAmount;
 		}
 	}
 	
