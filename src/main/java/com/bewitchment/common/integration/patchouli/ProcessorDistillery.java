37cac587065bc393cd7715ab2bbdcf8f29aeb560
MoriyaShiine <moriyashiine@gmail.com>
2019-06-29 12:57:11 -0500

    oven + distillery patchy recipes

--- src/main/java/com/bewitchment/common/integration/patchouli/ProcessorDistillery.java
+++ src/main/java/com/bewitchment/common/integration/patchouli/ProcessorDistillery.java
@@ -1,19 +1,19 @@
 package com.bewitchment.common.integration.patchouli;
 
 import com.bewitchment.api.BewitchmentAPI;
-import com.bewitchment.api.registry.SpinningWheelRecipe;
+import com.bewitchment.api.registry.DistilleryRecipe;
 import net.minecraft.util.ResourceLocation;
 import vazkii.patchouli.api.IComponentProcessor;
 import vazkii.patchouli.api.IVariableProvider;
 import vazkii.patchouli.api.PatchouliAPI;
 
 @SuppressWarnings("unused")
-public class ProcessorSpinningWheel implements IComponentProcessor {
-	private SpinningWheelRecipe recipe;
+public class ProcessorDistillery implements IComponentProcessor {
+	private DistilleryRecipe recipe;
 	
 	@Override
 	public void setup(IVariableProvider<String> provider) {
-		recipe = BewitchmentAPI.REGISTRY_SPINNING_WHEEL.getValue(new ResourceLocation(provider.get("recipe")));
+		recipe = BewitchmentAPI.REGISTRY_DISTILLERY.getValue(new ResourceLocation(provider.get("recipe")));
 	}
 	
 	@Override
@@ -23,8 +23,11 @@ public class ProcessorSpinningWheel implements IComponentProcessor {
 			int id = Integer.parseInt(key.substring(5));
 			if (recipe.input.size() > id) return PatchouliAPI.instance.serializeIngredient(recipe.input.get(id));
 		}
-		else if (key.equals("output")) return PatchouliAPI.instance.serializeItemStack(recipe.output);
-		else if (key.equals("name")) return recipe.output.getDisplayName();
+		else if (key.contains("output")) {
+			int id = Integer.parseInt(key.substring(6));
+			if (recipe.output.size() > id) return PatchouliAPI.instance.serializeItemStack(recipe.output.get(id));
+		}
+		else if (key.equals("name")) return recipe.output.get(0).getDisplayName();
 		return null;
 	}
 }
---
+++ src/main/java/com/bewitchment/common/integration/patchouli/ProcessorOven.java
@@ -0,0 +1,28 @@
+package com.bewitchment.common.integration.patchouli;
+
+import com.bewitchment.api.BewitchmentAPI;
+import com.bewitchment.api.registry.OvenRecipe;
+import net.minecraft.util.ResourceLocation;
+import vazkii.patchouli.api.IComponentProcessor;
+import vazkii.patchouli.api.IVariableProvider;
+import vazkii.patchouli.api.PatchouliAPI;
+
+@SuppressWarnings({"unused"})
+public class ProcessorOven implements IComponentProcessor {
+	private OvenRecipe recipe;
+	
+	@Override
+	public void setup(IVariableProvider<String> provider) {
+		recipe = BewitchmentAPI.REGISTRY_OVEN.getValue(new ResourceLocation(provider.get("recipe")));
+	}
+	
+	@Override
+	public String process(String key) {
+		if (recipe == null) return null;
+		else if (key.equals("input")) return PatchouliAPI.instance.serializeItemStack(recipe.input);
+		else if (key.equals("output")) return PatchouliAPI.instance.serializeItemStack(recipe.output);
+		else if (key.equals("byproduct")) return PatchouliAPI.instance.serializeItemStack(recipe.byproduct);
+		else if (key.equals("name")) return recipe.byproduct.getDisplayName();
+		return null;
+	}
+}
