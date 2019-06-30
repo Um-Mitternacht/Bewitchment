9e84df2b3ce3d04142e4d693e05ceef9d51493ac
MoriyaShiine <moriyashiine@gmail.com>
2019-06-30 18:46:02 -0500

    brew patchy

---
+++ src/main/java/com/bewitchment/common/integration/patchouli/ProcessorBrew.java
@@ -0,0 +1,27 @@
+package com.bewitchment.common.integration.patchouli;
+
+import com.bewitchment.api.BewitchmentAPI;
+import com.bewitchment.api.registry.Brew;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.text.TextComponentTranslation;
+import vazkii.patchouli.api.IComponentProcessor;
+import vazkii.patchouli.api.IVariableProvider;
+import vazkii.patchouli.api.PatchouliAPI;
+
+@SuppressWarnings("unused")
+public class ProcessorBrew implements IComponentProcessor {
+	private Brew recipe;
+	
+	@Override
+	public void setup(IVariableProvider<String> provider) {
+		recipe = BewitchmentAPI.REGISTRY_BREW.getValue(new ResourceLocation(provider.get("recipe")));
+	}
+	
+	@Override
+	public String process(String key) {
+		if (recipe == null) return null;
+		else if (key.equals("input")) return PatchouliAPI.instance.serializeIngredient(recipe.input);
+		else if (key.equals("name")) return new TextComponentTranslation(recipe.effect.getEffectName()).getUnformattedComponentText();
+		return null;
+	}
+}
