c8753adf9013e4633d24b8d34b0881c66e738448
Sunconure11 <eyespot7b@aol.com>
2019-07-14 16:53:00 -0400

    whoops

--- src/main/java/com/bewitchment/common/block/tile/entity/TileEntityLeonardIdol.java
+++
@@ -1,8 +0,0 @@
-package com.bewitchment.common.block.tile.entity;
-
-import com.bewitchment.common.block.tile.entity.util.ModTileEntity;
-
-/**
- * Created by Joseph on 7/14/2019.
- */
-public class TileEntityLeonardIdol extends ModTileEntity {}
