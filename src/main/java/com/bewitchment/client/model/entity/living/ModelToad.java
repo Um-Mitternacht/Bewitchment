7d570027e212b3e19293b06e285b4c9ec28946f7
Sunconure11 <eyespot7b@aol.com>
2019-07-19 18:04:23 -0400

    Clean up toad slightly

--- src/main/java/com/bewitchment/client/model/entity/living/ModelToad.java
+++ src/main/java/com/bewitchment/client/model/entity/living/ModelToad.java
@@ -1,11 +1,9 @@
 package com.bewitchment.client.model.entity.living;
 
-import com.bewitchment.common.entity.living.EntityRaven;
 import com.bewitchment.common.entity.living.EntityToad;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.math.MathHelper;
 
 /**
