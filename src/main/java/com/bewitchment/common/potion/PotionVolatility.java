2f0615a2053f95328840af8bcef0a6bd94ba98b4
MoriyaShiine <moriyashiine@gmail.com>
2019-08-12 13:34:37 -0500

    fixes #398

--- src/main/java/com/bewitchment/common/potion/PotionVolatility.java
+++ src/main/java/com/bewitchment/common/potion/PotionVolatility.java
@@ -24,8 +24,12 @@ public class PotionVolatility extends ModPotion {
 		if (!event.getEntityLiving().world.isRemote && event.getEntityLiving().isPotionActive(this) && (event.getSource().isMagicDamage() || !event.getSource().isProjectile())) {
 			event.getEntityLiving().world.playSound(null, event.getEntityLiving().getPosition(), SoundEvents.ENTITY_GENERIC_EXPLODE, SoundCategory.NEUTRAL, 1, 1);
 			Bewitchment.network.sendToDimension(new SpawnParticle(EnumParticleTypes.EXPLOSION_HUGE, event.getEntityLiving().getPosition()), event.getEntityLiving().dimension);
+			int amplifier = event.getEntityLiving().getActivePotionEffect(this).getAmplifier() + 1;
 			for (EntityLivingBase living : event.getEntityLiving().world.getEntitiesWithinAABB(EntityLivingBase.class, event.getEntityLiving().getEntityBoundingBox().grow(3)))
-				living.attackEntityFrom(DamageSource.causeExplosionDamage(event.getEntityLiving()), 4 * (event.getEntityLiving().getActivePotionEffect(this).getAmplifier() + 1));
+			{
+				living.removePotionEffect(this);
+				living.attackEntityFrom(DamageSource.causeExplosionDamage(event.getEntityLiving()), 8 * amplifier);
+			}
 			event.getEntityLiving().removePotionEffect(this);
 		}
 	}
