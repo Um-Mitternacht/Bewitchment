1ba3c0a684b65783d399dcf47cdee0dafb7fa579
Anqing Chen <benjamin.c0427@gmail.com>
2019-08-21 01:25:37 -0500

    moved tarot functions to crystal ball

--- src/main/java/com/bewitchment/api/BewitchmentAPI.java
+++ src/main/java/com/bewitchment/api/BewitchmentAPI.java
@@ -1,6 +1,7 @@
 package com.bewitchment.api;
 
 import com.bewitchment.Bewitchment;
+import com.bewitchment.api.capability.extendedplayer.ExtendedPlayer;
 import com.bewitchment.api.registry.AltarUpgrade;
 import net.minecraft.block.state.BlockWorldState;
 import net.minecraft.entity.EntityLivingBase;
@@ -10,6 +11,7 @@ import net.minecraft.entity.monster.EntityVex;
 import net.minecraft.entity.monster.EntityWitch;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.fml.common.registry.EntityEntry;
@@ -17,7 +19,7 @@ import net.minecraftforge.fml.common.registry.EntityEntry;
 import java.util.*;
 import java.util.function.Predicate;
 
-@SuppressWarnings({"unused", "SameReturnValue"})
+@SuppressWarnings({"unused", "SameReturnValue", "ConstantConditions"})
 public class BewitchmentAPI {
 	/**
 	 * a Map of loot for mobs to drop when killed with the Athame
@@ -73,6 +75,50 @@ public class BewitchmentAPI {
 	 * @return false always, spectres are not currently in the mod
 	 */
 	public static boolean isSpectre(EntityLivingBase entity) {
+		return false;
+	}
+
+	/**
+	 * @param entity the EntityPlayer to check
+	 * @return false always, has pet check WIP
+	 */
+	public static boolean hasPets(EntityPlayer entity) {
+		return (entity.getCapability(ExtendedPlayer.CAPABILITY, null)).pets > 0;
+	}
+
+	/**
+	 * @param entity the entity to check
+	 * @return if player defeated at least one boss
+	 */
+	public static boolean defeatedBoss(EntityPlayer entity) {
+		return !(entity.getCapability(ExtendedPlayer.CAPABILITY, null)).uniqueDefeatedBosses.isEmpty();
+	}
+
+
+	/**
+	 * @param entity the entity to check
+	 * @return false always, infusion is not currently in the mod
+	 */
+	public static boolean isInfused(EntityPlayer entity) {
+		return false;
+	}
+
+	/**
+	 * @param entity the entity to check
+	 * @return true when there's active effect not provided by beacon, vice versa
+	 */
+	public static boolean hasEffects(EntityPlayer entity) {
+		for(PotionEffect potion: entity.getActivePotionEffects()) {
+			if(!potion.getIsAmbient()) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * @param entity the entity to check
+	 * @return false always, poppets are not currently in the mod
+	 */
+	public static boolean hasPoppets(EntityPlayer entity) {
 		return false;
 	}
 	
