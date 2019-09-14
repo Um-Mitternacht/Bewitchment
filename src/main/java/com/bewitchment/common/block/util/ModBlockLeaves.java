3a11abefe9b16963c34e5470015cba988c11926c
Anqing Chen <benjamin.c0427@gmail.com>
2019-09-13 19:07:16 -0500

    fix #425

--- src/main/java/com/bewitchment/common/block/util/ModBlockLeaves.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockLeaves.java
@@ -2,6 +2,7 @@ package com.bewitchment.common.block.util;
 
 import com.bewitchment.Bewitchment;
 import com.bewitchment.Util;
+import com.bewitchment.registry.ModObjects;
 import net.minecraft.block.BlockLeaves;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.SoundType;
@@ -9,6 +10,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemShears;
 import net.minecraft.item.ItemStack;
 import net.minecraft.stats.StatList;
@@ -19,11 +21,15 @@ import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.List;
+import java.util.Random;
 
 @SuppressWarnings({"deprecation", "NullableProblems", "ConstantConditions"})
 public class ModBlockLeaves extends BlockLeaves {
+	private final String name;
+
 	public ModBlockLeaves(String name, String... oreDictionaryNames) {
 		super();
+		this.name = name;
 		Util.registerBlock(this, name, Material.LEAVES, SoundType.PLANT, 0.2f, 0, "shears", 0, oreDictionaryNames);
 		setDefaultState(this.getBlockState().getBaseState().withProperty(CHECK_DECAY, true).withProperty(DECAYABLE, true));
 	}
@@ -52,7 +58,22 @@ public class ModBlockLeaves extends BlockLeaves {
 		}
 		else super.harvestBlock(world, player, pos, state, tile, stack);
 	}
-	
+
+	@Override
+	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+		switch(name) {
+			case "cypress_leaves":
+				return Item.getItemFromBlock(ModObjects.cypress_sapling);
+			case "juniper_leaves":
+				return Item.getItemFromBlock(ModObjects.juniper_sapling);
+			case "dragons_blood_leaves":
+				return Item.getItemFromBlock(ModObjects.dragons_blood_sapling);
+			case "elder_leaves":
+				return Item.getItemFromBlock(ModObjects.elder_sapling);
+		}
+		return super.getItemDropped(state, rand, fortune);
+	}
+
 	@Override
 	public IBlockState getStateFromMeta(int meta) {
 		return getDefaultState().withProperty(DECAYABLE, ((meta) & 1) == 1).withProperty(CHECK_DECAY, ((meta) & 2) > 0);
