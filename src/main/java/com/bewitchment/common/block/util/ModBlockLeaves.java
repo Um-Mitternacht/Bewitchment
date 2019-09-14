1ae2ae7bfda0ef5407c9abf442151f1592db787f
Anqing Chen <benjamin.c0427@gmail.com>
2019-09-13 19:14:00 -0500

    minor tweak

--- src/main/java/com/bewitchment/common/block/util/ModBlockLeaves.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockLeaves.java
@@ -25,11 +25,8 @@ import java.util.Random;
 
 @SuppressWarnings({"deprecation", "NullableProblems", "ConstantConditions"})
 public class ModBlockLeaves extends BlockLeaves {
-	private final String name;
-
 	public ModBlockLeaves(String name, String... oreDictionaryNames) {
 		super();
-		this.name = name;
 		Util.registerBlock(this, name, Material.LEAVES, SoundType.PLANT, 0.2f, 0, "shears", 0, oreDictionaryNames);
 		setDefaultState(this.getBlockState().getBaseState().withProperty(CHECK_DECAY, true).withProperty(DECAYABLE, true));
 	}
@@ -61,16 +58,10 @@ public class ModBlockLeaves extends BlockLeaves {
 
 	@Override
 	public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-		switch(name) {
-			case "cypress_leaves":
-				return Item.getItemFromBlock(ModObjects.cypress_sapling);
-			case "juniper_leaves":
-				return Item.getItemFromBlock(ModObjects.juniper_sapling);
-			case "dragons_blood_leaves":
-				return Item.getItemFromBlock(ModObjects.dragons_blood_sapling);
-			case "elder_leaves":
-				return Item.getItemFromBlock(ModObjects.elder_sapling);
-		}
+		if (this == ModObjects.cypress_leaves) return Item.getItemFromBlock(ModObjects.cypress_sapling);
+		if (this == ModObjects.juniper_leaves) return Item.getItemFromBlock(ModObjects.juniper_sapling);
+		if (this == ModObjects.elder_leaves) return Item.getItemFromBlock(ModObjects.elder_sapling);
+		if (this == ModObjects.dragons_blood_leaves) return Item.getItemFromBlock(ModObjects.dragons_blood_sapling);
 		return super.getItemDropped(state, rand, fortune);
 	}
 
