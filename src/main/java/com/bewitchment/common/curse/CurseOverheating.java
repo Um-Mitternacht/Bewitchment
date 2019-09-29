c276fac1e0bc884fc0ee45c969ba9f2f95cf642c
Anqing Chen <benjamin.c0427@gmail.com>
2019-09-29 02:06:57 -0500

    curse of overheating

---
+++ src/main/java/com/bewitchment/common/curse/CurseOverheating.java
@@ -0,0 +1,31 @@
+package com.bewitchment.common.curse;
+
+import com.bewitchment.Bewitchment;
+import com.bewitchment.Util;
+import com.bewitchment.api.registry.Curse;
+import com.bewitchment.registry.ModObjects;
+import com.bewitchment.registry.ModPotions;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.common.BiomeDictionary;
+
+import java.util.Arrays;
+
+public class CurseOverheating extends Curse {
+	public CurseOverheating() {
+		super(new ResourceLocation(Bewitchment.MODID, "overheating"), Arrays.asList(Util.get(ModObjects.oil_of_vitriol), Util.get(ModObjects.fiery_unguent), Util.get(ModObjects.tallow), Util.get(Items.BONE), Util.get(Items.BONE), Util.get(ModObjects.taglock)), true, CurseCondition.RANDOM, 0.001);
+	}
+
+	@Override
+	public boolean doCurse(EntityPlayer target) {
+		BlockPos pos = target.getPosition();
+		if (BiomeDictionary.hasType(target.world.getBiome(pos), BiomeDictionary.Type.HOT)) {
+			target.setFire(5);
+			target.addPotionEffect(new PotionEffect(ModPotions.hellfire, 200, this.getLevel()));
+		}
+		return false;
+	}
+}
--- src/main/java/com/bewitchment/registry/ModCurses.java
+++ src/main/java/com/bewitchment/registry/ModCurses.java
@@ -1,14 +1,12 @@
 package com.bewitchment.registry;
 
 import com.bewitchment.api.registry.Curse;
-import com.bewitchment.common.curse.CurseConflagration;
-import com.bewitchment.common.curse.CurseMisfortune;
-import com.bewitchment.common.curse.CurseReturnToSender;
-import com.bewitchment.common.curse.CurseSolarHatred;
+import com.bewitchment.common.curse.*;
 
 public class ModCurses {
 	public static Curse curseReturnToSender = new CurseReturnToSender();
 	public static Curse curseMisfortune = new CurseMisfortune();
 	public static Curse curseConflagration = new CurseConflagration();
 	public static Curse curseSolarHatred = new CurseSolarHatred();
+	public static Curse curseOverheating = new CurseOverheating();
 }
