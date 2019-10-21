bc9f3c3db18c2542a144efe5336d6caad1d0517a
Sunconure11 <eyespot7b@aol.com>
2019-10-21 19:55:32 -0400

    Shadow people death timer

--- src/main/java/com/bewitchment/common/entity/spirit/demon/EntityShadowPersonStandard.java
+++ src/main/java/com/bewitchment/common/entity/spirit/demon/EntityShadowPersonStandard.java
@@ -4,13 +4,22 @@ import com.bewitchment.Bewitchment;
 import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.common.entity.util.ModEntityMob;
 import net.minecraft.entity.EnumCreatureAttribute;
+import net.minecraft.entity.IEntityLivingData;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Joseph on 10/21/2019.
  */
 public class EntityShadowPersonStandard extends ModEntityMob {
+	
+	private int livingTimer = 0;
 	
 	
 	public EntityShadowPersonStandard(World world) {
@@ -22,6 +31,31 @@ public class EntityShadowPersonStandard extends ModEntityMob {
 	@Override
 	public EnumCreatureAttribute getCreatureAttribute() {
 		return BewitchmentAPI.DEMON;
+	}
+	
+	@Override
+	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, @Nullable IEntityLivingData data) {
+		livingTimer = 600;
+		return super.onInitialSpawn(difficulty, data);
+	}
+	
+	@Override
+	public void onLivingUpdate() {
+		super.onLivingUpdate();
+		livingTimer--;
+		if (livingTimer <= 0) setDead();
+	}
+	
+	@Override
+	public void writeEntityToNBT(NBTTagCompound tag) {
+		tag.setInteger("livingTimer", livingTimer);
+		super.writeEntityToNBT(tag);
+	}
+	
+	@Override
+	public void readEntityFromNBT(NBTTagCompound tag) {
+		if (tag.hasKey("livingTimer")) livingTimer = tag.getInteger("livingTimer");
+		super.readEntityFromNBT(tag);
 	}
 	
 	@Override
