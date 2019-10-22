c37a0376e6080fb449f271396740f1d390f23109
Anqing Chen <benjamin.c0427@gmail.com>
2019-10-22 12:03:14 -0500

    frostflower/blue_ink_cap now only spread at night, recipe for dragons' blood broom

--- src/main/java/com/bewitchment/common/block/plants/BlockFrostFlower.java
+++ src/main/java/com/bewitchment/common/block/plants/BlockFrostFlower.java
@@ -1,9 +1,21 @@
 package com.bewitchment.common.block.plants;
 
 import com.bewitchment.common.block.plants.util.BlockBushSpreading;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import java.util.Random;
 
 public class BlockFrostFlower extends BlockBushSpreading {
 	public BlockFrostFlower() {
 		super("frostflower");
+	}
+
+	@Override
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		if (world.getWorldTime() >= 12500) {
+			super.updateTick(world, pos, state, rand);
+		}
 	}
 }
