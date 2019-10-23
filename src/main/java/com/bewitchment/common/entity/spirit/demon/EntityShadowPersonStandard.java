4fc5bcc87d0eb0269393b67b0f17080b43ac57a6
Sunconure11 <eyespot7b@aol.com>
2019-10-23 05:37:20 -0400

    Update EntityShadowPersonStandard.java

--- src/main/java/com/bewitchment/common/entity/spirit/demon/EntityShadowPersonStandard.java
+++ src/main/java/com/bewitchment/common/entity/spirit/demon/EntityShadowPersonStandard.java
@@ -76,7 +76,7 @@ public class EntityShadowPersonStandard extends ModEntityMob {
 	
 	@Override
 	protected boolean isValidLightLevel() {
-		return !world.isDaytime();
+		return true;
 	}
 	
 	@Override
