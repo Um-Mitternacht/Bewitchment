c63d05a60f5d347eaa8cee2daadde80c48dda9c8
Sunconure11 <eyespot7b@aol.com>
2019-11-08 21:07:28 -0500

    Update ModelBaphomet.java

--- src/main/java/com/bewitchment/client/model/entity/spirit/demon/ModelBaphomet.java
+++ src/main/java/com/bewitchment/client/model/entity/spirit/demon/ModelBaphomet.java
@@ -494,11 +494,11 @@ public class ModelBaphomet extends ModelBiped {
 			this.bipedHead.rotateAngleX = headPitch * 0.017453292F;
 		}
 		float swingMod = 0.8F;
-		this.bipedLeftLeg.rotateAngleX = MathHelper.sin(limbSwing * 0.8665F + (float) Math.PI) * swingMod * limbSwingAmount - 0.26F;
-		this.bipedRightLeg.rotateAngleX = MathHelper.cos(limbSwing * 0.8665F) * swingMod * limbSwingAmount - 0.26F;
+		this.leftLeg00.rotateAngleX = MathHelper.sin(limbSwing * 0.8665F + (float) Math.PI) * swingMod * limbSwingAmount - 0.26F;
+		this.rightLeg00.rotateAngleX = MathHelper.cos(limbSwing * 0.8665F) * swingMod * limbSwingAmount - 0.26F;
 		
-		this.bipedRightArm.rotateAngleX = MathHelper.sin(limbSwing * 0.8665F + (float) Math.PI) * swingMod * limbSwingAmount;
-		this.bipedLeftArm.rotateAngleX = MathHelper.cos(limbSwing * 0.8665F) * swingMod * limbSwingAmount;
+		this.rightArm00.rotateAngleX = MathHelper.sin(limbSwing * 0.8665F + (float) Math.PI) * swingMod * limbSwingAmount;
+		this.leftArm00.rotateAngleX = MathHelper.cos(limbSwing * 0.8665F) * swingMod * limbSwingAmount;
 		
 		
 		setLivingAnimations((EntityLivingBase) entity, limbSwing, limbSwingAmount, Minecraft.getMinecraft().getRenderPartialTicks());
