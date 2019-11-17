fbab294cddad2719cfec3072f0dc56591b6a6679
samaritans <benjamin.c0427@gmail.com>
2019-11-17 14:32:14 -0600

    mixin startup

---
+++ src/main/java/com/bewitchment/common/MixinEntityTippedArrow.java
@@ -0,0 +1,9 @@
+package com.bewitchment.common;
+
+import net.minecraft.entity.projectile.EntityTippedArrow;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(EntityTippedArrow.class)
+public class MixinEntityTippedArrow {
+
+}
