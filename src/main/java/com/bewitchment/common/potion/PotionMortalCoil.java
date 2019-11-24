fe86afaac7745f5837e5b96ac221b2c784150e81
samaritans <benjamin.c0427@gmail.com>
2019-11-24 02:05:02 -0600

    mortal coil icon

--- src/main/java/com/bewitchment/common/potion/PotionMortalCoil.java
+++ src/main/java/com/bewitchment/common/potion/PotionMortalCoil.java
@@ -1,14 +1,22 @@
 package com.bewitchment.common.potion;
 
+import com.bewitchment.Bewitchment;
 import com.bewitchment.common.potion.util.ModPotion;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @SuppressWarnings({"ConstantConditions", "unused"})
 public class PotionMortalCoil extends ModPotion {
+	private static final ResourceLocation icon = new ResourceLocation(Bewitchment.MODID, "textures/gui/effect/mortal_coil.png");
 	public PotionMortalCoil() {
 		super("mortal_coil", true, 0x4B3621);
 		MinecraftForge.EVENT_BUS.register(this);
@@ -20,5 +28,19 @@ public class PotionMortalCoil extends ModPotion {
 			if (event.getEntityLiving() instanceof EntityPlayer) event.getEntityLiving().attackEntityFrom(DamageSource.WITHER, Integer.MAX_VALUE);
 			else event.getEntityLiving().setDead();
 		}
+	}
+
+	@Override
+	@SideOnly(Side.CLIENT)
+	public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+		mc.getTextureManager().bindTexture(icon);
+		Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 18, 18, 18, 18);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public void renderHUDEffect(int x, int y, PotionEffect effect, Minecraft mc, float alpha) {
+		mc.getTextureManager().bindTexture(icon);
+		Gui.drawModalRectWithCustomSizedTexture(x + 3, y + 3, 0, 0, 18, 18, 18, 18);
 	}
 }
