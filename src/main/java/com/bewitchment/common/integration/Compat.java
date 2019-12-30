96696deb8a81d0e6d83cd9703aa4c3d17cec41b4
samaritans <benjamin.c0427@gmail.com>
2019-12-29 23:11:11 -0600

    remove rustic from workspace

--- src/main/java/com/bewitchment/common/integration/Compat.java
+++ src/main/java/com/bewitchment/common/integration/Compat.java
@@ -16,8 +16,6 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import rustic.common.items.ModItems;
-import rustic.common.potions.PotionsRustic;
 import vazkii.quark.base.module.ModuleLoader;
 import vazkii.quark.world.feature.*;
 
@@ -64,12 +62,5 @@ public class Compat {
 			event.getRegistry().register(new Brew(new ResourceLocation(Bewitchment.MODID, "whitening"), Util.get(new ItemStack(root_flower, 1, 2)), new PotionEffect(white_effect, (20 * 30))));
 		}
 		if (ModuleLoader.isFeatureEnabled(Crabs.class)) event.getRegistry().register(new Brew(new ResourceLocation(Bewitchment.MODID, "stability"), Util.get(crabShell), new PotionEffect(resilience, (20 * 30))));
-	}
-	
-	@Optional.Method(modid = "rustic")
-	@SubscribeEvent
-	public void registerBrew(RegistryEvent.Register<Brew> event) {
-		event.getRegistry().register(new Brew(new ResourceLocation(Bewitchment.MODID, "blazing_trail"), Util.get(ModItems.CHILI_PEPPER), new PotionEffect(PotionsRustic.BLAZING_TRAIL_POTION, (20 * 30))));
-		event.getRegistry().register(new Brew(new ResourceLocation(Bewitchment.MODID, "iron_skin"), Util.get(ModItems.IRONBERRIES), new PotionEffect(PotionsRustic.IRON_SKIN_POTION, (20 * 30))));
 	}
 }
