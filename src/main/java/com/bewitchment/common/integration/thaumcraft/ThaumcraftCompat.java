bffcda1e5dd2cb79e25fdb7db7023dddbea7ddae
Sunconure11 <eyespot7b@aol.com>
2020-01-16 16:02:14 -0500

    Update ThaumcraftCompat.java

--- src/main/java/com/bewitchment/common/integration/thaumcraft/ThaumcraftCompat.java
+++ src/main/java/com/bewitchment/common/integration/thaumcraft/ThaumcraftCompat.java
@@ -60,7 +60,7 @@ public class ThaumcraftCompat {
 		return golem instanceof EntityThaumcraftGolem && ((EntityThaumcraftGolem) golem).getProperties().hasTrait(UNCANNY);
 	}
 	
-	//Todo: Only inflict slowness, no other special effects. Apply slowness to all mobs attacked by this.
+	//Todo: Apply slowness to mobs attacked by this + make mobs flee it's presence.
 	@SubscribeEvent
 	public void handleDragonsBloodGolems(LivingHurtEvent event) {
 		EntityLivingBase entity = event.getEntityLiving();
