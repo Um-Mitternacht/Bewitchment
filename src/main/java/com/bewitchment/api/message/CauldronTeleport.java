3989d19468c639715d429aa4112d622ec3133f39
Sunconure11 <eyespot7b@aol.com>
2020-01-19 12:09:56 -0500

    Reformat

--- src/main/java/com/bewitchment/api/message/CauldronTeleport.java
+++ src/main/java/com/bewitchment/api/message/CauldronTeleport.java
@@ -7,7 +7,6 @@ import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.SoundEvents;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
