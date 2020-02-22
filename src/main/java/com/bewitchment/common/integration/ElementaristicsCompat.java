10e4ff1efeb4f6d6a9433ed76d6fdf5f1bad3efa
Sunconure11 <eyespot7b@aol.com>
2020-02-22 13:39:15 -0500

    Update ElementaristicsCompat.java

--- src/main/java/com/bewitchment/common/integration/ElementaristicsCompat.java
+++ src/main/java/com/bewitchment/common/integration/ElementaristicsCompat.java
@@ -11,5 +11,9 @@ public class ElementaristicsCompat {
 	
 	public static void registerElementaristicsStuff() {
 		Util.registerAltarUpgradeItem(de.aelpecyem.elementaristics.init.ModItems.thaumagral_wood, new AltarUpgrade(AltarUpgrade.Type.SWORD, 0, 1.24));
+		Util.registerAltarUpgradeItem(de.aelpecyem.elementaristics.init.ModItems.thaumagral_stone, new AltarUpgrade(AltarUpgrade.Type.SWORD, 0, 1.29));
+		Util.registerAltarUpgradeItem(de.aelpecyem.elementaristics.init.ModItems.thaumagral_iron, new AltarUpgrade(AltarUpgrade.Type.SWORD, 0, 1.35));
+		Util.registerAltarUpgradeItem(de.aelpecyem.elementaristics.init.ModItems.thaumagral_gold, new AltarUpgrade(AltarUpgrade.Type.SWORD, 0, 1.45));
+		Util.registerAltarUpgradeItem(de.aelpecyem.elementaristics.init.ModItems.thaumagral_diamond, new AltarUpgrade(AltarUpgrade.Type.SWORD, 0, 1.43));
 	}
 }
