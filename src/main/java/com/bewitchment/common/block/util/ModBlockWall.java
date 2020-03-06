1754cc8c4ea4202c7ac60cd2847d43ef07c06932
Sunconure11 <eyespot7b@aol.com>
2020-03-06 06:41:40 -0500

    Update ModBlockWall.java

--- src/main/java/com/bewitchment/common/block/util/ModBlockWall.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockWall.java
@@ -3,6 +3,13 @@ package com.bewitchment.common.block.util;
 import com.bewitchment.Util;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 /**
  * Created by Joseph on 3/6/2020.
@@ -11,7 +18,18 @@ import net.minecraft.block.BlockWall;
 @SuppressWarnings({"deprecation", "NullableProblems"})
 public class ModBlockWall extends BlockWall {
 	public ModBlockWall(String name, Block base, String... oreDictionaryNames) {
-		super(base.getMaterial(this));
+		super(base);
 		Util.registerBlock(this, name, base, oreDictionaryNames);
+	}
+	
+	@Override
+	@SideOnly(Side.CLIENT)
+	public BlockRenderLayer getRenderLayer() {
+		return Util.isTransparent(getDefaultState()) ? BlockRenderLayer.TRANSLUCENT : BlockRenderLayer.CUTOUT;
+	}
+	
+	@Override
+	public boolean shouldSideBeRendered(IBlockState state, IBlockAccess world, BlockPos pos, EnumFacing face) {
+		return (!Util.isTransparent(state) || world.getBlockState(pos.offset(face)).getBlock() != this) && super.shouldSideBeRendered(state, world, pos, face);
 	}
 }
