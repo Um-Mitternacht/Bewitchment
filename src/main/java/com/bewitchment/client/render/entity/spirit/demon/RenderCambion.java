a14f143f10722fd4eae68e48b10b01bc06556433
Sunconure11 <eyespot7b@aol.com>
2020-03-31 20:43:12 -0400

    Clean up some garbage

--- src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderCambion.java
+++ src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderCambion.java
@@ -1,18 +1,15 @@
 package com.bewitchment.client.render.entity.spirit.demon;
 
 import com.bewitchment.Bewitchment;
-import com.bewitchment.client.model.entity.spirit.demon.*;
+import com.bewitchment.client.model.entity.spirit.demon.ModelCambion;
+import com.bewitchment.client.model.entity.spirit.demon.ModelCambionSlim;
 import com.bewitchment.common.entity.spirit.demon.EntityCambion;
-import com.bewitchment.common.entity.spirit.demon.EntityImp;
-import com.bewitchment.common.entity.spirit.demon.EntityShadowPerson;
 import com.bewitchment.common.entity.util.ModEntityMob;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.util.ResourceLocation;
-
-import javax.annotation.Nullable;
 
 /**
  * Created by Joseph on 3/30/2020.
