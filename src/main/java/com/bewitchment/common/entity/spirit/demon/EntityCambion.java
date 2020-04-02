5d0322d12f8cd43130730e16c8f67a344b6ce3f8
Sunconure11 <eyespot7b@aol.com>
2020-04-02 04:53:19 -0400

    Cambion stuff

--- src/main/java/com/bewitchment/common/entity/spirit/demon/EntityCambion.java
+++ src/main/java/com/bewitchment/common/entity/spirit/demon/EntityCambion.java
@@ -3,24 +3,36 @@ package com.bewitchment.common.entity.spirit.demon;
 import com.bewitchment.Bewitchment;
 import com.bewitchment.api.BewitchmentAPI;
 import com.bewitchment.common.entity.util.ModEntityMob;
+import com.bewitchment.registry.ModObjects;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.init.Items;
+import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemShield;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemSword;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.World;
 
 /**
  * Created by Joseph on 3/30/2020.
  */
 public class EntityCambion extends ModEntityMob {
+	public int attackTimer = 0;
+	
 	protected EntityCambion(World world) {
 		super(world, new ResourceLocation(Bewitchment.MODID, "entities/cambion"));
-	}
-	
-	protected SoundEvent getAmbientSound() {
-		return SoundEvents.ENTITY_EVOCATION_ILLAGER_AMBIENT;
+		experienceValue = 25;
 	}
 	
 	protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
@@ -29,6 +41,42 @@ public class EntityCambion extends ModEntityMob {
 	
 	protected SoundEvent getDeathSound() {
 		return SoundEvents.EVOCATION_ILLAGER_DEATH;
+	}
+	
+	@Override
+	public boolean attackEntityAsMob(Entity entity) {
+		boolean flag = super.attackEntityAsMob(entity);
+		if (flag) {
+			attackTimer = 10;
+			int i = this.rand.nextInt(100);
+			world.setEntityState(this, (byte) 4);
+			if (entity instanceof EntityLivingBase) {
+				if (i < 10) {
+					((EntityLivingBase) entity).addPotionEffect(new PotionEffect(MobEffects.POISON, 500, 0, false, false));
+				}
+				else if (i < 10) {
+					entity.setFire(25);
+				}
+			}
+		}
+		return flag;
+	}
+	
+	@Override
+	protected void updateEquipmentIfNeeded(EntityItem itemEntity) {
+		if (itemEntity.getItem().getItem() instanceof ItemSword || itemEntity.getItem().getItem() instanceof ItemArmor || itemEntity.getItem().getItem() instanceof ItemShield) {
+			super.updateEquipmentIfNeeded(itemEntity);
+		}
+	}
+	
+	@Override
+	protected void setEquipmentBasedOnDifficulty(DifficultyInstance difficulty) {
+		super.setEquipmentBasedOnDifficulty(difficulty);
+		this.setItemStackToSlot(EntityEquipmentSlot.MAINHAND, new ItemStack(Items.IRON_SWORD));
+		this.setItemStackToSlot(EntityEquipmentSlot.HEAD, new ItemStack(Items.LEATHER_HELMET));
+		this.setItemStackToSlot(EntityEquipmentSlot.CHEST, new ItemStack(Items.LEATHER_CHESTPLATE));
+		this.setItemStackToSlot(EntityEquipmentSlot.LEGS, new ItemStack(Items.LEATHER_LEGGINGS));
+		this.setItemStackToSlot(EntityEquipmentSlot.FEET, new ItemStack(Items.LEATHER_BOOTS));
 	}
 	
 	@Override
@@ -41,6 +89,16 @@ public class EntityCambion extends ModEntityMob {
 		getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(35);
 		getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.6D);
 		getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(0.2D);
+	}
+	
+	@Override
+	public void handleStatusUpdate(byte id) {
+		if (id == 4) attackTimer = 10;
+		else super.handleStatusUpdate(id);
+	}
+	
+	protected SoundEvent getAmbientSound() {
+		return SoundEvents.ENTITY_EVOCATION_ILLAGER_AMBIENT;
 	}
 	
 	@Override
