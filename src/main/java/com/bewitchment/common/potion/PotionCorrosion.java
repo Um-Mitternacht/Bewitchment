723a127006439b869f96d0c92fc4a819468b6462
Sunconure11 <eyespot7b@aol.com>
2020-04-02 13:45:46 -0400

    Fear brews now exist, but are costly

---
+++ src/main/java/com/bewitchment/common/potion/PotionCorrosion.java
@@ -0,0 +1,22 @@
+package com.bewitchment.common.potion;
+
+import com.bewitchment.common.potion.util.ModPotion;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+/**
+ * Created by Joseph on 4/2/2020.
+ */
+public class PotionCorrosion extends ModPotion {
+	protected PotionCorrosion(String name, boolean isNegative, int color) {
+		super("corrosion", true, 0x0BDA51);
+	}
+	
+	@SubscribeEvent
+	public void onLivingTick(LivingEvent.LivingUpdateEvent event) {
+		if (!event.getEntityLiving().world.isRemote && event.getEntityLiving().isPotionActive(this)) {
+			event.getEntityLiving().getTotalArmorValue();
+		}
+	}
+}
