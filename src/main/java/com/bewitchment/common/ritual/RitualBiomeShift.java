010f837d654050da4c07b14b69866d9f66813476
Sunconure11 <eyespot7b@aol.com>
2020-04-07 06:55:59 -0400

    Working on adapting zabi code to this workspace

--- src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
+++ src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
@@ -4,6 +4,9 @@ import com.bewitchment.Bewitchment;
 import com.bewitchment.Util;
 import com.bewitchment.api.registry.Ritual;
 import com.bewitchment.common.block.BlockGlyph;
+import com.bewitchment.common.block.tile.entity.TileEntityGlyph;
+import com.bewitchment.common.item.ItemWaystone;
+import com.bewitchment.common.item.tool.ItemBoline;
 import com.bewitchment.common.world.BiomeChangingUtils;
 import com.bewitchment.registry.ModObjects;
 import net.minecraft.entity.player.EntityPlayer;
@@ -27,35 +30,36 @@ import java.util.List;
  */
 public class RitualBiomeShift extends Ritual {
 	public RitualBiomeShift() {
-		super(new ResourceLocation(Bewitchment.MODID, "biome_shift"), Arrays.asList(Util.get("treeSapling"), Util.get("logWood"), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.pentacle), Util.get(new ItemStack(ModObjects.oak_spirit)), Util.get(new ItemStack(ModObjects.boline))), null, null, 200, 1300, 33, BlockGlyph.NORMAL, BlockGlyph.NORMAL, BlockGlyph.NORMAL);
+		super(new ResourceLocation(Bewitchment.MODID, "biome_shift"), Arrays.asList(Util.get("treeSapling"), Util.get("logWood"), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.pentacle), Util.get(new ItemStack(ModObjects.oak_spirit)), Util.get(new ItemStack(ModObjects.boline))), null, null, 50, 1300, 33, BlockGlyph.NORMAL, BlockGlyph.NORMAL, BlockGlyph.NORMAL);
 	}
 	
 	@Override
 	public void onFinished(World world, BlockPos altarPos, BlockPos effectivePos, EntityPlayer caster, ItemStackHandler inventory) {
-		List<Ingredient> recipe = input;
-		EnumHand hand = EnumHand.MAIN_HAND;
-		ItemStack stack = caster.getHeldItem(hand);
 		int id = Biome.getIdForBiome(Biomes.PLAINS);
-		for (Ingredient is : recipe) {
-			if (stack.getItem() == ModObjects.boline && stack.hasTagCompound() && stack.getTagCompound().hasKey("biome_id")) {
-				id = stack.getTagCompound().getInteger("biome_id");
-			}
-		}
-		int radius = (int) MathHelper.sqrt(64 * (1 + 2) * (1 + 2));
-		BiomeChangingUtils.BiomeChangerWalker walker = new BiomeChangingUtils.BiomeChangerWalker(id);
-		BlockPos.MutableBlockPos mpos = new BlockPos.MutableBlockPos();
-		mpos.setPos(effectivePos.getX() - radius, 0, effectivePos.getZ() - radius);
-		for (int x = -radius; x <= radius; x++) {
-			for (int z = -radius; z <= radius; z++) {
-				if (x * x + z * z <= radius * radius) {
-					walker.visit(world, mpos);
+		if (world.getTileEntity(effectivePos) instanceof TileEntityGlyph) {
+			for (int i = 0; i < inventory.getSlots(); i++) {
+				ItemStack stack = inventory.getStackInSlot(i);
+				if (stack.getItem() instanceof ItemBoline) {
+					id = stack.getTagCompound().getInteger("biome_id");
+					stack.damageItem(25, caster);
+					break;
 				}
-				mpos.move(EnumFacing.SOUTH);
 			}
-			mpos.move(EnumFacing.NORTH, 2 * radius + 1);
-			mpos.move(EnumFacing.EAST);
+			int radius = (int) MathHelper.sqrt(64 * (1 + 2) * (1 + 2));
+			BiomeChangingUtils.BiomeChangerWalker walker = new BiomeChangingUtils.BiomeChangerWalker(id);
+			BlockPos.MutableBlockPos mpos = new BlockPos.MutableBlockPos();
+			mpos.setPos(effectivePos.getX() - radius, 0, effectivePos.getZ() - radius);
+			for (int x = -radius; x <= radius; x++) {
+				for (int z = -radius; z <= radius; z++) {
+					if (x * x + z * z <= radius * radius) {
+						walker.visit(world, mpos);
+					}
+					mpos.move(EnumFacing.SOUTH);
+				}
+				mpos.move(EnumFacing.NORTH, 2 * radius + 1);
+				mpos.move(EnumFacing.EAST);
+			}
+			walker.complete();
 		}
-		walker.complete();
 	}
-	
 }
