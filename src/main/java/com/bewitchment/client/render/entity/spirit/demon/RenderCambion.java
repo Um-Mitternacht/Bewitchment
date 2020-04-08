3d4aabf291666eb5913f393faecdd3678db9e4c5
Sunconure11 <eyespot7b@aol.com>
2020-04-08 14:58:16 -0400

    Cambion armor rendering

--- src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderCambion.java
+++ src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderCambion.java
@@ -6,11 +6,13 @@ import com.bewitchment.client.model.entity.spirit.demon.ModelCambionSlim;
 import com.bewitchment.common.entity.spirit.demon.EntityCambion;
 import com.bewitchment.common.entity.util.ModEntityMob;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.entity.layers.LayerBipedArmor;
 import net.minecraft.client.renderer.entity.layers.LayerHeldItem;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.util.ResourceLocation;
 
 /**
@@ -25,7 +27,45 @@ public class RenderCambion extends RenderLiving<EntityCambion> {
 	public RenderCambion(RenderManager manager) {
 		super(manager, new ModelCambion(), 0.3f);
 		this.addLayer(new LayerHeldItem(this));
-		this.addLayer(new LayerBipedArmor(this));
+		this.addLayer(new LayerBipedArmor(this) {
+			@Override
+			protected void initArmor() {
+				this.modelLeggings = new ModelBiped(0.5F);
+				this.modelArmor = new ModelBiped(1);
+			}
+			
+			@Override
+			protected void setModelSlotVisible(ModelBiped p_188359_1_, EntityEquipmentSlot slotIn) {
+				this.setModelVisible(p_188359_1_);
+				
+				switch (slotIn) {
+					case HEAD:
+						p_188359_1_.bipedHead.showModel = true;
+						p_188359_1_.bipedHeadwear.showModel = true;
+						
+						p_188359_1_.bipedHead.offsetY = -0.10f;
+						p_188359_1_.bipedHeadwear.offsetY = -0.10f;
+						break;
+					case CHEST:
+						p_188359_1_.bipedBody.showModel = true;
+						p_188359_1_.bipedRightArm.showModel = true;
+						p_188359_1_.bipedLeftArm.showModel = true;
+						
+						p_188359_1_.bipedBody.offsetY = -0.10f;
+						p_188359_1_.bipedRightArm.offsetY = -0.10f;
+						p_188359_1_.bipedLeftArm.offsetY = -0.10f;
+						break;
+					case LEGS:
+						p_188359_1_.bipedBody.showModel = false;
+						p_188359_1_.bipedRightLeg.showModel = false;
+						p_188359_1_.bipedLeftLeg.showModel = false;
+						break;
+					case FEET:
+						p_188359_1_.bipedRightLeg.showModel = false;
+						p_188359_1_.bipedLeftLeg.showModel = false;
+				}
+			}
+		});
 	}
 	
 	@Override
