3b7bd2062e966dd2a26b286766fa4e51c2d35ab6
Sunconure11 <eyespot7b@aol.com>
2020-04-13 19:52:23 -0400

    Update ModBlockWall.java

--- src/main/java/com/bewitchment/common/block/util/ModBlockWall.java
+++ src/main/java/com/bewitchment/common/block/util/ModBlockWall.java
@@ -3,23 +3,79 @@ package com.bewitchment.common.block.util;
 import com.bewitchment.Util;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWall;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
+import javax.annotation.Nonnull;
+
 /**
  * Created by Joseph on 3/6/2020.
  */
 
+//Credit to Vazkii for code that helped fix this up.
 @SuppressWarnings({"deprecation", "NullableProblems"})
 public class ModBlockWall extends BlockWall {
 	public ModBlockWall(String name, Block base, String... oreDictionaryNames) {
 		super(base);
+		setDefaultState(pickDefaultState());
 		Util.registerBlock(this, name, base, oreDictionaryNames);
+	}
+	
+	@Nonnull
+	@Override
+	public IBlockState getActualState(IBlockState state, IBlockAccess world, BlockPos pos) {
+		return super.getActualState(state, world, pos).withProperty(VARIANT, EnumType.NORMAL);
+	}
+	
+	@Override
+	public int damageDropped(IBlockState state) {
+		return getMetaFromState(state);
+	}
+	
+	@Nonnull
+	@Override
+	public BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, VARIANT, UP, NORTH, SOUTH, WEST, EAST);
+	}
+	
+	public IBlockState pickDefaultState() {
+		return blockState.getBaseState()
+				.withProperty(UP, false)
+				.withProperty(NORTH, false)
+				.withProperty(SOUTH, false)
+				.withProperty(WEST, false)
+				.withProperty(EAST, false)
+				.withProperty(VARIANT, EnumType.NORMAL);
+	}
+	
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return 0;
+	}
+	
+	@Nonnull
+	@Override
+	public IBlockState getStateFromMeta(int meta) {
+		return getDefaultState();
+	}
+	
+	@Override
+	public boolean canPlaceTorchOnTop(IBlockState state, @Nonnull IBlockAccess world, @Nonnull BlockPos pos) {
+		return true;
+	}
+	
+	@Override
+	public void getSubBlocks(CreativeTabs tabs, @Nonnull NonNullList<ItemStack> list) {
+		list.add(new ItemStack(this));
 	}
 	
 	@Override
