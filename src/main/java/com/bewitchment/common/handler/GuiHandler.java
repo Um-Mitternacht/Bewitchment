9d8942a13a62f282b2128c20ece0a5172251ed8e
Sunconure11 <eyespot7b@aol.com>
2020-04-21 16:57:39 -0400

    Update GuiHandler.java

--- src/main/java/com/bewitchment/common/handler/GuiHandler.java
+++ src/main/java/com/bewitchment/common/handler/GuiHandler.java
@@ -21,12 +21,6 @@ public class GuiHandler implements IGuiHandler {
 		if (tile instanceof TileEntityWitchesOven) return new ContainerWitchesOven(player.inventory, (TileEntityWitchesOven) tile);
 		if (tile instanceof TileEntityDistillery) return new ContainerDistillery(player.inventory, (TileEntityDistillery) tile);
 		if (tile instanceof TileEntitySpinningWheel) return new ContainerSpinningWheel(player.inventory, (TileEntitySpinningWheel) tile);
-		if (tile instanceof TileEntityTarotTable) {
-			ItemStack stack = player.getHeldItemMainhand();
-			if (stack.getItem() instanceof ItemTarotCards && stack.hasTagCompound() && stack.getTagCompound().hasKey("readId")) {
-				return new ContainerTarotTable(Collections.emptyList());
-			}
-		}
 		if (tile instanceof TileEntityJuniperChest) return new ContainerJuniperChest(player.inventory, (TileEntityJuniperChest) tile);
 		if (tile instanceof TileEntityDBChest) return new ContainerDBChest(player.inventory, (TileEntityDBChest) tile);
 		if (tile instanceof TileEntitySigilTable) return new ContainerSigilTable(player.inventory, (TileEntitySigilTable) tile);
@@ -39,7 +33,6 @@ public class GuiHandler implements IGuiHandler {
 		if (tile instanceof TileEntityWitchesOven) return new GuiWitchesOven((ContainerWitchesOven) getServerGuiElement(ModGui.OVEN.ordinal(), player, world, x, y, z), player.inventory);
 		if (tile instanceof TileEntityDistillery) return new GuiDistillery((ContainerDistillery) getServerGuiElement(ModGui.DISTILLERY.ordinal(), player, world, x, y, z));
 		if (tile instanceof TileEntitySpinningWheel) return new GuiSpinningWheel((ContainerSpinningWheel) getServerGuiElement(ModGui.SPINNING_WHEEL.ordinal(), player, world, x, y, z));
-		if (tile instanceof TileEntityTarotTable) return new GuiTarotTable((ContainerTarotTable) getServerGuiElement(ModGui.TAROT_TABLE.ordinal(), player, world, x, y, z));
 		if (tile instanceof TileEntityJuniperChest) return new GuiJuniperChest((ContainerJuniperChest) getServerGuiElement(ModGui.JUNIPER_CHEST.ordinal(), player, world, x, y, z), player.inventory);
 		if (tile instanceof TileEntitySigilTable) return new GuiSigilTable((ContainerSigilTable) getServerGuiElement(ModGui.SIGIL_TABLE.ordinal(), player, world, x, y, z));
 		if (tile instanceof TileEntityDBChest) return new GuiDragonsBloodChest((ContainerDBChest) getServerGuiElement(ModGui.DRAGONS_BLOOD_CHEST.ordinal(), player, world, x, y, z), player.inventory);
