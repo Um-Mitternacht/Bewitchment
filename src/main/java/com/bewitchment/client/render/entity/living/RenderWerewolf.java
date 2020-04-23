7a2a36129e7fbafec3799ae7c68fc0d10f689bf1
Sunconure11 <eyespot7b@aol.com>
2020-04-23 18:59:38 -0400

    Add werewolf eye layer

--- src/main/java/com/bewitchment/client/render/entity/living/RenderWerewolf.java
+++ src/main/java/com/bewitchment/client/render/entity/living/RenderWerewolf.java
@@ -3,8 +3,11 @@ package com.bewitchment.client.render.entity.living;
 import com.bewitchment.Bewitchment;
 import com.bewitchment.client.model.entity.living.ModelWerewolf;
 import com.bewitchment.common.entity.living.EntityWerewolf;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -25,6 +28,7 @@ public class RenderWerewolf extends RenderLiving<EntityWerewolf> {
 	
 	public RenderWerewolf(RenderManager manager) {
 		super(manager, new ModelWerewolf(), 0.3f);
+		addLayer(new RenderWerewolf.LayerEyes(this));
 	}
 	
 	@Nullable
@@ -44,6 +48,32 @@ public class RenderWerewolf extends RenderLiving<EntityWerewolf> {
 				return TIMBER;
 			case 5:
 				return GREY;
+		}
+	}
+	
+	private static class LayerEyes implements LayerRenderer<EntityWerewolf> {
+		private static final ResourceLocation TEX = new ResourceLocation(Bewitchment.MODID, "textures/entity/werewolf/werewolf_eyes.png");
+		
+		private final RenderWerewolf renderer;
+		
+		private LayerEyes(RenderWerewolf renderer) {
+			this.renderer = renderer;
+		}
+		
+		@Override
+		public void doRenderLayer(EntityWerewolf entity, float limbSwing, float limbSwingAmount, float partialTicks, float age, float rotationYaw, float rotationPitch, float scale) {
+			renderer.bindTexture(TEX);
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+			Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+			renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, age, rotationYaw, rotationPitch, scale);
+			Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
+			GlStateManager.disableBlend();
+		}
+		
+		@Override
+		public boolean shouldCombineTextures() {
+			return false;
 		}
 	}
 }
