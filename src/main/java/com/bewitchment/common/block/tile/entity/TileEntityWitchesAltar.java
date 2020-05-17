e5511d3a353da225696521ebc2f98f0db5092634
Pterolatypus <pterolatypus@gmail.com>
2020-05-17 12:55:18 +0100

    Reduced offset by 1, feels better

--- src/main/java/com/bewitchment/common/block/tile/entity/TileEntityWitchesAltar.java
+++ src/main/java/com/bewitchment/common/block/tile/entity/TileEntityWitchesAltar.java
@@ -130,7 +130,7 @@ public class TileEntityWitchesAltar extends ModTileEntity implements ITickable {
 			int x = counter & 31;
 			int y = (counter >> 5) & 31;
 			int z = (counter >> 10) & 31;
-			checking.setPos(pos.getX() + x - 16, pos.getY() + y - 16, pos.getZ() + z - 16);
+			checking.setPos(pos.getX() + x - 15, pos.getY() + y - 15, pos.getZ() + z - 15);
 			registerToMap(world.getBlockState(checking));
 			if (counter == Short.MAX_VALUE - 1) {
 				boolean foundStone = false, foundCup = false, foundPentacle = false, foundSword = false, foundWand = false;
