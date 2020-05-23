8b38741cd10510d21e57f64a3900ccd3e7d45d79
Sunconure11 <eyespot7b@aol.com>
2020-05-23 19:55:24 -0400

    Update RenderBafometyr.java

--- src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderBafometyr.java
+++ src/main/java/com/bewitchment/client/render/entity/spirit/demon/RenderBafometyr.java
@@ -3,9 +3,13 @@ package com.bewitchment.client.render.entity.spirit.demon;
 import com.bewitchment.Bewitchment;
 import com.bewitchment.client.model.entity.spirit.demon.ModelBafometyr;
 import com.bewitchment.common.entity.spirit.demon.EntityBafometyr;
+import com.bewitchment.common.entity.util.ModEntityMob;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
 import net.minecraft.util.ResourceLocation;
 
 import javax.annotation.Nullable;
@@ -15,10 +19,10 @@ import javax.annotation.Nullable;
  */
 public class RenderBafometyr extends RenderLiving<EntityBafometyr> {
 	private static final ResourceLocation TEX = new ResourceLocation(Bewitchment.MODID, "textures/entity/bafometyr.png");
-	private static final ResourceLocation[] FLAMES = {new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_1.png"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_2"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_3"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_4"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_5"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_6"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_7"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_8")};
 	
 	public RenderBafometyr(RenderManager manager) {
 		this(manager, new ModelBafometyr());
+		addLayer(new RenderBafometyr.LayerFlames(this));
 	}
 	
 	protected RenderBafometyr(RenderManager manager, ModelBase model) {
@@ -29,5 +33,31 @@ public class RenderBafometyr extends RenderLiving<EntityBafometyr> {
 	@Override
 	protected ResourceLocation getEntityTexture(EntityBafometyr entity) {
 		return TEX;
+	}
+	
+	private static class LayerFlames implements LayerRenderer<EntityBafometyr> {
+		private static final ResourceLocation[] TEX = {new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_1.png"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_2"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_3"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_4"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_5"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_6"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_7"), new ResourceLocation(Bewitchment.MODID, "textures/entity/layer/bafometyr_fire_8")};
+		
+		private final RenderBafometyr renderer;
+		
+		private LayerFlames(RenderBafometyr renderer) {
+			this.renderer = renderer;
+		}
+		
+		@Override
+		public void doRenderLayer(EntityBafometyr entity, float limbSwing, float limbSwingAmount, float partialTicks, float age, float rotationYaw, float rotationPitch, float scale) {
+			renderer.bindTexture(TEX[entity.getDataManager().get(ModEntityMob.SKIN)]);
+			GlStateManager.enableBlend();
+			GlStateManager.blendFunc(GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ONE);
+			Minecraft.getMinecraft().entityRenderer.setupFogColor(true);
+			renderer.getMainModel().render(entity, limbSwing, limbSwingAmount, age, rotationYaw, rotationPitch, scale);
+			Minecraft.getMinecraft().entityRenderer.setupFogColor(false);
+			GlStateManager.disableBlend();
+		}
+		
+		@Override
+		public boolean shouldCombineTextures() {
+			return false;
+		}
 	}
 }
