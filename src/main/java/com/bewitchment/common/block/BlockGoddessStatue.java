47f4e34b7ca8fa441f7ba89404250f8e2c12e01c
Sunconure11 <eyespot7b@aol.com>
2020-06-08 10:23:26 -0400

    More work on it

--- src/main/java/com/bewitchment/common/block/BlockGoddessStatue.java
+++ src/main/java/com/bewitchment/common/block/BlockGoddessStatue.java
@@ -1,26 +1,29 @@
 package com.bewitchment.common.block;
 
 import com.bewitchment.api.capability.extendedplayer.ExtendedPlayer;
+import com.bewitchment.common.block.tile.entity.TileEntityGoddessStatue;
 import com.bewitchment.common.block.util.ModBlock;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 /**
  * Created by Joseph on 6/8/2020.
  */
-public class BlockGoddessStatue extends ModBlock {
+public class BlockGoddessStatue extends ModBlock implements ITileEntityProvider {
 	public BlockGoddessStatue() {
 		super("goddess_statue", Material.ROCK, SoundType.STONE, 10000, 10000, "pickaxe", 10000);
 	}
 	
+	@Nullable
 	@Override
-	public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
-		super.onBlockClicked(worldIn, pos, playerIn);
-		ExtendedPlayer cap = playerIn.getCapability(ExtendedPlayer.CAPABILITY, null);
-		if (!cap.getCurses().isEmpty())
-			cap.getCurses().clear();
+	public TileEntity createNewTileEntity(World worldIn, int meta) {
+		return new TileEntityGoddessStatue();
 	}
 }
---
+++ src/main/java/com/bewitchment/common/block/tile/entity/TileEntityGoddessStatue.java
@@ -0,0 +1,8 @@
+package com.bewitchment.common.block.tile.entity;
+
+import com.bewitchment.common.block.tile.entity.util.ModTileEntity;
+
+/**
+ * Created by Joseph on 6/8/2020.
+ */
+public class TileEntityGoddessStatue extends ModTileEntity {}
