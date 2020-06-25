9a8aad57fe2cdf84e931afed45253e5dd3419679
Sunconure11 <eyespot7b@aol.com>
2020-06-25 18:09:03 -0400

    Prepare for a new build

--- src/main/java/com/bewitchment/common/curse/CurseParanoia.java
+++ src/main/java/com/bewitchment/common/curse/CurseParanoia.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.init.MobEffects;
 import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundCategory;
@@ -28,7 +27,7 @@ public class CurseParanoia extends Curse {
 	//Blindness
 	//Display random messages
 	//Need to figure out capabilities so every player does not hear the same sound as the same time if there are multiples with the same curse. For now, this works.
-	public int timer = 750;
+	public int timer = 1150;
 
 	public CurseParanoia() {
 		super(new ResourceLocation(Bewitchment.MODID, "paranoia"), Arrays.asList(Util.get(Items.ENDER_EYE), Util.get(Items.BLAZE_POWDER), Util.get(ModObjects.dragons_blood_resin), Util.get(ModObjects.snake_venom), Util.get(Items.GHAST_TEAR), Util.get(ModObjects.ectoplasm), Util.get(ModObjects.demonic_elixir), Util.get(ModObjects.taglock)), false, false, CurseCondition.EXIST);
@@ -36,49 +35,73 @@ public class CurseParanoia extends Curse {
 
 	@Override
 	public boolean doCurse(Event event, EntityPlayer target) {
-		target.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 256, 1));
+		target.addPotionEffect(new PotionEffect(MobEffects.BLINDNESS, 256, 3));
 		Random rand = target.getRNG();
 		World world = target.getEntityWorld();
 		BlockPos pos = target.getPosition();
 		int i = rand.nextInt(100);
 		if (timer > 0) timer--;
 		if (i < 10 && timer == 0) {
-			switch (rand.nextInt(10)) {
+			switch (rand.nextInt(16)) {
 				case 0:
 					world.playSound(null, pos, SoundEvents.ENTITY_ENDERMEN_SCREAM, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 1:
 					world.playSound(null, pos, SoundEvents.ENTITY_BLAZE_AMBIENT, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 2:
 					world.playSound(null, pos, SoundEvents.ENTITY_WOLF_GROWL, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 3:
 					world.playSound(null, pos, ModSounds.WEREWOLF_HOWL, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 4:
 					world.playSound(null, pos, SoundEvents.ENTITY_SLIME_SQUISH, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 5:
 					world.playSound(null, pos, SoundEvents.ENTITY_CREEPER_PRIMED, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 6:
 					world.playSound(null, pos, SoundEvents.ENTITY_SPIDER_AMBIENT, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 7:
 					world.playSound(null, pos, SoundEvents.ENTITY_ENDERDRAGON_GROWL, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
 					break;
 				case 8:
 					world.playSound(null, pos, SoundEvents.BLOCK_CHEST_OPEN, SoundCategory.HOSTILE, 1, 1);
-					timer = 750;
+					timer = 1150;
+					break;
+				case 9:
+					world.playSound(null, pos, SoundEvents.BLOCK_WOODEN_DOOR_OPEN, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
+					break;
+				case 10:
+					world.playSound(null, pos, SoundEvents.ENTITY_PLAYER_HURT, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
+					break;
+				case 11:
+					world.playSound(null, pos, SoundEvents.ITEM_FLINTANDSTEEL_USE, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
+					break;
+				case 12:
+					world.playSound(null, pos, SoundEvents.ENTITY_ZOMBIE_AMBIENT, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
+					break;
+				case 13:
+					world.playSound(null, pos, SoundEvents.BLOCK_LAVA_POP, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
+					break;
+				case 14:
+					world.playSound(null, pos, SoundEvents.BLOCK_GLASS_BREAK, SoundCategory.HOSTILE, 1, 1);
+					timer = 1150;
 					break;
 			}
 		}
