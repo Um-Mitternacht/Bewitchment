454778aac260440ff86815dc41ded5e38285b9cc
Sunconure11 <eyespot7b@aol.com>
2020-07-06 03:21:40 -0400

    salamander skin is no longer nether-based

--- src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
+++ src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
@@ -6,24 +6,15 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.DimensionType;
-import net.minecraftforge.common.BiomeDictionary;
 
 public class FortuneSalamanderSkin extends Fortune {
 	public FortuneSalamanderSkin() {
-		super(new ResourceLocation(Bewitchment.MODID, "salamander_skin"), false, 66, 6666);
+		super(new ResourceLocation(Bewitchment.MODID, "salamander_skin"), false, (60), (60 * 10));
 	}
 
 	@Override
 	public boolean apply(EntityPlayer player) {
-		BlockPos pos = player.getPosition();
-		if (BiomeDictionary.hasType(player.world.getBiome(pos), BiomeDictionary.Type.NETHER)) {
-			if (player.world.provider.getDimensionType() == DimensionType.NETHER) {
-				player.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 3600, 0, false, false));
-				return true;
-			}
-		}
-		return false;
+		player.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 3600, 0, false, false));
+		return true;
 	}
 }
