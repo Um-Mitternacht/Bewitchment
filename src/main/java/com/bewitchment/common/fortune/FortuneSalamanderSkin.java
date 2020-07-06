1bf7176ea7b8d170ad6fa983ed0e41717b012fdb
Sunconure11 <eyespot7b@aol.com>
2020-07-06 01:02:23 -0400

    Update FortuneSalamanderSkin.java

--- src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
+++ src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
@@ -7,6 +7,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.DimensionType;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,21 +15,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class FortuneSalamanderSkin extends Fortune {
     public FortuneSalamanderSkin() {
         super(new ResourceLocation(Bewitchment.MODID, "salamander_skin"), false, (60), (60 * 10));
-        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
     public boolean apply(EntityPlayer player) {
-        return true;
-    }
-
-    @SubscribeEvent
-    public void onNetherPotion(PotionEvent.PotionAddedEvent event) {
-        ExtendedPlayer cap = event.getEntityLiving().getCapability(ExtendedPlayer.CAPABILITY, null);
-        if (cap.fortune == this) {
-            if (event.getEntityLiving().world.provider.isNether()) {
-                event.getEntityLiving().addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 1000, 0, false, false));
-            }
+        if (player.world.provider.getDimensionType() == DimensionType.NETHER) {
+            player.addPotionEffect(new PotionEffect(MobEffects.FIRE_RESISTANCE, 1000, 0, false, false));
         }
+        return true;
     }
 }
