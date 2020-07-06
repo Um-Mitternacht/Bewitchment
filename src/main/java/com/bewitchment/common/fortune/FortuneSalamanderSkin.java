8db6bc0c8f307a30716bfc8147d5a69137ff8312
Sunconure11 <eyespot7b@aol.com>
2020-07-06 01:02:32 -0400

    Update FortuneSalamanderSkin.java

--- src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
+++ src/main/java/com/bewitchment/common/fortune/FortuneSalamanderSkin.java
@@ -1,16 +1,12 @@
 package com.bewitchment.common.fortune;
 
 import com.bewitchment.Bewitchment;
-import com.bewitchment.api.capability.extendedplayer.ExtendedPlayer;
 import com.bewitchment.api.registry.Fortune;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.MobEffects;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.DimensionType;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.entity.living.PotionEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class FortuneSalamanderSkin extends Fortune {
     public FortuneSalamanderSkin() {
