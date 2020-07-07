78a54fd75953fc76d2f4abc7dec07ca6190d934a
Sunconure11 <eyespot7b@aol.com>
2020-07-07 14:12:28 -0400

    Part 2

--- src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
+++ src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
@@ -16,6 +16,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.items.ItemStackHandler;
 
 import java.util.Arrays;
@@ -43,11 +44,15 @@ public class RitualBiomeShift extends Ritual {
 			}
 			int radius = (int) MathHelper.sqrt(64 * (1 + 2) * (1 + 2));
 			BlockPos.MutableBlockPos mpos = new BlockPos.MutableBlockPos();
+			BlockPos.MutableBlockPos radiusvalue = new BlockPos.MutableBlockPos();
+			BlockPos.MutableBlockPos radiusvalue2 = new BlockPos.MutableBlockPos();
 			mpos.setPos(effectivePos.getX() - radius, 0, effectivePos.getZ() - radius);
+			radiusvalue.setPos(0, 0, effectivePos.getZ());
+			radiusvalue2.setPos(effectivePos.getX(), 0, 0);
 			for (int x = -radius; x <= radius; x++) {
 				for (int z = -radius; z <= radius; z++) {
 					if (x * x + z * z <= radius * radius) {
-						BiomeChangingUtils.setBiome(world, Biome.getBiomeForId(id), mpos);
+						BiomeChangingUtils.setMultiBiome(world, Biome.getBiomeForId(id), radiusvalue, radiusvalue2);
 					}
 				}
 			}
