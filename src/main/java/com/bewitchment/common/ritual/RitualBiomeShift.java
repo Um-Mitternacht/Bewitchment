156d8ac36903f42886e2e67bf72a1d464ea30e88
Sunconure11 <eyespot7b@aol.com>
2020-07-09 13:16:42 -0400

    Revert "Update RitualBiomeShift.java"
    This reverts commit b3331b3613ec80e30fcd2b411482356fce756eea.

--- src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
+++ src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
@@ -13,8 +13,10 @@ import net.minecraft.init.Biomes;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.Chunk;
 import net.minecraftforge.items.ItemStackHandler;
 
 import java.util.Arrays;
@@ -24,35 +26,44 @@ import java.util.Collections;
  * Original code by Zabi94, modified by Sunconure11 afterwards, with bits stuck on from Ael.
  */
 public class RitualBiomeShift extends Ritual {
-    public RitualBiomeShift() {
-        super(new ResourceLocation(Bewitchment.MODID, "biome_shift"), Arrays.asList(Util.get("treeSapling"), Util.get("logWood"), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.pentacle), Util.get(new ItemStack(ModObjects.oak_spirit)), Util.get(new ItemStack(ModObjects.dimensional_sand)), Util.get(new ItemStack(ModObjects.boline))), null, Collections.singletonList(new ItemStack(ModObjects.boline, 1, 0)), 50, 1300, 33, BlockGlyph.ENDER, BlockGlyph.ENDER, BlockGlyph.ENDER);
-    }
+	public RitualBiomeShift() {
+		super(new ResourceLocation(Bewitchment.MODID, "biome_shift"), Arrays.asList(Util.get("treeSapling"), Util.get("logWood"), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.oak_apple_gall), Util.get(ModObjects.pentacle), Util.get(new ItemStack(ModObjects.oak_spirit)), Util.get(new ItemStack(ModObjects.dimensional_sand)), Util.get(new ItemStack(ModObjects.boline))), null, Collections.singletonList(new ItemStack(ModObjects.boline, 1, 0)), 50, 1300, 33, BlockGlyph.ENDER, BlockGlyph.ENDER, BlockGlyph.ENDER);
+	}
 
-    @Override
-    public void onFinished(World world, BlockPos altarPos, BlockPos effectivePos, EntityPlayer caster, ItemStackHandler inventory) {
-        int id = Biome.getIdForBiome(Biomes.PLAINS);
-        if (world.getTileEntity(effectivePos) instanceof TileEntityGlyph) {
-            for (int i = 0; i < inventory.getSlots(); i++) {
-                ItemStack stack = inventory.getStackInSlot(i);
-                if (stack.getItem() instanceof ItemBoline) {
-                    id = stack.getTagCompound().getInteger("biome_id");
-                    stack.damageItem(25, caster);
-                    {
-                        //
-                        BlockPos.MutableBlockPos Z = new BlockPos.MutableBlockPos();
-                        Z.setPos(16, 0, 16);
-                        //
-                        BlockPos.MutableBlockPos X = new BlockPos.MutableBlockPos();
-                        X.setPos(-16, 0, -16);
+	public int getRadius() {
+		return 16;
+	}
 
-                        Iterable<BlockPos> pos1 = BlockPos.getAllInBox(Z, X);
-                        int finalId = id;
-                        pos1.forEach(pos -> {
-                            BiomeChangingUtils.setBiome(world, Biome.getBiomeForId(finalId), effectivePos);
-                        });
-                    }
-                }
-            }
-        }
-    }
+	@Override
+	public void onFinished(World world, BlockPos altarPos, BlockPos effectivePos, EntityPlayer caster, ItemStackHandler inventory) {
+		int id = Biome.getIdForBiome(Biomes.PLAINS);
+		if (world.getTileEntity(effectivePos) instanceof TileEntityGlyph) {
+			for (int i = 0; i < inventory.getSlots(); i++) {
+				ItemStack stack = inventory.getStackInSlot(i);
+				if (stack.getItem() instanceof ItemBoline) {
+					id = stack.getTagCompound().getInteger("biome_id");
+					stack.damageItem(25, caster);
+					{
+						double radius = getRadius();
+						//
+						BlockPos.MutableBlockPos minZ = new BlockPos.MutableBlockPos();
+						minZ.setPos(0, 0, effectivePos.getZ() - --radius);
+						//
+						BlockPos.MutableBlockPos maxZ = new BlockPos.MutableBlockPos();
+						maxZ.setPos(0, 0, effectivePos.getZ() + ++radius);
+						//
+						BlockPos.MutableBlockPos minX = new BlockPos.MutableBlockPos();
+						minX.setPos(effectivePos.getX() - --radius, 0, 0);
+						//
+						BlockPos.MutableBlockPos maxX = new BlockPos.MutableBlockPos();
+						maxX.setPos(effectivePos.getX() + ++radius, 0, 0);
+						{
+							BiomeChangingUtils.setMultiBiome(world, Biome.getBiomeForId(id), minZ, maxZ, minX, maxX);
+							break;
+						}
+					}
+				}
+			}
+		}
+	}
 }
