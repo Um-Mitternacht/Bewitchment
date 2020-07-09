6212f127f4b6487e7f41fd6a702bc500b1774fa6
Sunconure11 <eyespot7b@aol.com>
2020-07-09 11:44:23 -0400

    Update RitualBiomeShift.java

--- src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
+++ src/main/java/com/bewitchment/common/ritual/RitualBiomeShift.java
@@ -46,10 +46,9 @@ public class RitualBiomeShift extends Ritual {
                         X.setPos(-16, 0, -16);
 
                         Iterable<BlockPos> pos1 = BlockPos.getAllInBox(Z, X);
-                        int finalId = id;
-                        pos1.forEach(pos -> {
-                            BiomeChangingUtils.setBiome(world, Biome.getBiomeForId(finalId), effectivePos);
-                        });
+                        for (BlockPos pos : pos1) {
+                            BiomeChangingUtils.setBiome(world, Biome.getBiomeForId(id), (BlockPos) BlockPos.MutableBlockPos.getAllInBox(X, Z));
+                        }
                     }
                 }
             }
