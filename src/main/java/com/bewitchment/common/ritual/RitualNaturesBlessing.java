1e6c320ece0dd4d5b6ce86a697086beb4043ca36
Sunconure11 <eyespot7b@aol.com>
2020-07-22 16:03:09 -0400

    Create RitualNaturesBlessing.java

---
+++ src/main/java/com/bewitchment/common/ritual/RitualNaturesBlessing.java
@@ -0,0 +1,49 @@
+package com.bewitchment.common.ritual;
+
+import com.bewitchment.Bewitchment;
+import com.bewitchment.Util;
+import com.bewitchment.api.registry.Ritual;
+import com.bewitchment.common.block.BlockGlyph;
+import com.bewitchment.common.world.BiomeChangingUtils;
+import com.bewitchment.registry.ModObjects;
+import net.minecraft.block.*;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Biomes;
+import net.minecraft.init.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
+import net.minecraftforge.items.ItemStackHandler;
+
+import java.util.Arrays;
+
+/**
+ * Original code by Zabi94, modified by Sunconure11 afterwards, with bits stuck on from Ael.
+ */
+public class RitualNaturesBlessing extends Ritual {
+	public RitualNaturesBlessing() {
+		super(new ResourceLocation(Bewitchment.MODID, "natures_blessing"), Arrays.asList(Util.get("salt"), Util.get("salt"), Util.get(ModObjects.demon_heart), Util.get(ModObjects.opal), Util.get(ModObjects.pentacle), Util.get(new ItemStack(ModObjects.ebb_of_death)), Util.get(new ItemStack(ModObjects.dimensional_sand)), Util.get(new ItemStack(ModObjects.lizard_leg)), Util.get(new ItemStack(ModObjects.ravens_feather)), Util.get(new ItemStack(ModObjects.ectoplasm))), null, null, true, 150, 1600, 75, BlockGlyph.ENDER, BlockGlyph.NETHER, BlockGlyph.ENDER);
+	}
+
+	@Override
+	public void onFinished(World world, BlockPos altarPos, BlockPos effectivePos, EntityPlayer caster, ItemStackHandler inventory) {
+		int radius = 32;
+		int minY = 64;
+		int maxY = 256;
+		for (double x = -radius; x < radius; x++) {
+			for (double y = -minY; y < maxY; y++) {
+				for (double z = -radius; z < radius; z++) {
+					if (Math.sqrt((x * x) + (z * z)) < radius) {
+						BlockPos pos = effectivePos.add(x, y, z);
+						Block block = world.getBlockState(pos).getBlock();
+						if (block == Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.COARSE_DIRT)) {
+							world.setBlockState(pos, Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT), 3);
+						}
+					}
+				}
+			}
+		}
+	}
+}
