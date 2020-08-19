ef184e48d3efd384d16b1bf6b9446c1e61a4c904
Sunconure11 <eyespot7b@aol.com>
2020-08-18 21:19:06 -0400

    Workspace tweaks, just in case I need to push out another build.

--- src/main/java/com/bewitchment/common/entity/spirit/demon/EntityDemon.java
+++ src/main/java/com/bewitchment/common/entity/spirit/demon/EntityDemon.java
@@ -262,7 +262,7 @@ public class EntityDemon extends ModEntityMob implements IMerchant {
 
 	@Override
 	public IEntityLivingData onInitialSpawn(DifficultyInstance difficulty, IEntityLivingData data) {
-		this.setCustomNameTag((rand.nextInt(3) == 0 ? new TextComponentTranslation("entity.bewitchment.prefix." + rand.nextInt(102)).getFormattedText() + " " : "") + new TextComponentTranslation("entity.bewitchment.given_name." + rand.nextInt(506)).getFormattedText());
+		this.setCustomNameTag((rand.nextInt(3) == 0 ? new TextComponentTranslation("entity.bewitchment.prefix." + rand.nextInt(102)).getFormattedText() + " " : "") + new TextComponentTranslation("entity.bewitchment.given_name." + rand.nextInt(518)).getFormattedText());
 		return super.onInitialSpawn(difficulty, data);
 	}
 
